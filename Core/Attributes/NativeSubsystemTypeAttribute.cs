d257f070c31bcbf2128e832e92785afcb6187f23
Andrew Piper <threax@users.noreply.github.com>
2009-04-03 12:12:45 +0000

    Added Core contents.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@12 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Core/Attributes/NativeSubsystemTypeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public sealed class NativeSubsystemTypeAttribute : Attribute
+    {
+
+    }
+}
