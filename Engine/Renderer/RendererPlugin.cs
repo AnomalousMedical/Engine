2b9d085bb73db38da023d6c22fb1164bbe9d5f15
Andrew Piper <threax@users.noreply.github.com>
2009-04-17 19:30:13 +0000

    Elaborated comments.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@95 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Engine/Renderer/RendererPlugin.cs
+++ Engine/Renderer/RendererPlugin.cs
@@ -14,19 +14,23 @@ namespace Engine.Renderer
         /// <summary>
         /// Get the RendererWindow of the primary window. If the window was
         /// auto-created this will be that window, otherwise it will be the
-        /// first embedded window added to the renderer.
+        /// first embedded window added to the renderer. The PrimaryWindow will
+        /// be managed by the plugin so it does not need to be destroyed.
         /// </summary>
         RendererWindow PrimaryWindow { get; }
 
         /// <summary>
-        /// Create a new RendererWindow from the Renderer.
+        /// Create a new RendererWindow from the Renderer. Any windows created
+        /// with this function should be destroyed using destroyRendererWindow
+        /// when they are no longer needed.
         /// </summary>
         /// <param name="embedWindow">The window to embed the new RendererWindow into.</param>
         /// <returns>A RendererWindow that can be used to access the newly created window.</returns>
         RendererWindow createRendererWindow(OSWindow embedWindow, String name);
 
         /// <summary>
-        /// Destroy a given RendererWindow.
+        /// Destroy a given RendererWindow. This should be called for all
+        /// windows created with createRendererWindow.
         /// </summary>
         /// <param name="window">The window to destroy.</param>
         void destroyRendererWindow(RendererWindow window);
