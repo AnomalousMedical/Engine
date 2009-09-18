3db6c4c6e6ab467b6d73157962a15acaf77eacf6
Andrew Piper <threax@users.noreply.github.com>
2009-09-18 15:18:00 +0000

    Allowed attributes to be applied to events.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@417 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Core/Attributes/DoNotSaveAttribute.cs
+++ Core/Attributes/DoNotSaveAttribute.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Engine.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Event, Inherited = true, AllowMultiple = false)]
     public sealed class DoNotSaveAttribute : Attribute
     {
 
