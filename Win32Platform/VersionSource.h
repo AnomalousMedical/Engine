65b7df74f79b58adb4daa30954901029474b7b42
Andrew Piper <threax@users.noreply.github.com>
2010-03-04 19:02:54 +0000

    Made Win32Platform have version info.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@497 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Win32Platform/VersionSource.h
@@ -0,0 +1,15 @@
+#define ASSEMBLY_TITLE "Win32Platform"
+#define ASSEMBLY_DESCRIPTION "Win32Platform Dynamic Link Library"
+#define ASSEMBLY_CONFIGURATION ""
+#define ASSEMBLY_COMPANY "Anomalous Medical"
+#define ASSEMBLY_PRODUCT "Win32Platform"
+#define ASSEMBLY_COPYRIGHT "Copyright (c) Anomalous Medical 2010"
+#define ASSEMBLY_TRADEMARK ""
+#define ASSEMBLY_CULTURE ""
+#define ORIGINAL_FILE_NAME "Win32Platform.dll"
+
+#define ASSEMBLY_VERSION "1.0.0.$WCREV$"
+#define FILE_VERSION 1,0,0,$WCREV$
+#define FILE_VERSION_STR "1.0.0.$WCREV$"
+#define PRODUCT_VERSION 1,0,0,$WCREV$
+#define PRODUCT_VERSION_STR "1.0.0.$WCREV$"
