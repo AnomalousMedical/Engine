bd79e1736c872611f6d314c80f0b4e8df3de8192
Andrew Piper <threax@users.noreply.github.com>
2010-03-04 18:46:20 +0000

    Made the BulletPlugin have version information.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@494 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ BulletPlugin/VersionSource.h
@@ -0,0 +1,15 @@
+#define ASSEMBLY_TITLE "BulletPlugin"
+#define ASSEMBLY_DESCRIPTION "BulletPlugin Dynamic Link Library"
+#define ASSEMBLY_CONFIGURATION ""
+#define ASSEMBLY_COMPANY "Anomalous Medical"
+#define ASSEMBLY_PRODUCT "BulletPlugin"
+#define ASSEMBLY_COPYRIGHT "Copyright (c) Anomalous Medical 2010"
+#define ASSEMBLY_TRADEMARK ""
+#define ASSEMBLY_CULTURE ""
+#define ORIGINAL_FILE_NAME "BulletPlugin.dll"
+
+#define ASSEMBLY_VERSION "1.0.0.$WCREV$"
+#define FILE_VERSION 1,0,0,$WCREV$
+#define FILE_VERSION_STR "1.0.0.$WCREV$"
+#define PRODUCT_VERSION 1,0,0,$WCREV$
+#define PRODUCT_VERSION_STR "1.0.0.$WCREV$"
