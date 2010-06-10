81fada7c2da5ca59391837c2ac2e140e858318ee
Andrew Piper <threax@users.noreply.github.com>
2010-06-10 13:47:44 +0000

    Implemented mouse wheel and key repeat for the CEGUIUpdate.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@677 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- CEGUIPlugin/Plugin/CEGUIUpdate.cs
+++ CEGUIPlugin/Plugin/CEGUIUpdate.cs
@@ -14,6 +14,10 @@ namespace CEGUIPlugin
         EventManager eventManager;
         bool[] mouseButtonsDown = new bool[(int)MouseButton.MouseButtonCount];
         bool[] keysDown = new bool[256];
+        float[] keysDownTime = new float[256]; //used for auto repeat
+        bool[] keyQuickRepeat = new bool[256];
+        private const float START_REPEAT_TIME = 0.5f;
+        private const float QUICK_REPEAT_TIME = 0.03f;
 
         public CEGUIUpdate(CEGUISystem system, EventManager eventManager)
         {
@@ -23,10 +27,15 @@ namespace CEGUIPlugin
 
         public void sendUpdate(Clock clock)
         {
-            system.injectTimePulse(clock.fSeconds);
+            float time = clock.fSeconds;
+            system.injectTimePulse(time);
+
+            //Mouse
             Mouse mouse = eventManager.Mouse;
             Vector3 mousePos = mouse.getAbsMouse();
             system.injectMousePosition(mousePos.x, mousePos.y);
+            Vector3 mouseRel = mouse.getRelMouse();
+            system.injectMouseWheelChange(mouseRel.z / 120.0f);
             for(int i = 0; i < mouseButtonsDown.Length; i++)
             {
                 bool down = mouse.buttonDown((MouseButtonCode)i);
@@ -44,10 +53,36 @@ namespace CEGUIPlugin
                 }
             }
 
+            //Keyboard
             Keyboard keyboard = eventManager.Keyboard;
             for (uint i = 0; i < keysDown.Length; i++)
             {
                 bool down = keyboard.isKeyDown((KeyboardButtonCode)i);
+
+                //Check auto repeat
+                if(down && keysDown[i])
+                {
+                    keysDownTime[i] += time;
+                    if (keyQuickRepeat[i])
+                    {
+                        if (keysDownTime[i] > QUICK_REPEAT_TIME)
+                        {
+                            keysDownTime[i] = 0.0f;
+                            keysDown[i] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (keysDownTime[i] > START_REPEAT_TIME)
+                        {
+                            keysDownTime[i] = 0.0f;
+                            keysDown[i] = false;
+                            keyQuickRepeat[i] = true;
+                        }
+                    }
+                }
+
+                //Alert CEGUI
                 if (down != keysDown[i])
                 {
                     if (down)
@@ -62,6 +97,8 @@ namespace CEGUIPlugin
                     else
                     {
                         system.injectKeyUp(i);
+                        keyQuickRepeat[i] = false;
+                        keysDownTime[i] = 0.0f;
                     }
                     keysDown[i] = down;
                 }
