5e5738464a54576d89181852fb6f1691a79d15a2
Andrew Piper <threax@users.noreply.github.com>
2010-06-14 19:47:45 +0000

    Added size and Vector2 operators.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@696 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Engine/Math/2d/Size.cs
+++ Engine/Math/2d/Size.cs
@@ -10,9 +10,79 @@ namespace Engine
     public struct Size
     {
         [FieldOffset(0)]
-        public float width;
+        public float Width;
 
         [FieldOffset(4)]
-        public float height;
+        public float Height;
+
+        public Size(float width, float height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        //public float Width
+        //{
+        //    get
+        //    {
+        //        return width;
+        //    }
+        //    set
+        //    {
+        //        width = value;
+        //    }
+        //}
+
+        //public float Height
+        //{
+        //    get
+        //    {
+        //        return height;
+        //    }
+        //    set
+        //    {
+        //        height = value;
+        //    }
+        //}
+
+        public static Size operator +(Size v1, Size v2)
+        {
+            return new Size(v1.Width + v2.Width, v1.Height + v2.Height);
+        }
+
+        public static Size operator *(Size v1, Size v2)
+        {
+            return new Size(v1.Width * v2.Width, v1.Height * v2.Height);
+        }
+
+        public static Size operator -(Size v1, Size v2)
+        {
+            return new Size(v1.Width - v2.Width, v1.Height - v2.Height);
+        }
+
+        public static Size operator -(Size v)
+        {
+            return new Size(-v.Width, -v.Height);
+        }
+
+        public static Size operator *(Size v, float s)
+        {
+            return new Size(v.Width + s, v.Height + s);
+        }
+
+        public static Size operator *(float s, Size v)
+        {
+            return v * s;
+        }
+
+        public static Size operator /(Size v, float s)
+        {
+            return v * (1.0f / s);
+        }
+
+        public static Size operator /(Size v1, Size v2)
+        {
+            return new Size(v1.Width / v2.Width, v1.Height / v2.Height);
+        }
     }
 }
--- Engine/Math/2d/Vector2.cs
+++ Engine/Math/2d/Vector2.cs
@@ -14,5 +14,51 @@ namespace Engine
 
         [FieldOffset(4)]
         public float y;
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x + v2.x, v1.y + v2.y);
+        }
+
+        public static Vector2 operator *(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x * v2.x, v1.y * v2.y);
+        }
+
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        public static Vector2 operator -(Vector2 v)
+        {
+            return new Vector2(-v.x, -v.y);
+        }
+
+        public static Vector2 operator *(Vector2 v, float s)
+        {
+            return new Vector2(v.x + s, v.y + s);
+        }
+
+        public static Vector2 operator *(float s, Vector2 v)
+        {
+            return v * s;
+        }
+
+        public static Vector2 operator /(Vector2 v, float s)
+        {
+            return v * (1.0f / s);
+        }
+
+        public static Vector2 operator /(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x / v2.x, v1.y / v2.y);
+        }
     }
 }
