0aab0f15b2e15d3b22a414e0ecd996f6aa85d427
Andrew Piper <threax@users.noreply.github.com>
2010-06-18 20:25:24 +0000

    Wrapped a couple of StaticImage functions.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@728 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Wrapper/Widgets/StaticImage.cs
+++ MyGUIPlugin/Wrapper/Widgets/StaticImage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace MyGUIPlugin
 {
@@ -12,5 +13,33 @@ namespace MyGUIPlugin
         {
 
         }
+
+        public void setItemResource(String value)
+        {
+            StaticImage_setItemResource(widget, value);
+        }
+
+        public void setItemGroup(String value)
+        {
+            StaticImage_setItemGroup(widget, value);
+        }
+
+        public void setItemName(String value)
+        {
+            StaticImage_setItemName(widget, value);
+        }
+
+#region PInvoke
+
+        [DllImport("MyGUIWrapper")]
+        private static extern void StaticImage_setItemResource(IntPtr staticImage, String value);
+
+        [DllImport("MyGUIWrapper")]
+        private static extern void StaticImage_setItemGroup(IntPtr staticImage, String value);
+
+        [DllImport("MyGUIWrapper")]
+        private static extern void StaticImage_setItemName(IntPtr staticImage, String value);
+
+#endregion
     }
 }
