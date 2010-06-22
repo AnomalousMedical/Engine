0096ee8f38f23df151a140dd40b25e9a52b7dd50
Andrew Piper <threax@users.noreply.github.com>
2010-06-22 15:00:02 +0000

    Added the LayerManager class.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@734 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Wrapper/System/LayerManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace MyGUIPlugin
+{
+    public class LayerManager
+    {
+        private static LayerManager instance;
+
+        public static LayerManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LayerManager();
+                }
+                return instance;
+            }
+        }
+
+        private IntPtr layerManager;
+
+        private LayerManager()
+        {
+            layerManager = LayerManager_getSingletonPtr();
+        }
+
+        public void attachToLayerNode(String name, Widget item)
+        {
+            LayerManager_attachToLayerNode(layerManager, name, item.WidgetPtr);
+        }
+
+        public void detachFromLayer(Widget item)
+        {
+            LayerManager_detachFromLayer(layerManager, item.WidgetPtr);
+        }
+
+        public void upLayerItem(Widget item)
+        {
+            LayerManager_upLayerItem(layerManager, item.WidgetPtr);
+        }
+
+        public bool load(String file)
+        {
+            return LayerManager_load(layerManager, file);
+        }
+
+        public bool isExist(String name)
+        {
+            return LayerManager_isExist(layerManager, name);
+        }
+
+        public Widget getWidgetFromPoint(int left, int top)
+        {
+            return WidgetManager.getWidget(LayerManager_getWidgetFromPoint(layerManager, left, top));
+        }
+
+#region PInvoke
+        [DllImport("MyGUIWrapper")]
+        private static extern IntPtr LayerManager_getSingletonPtr();
+
+        [DllImport("MyGUIWrapper")]
+        private static extern void LayerManager_attachToLayerNode(IntPtr layerManager, String name, IntPtr item);
+
+        [DllImport("MyGUIWrapper")]
+        private static extern void LayerManager_detachFromLayer(IntPtr layerManager, IntPtr item);
+
+        [DllImport("MyGUIWrapper")]
+        private static extern void LayerManager_upLayerItem(IntPtr layerManager, IntPtr item);
+
+        [DllImport("MyGUIWrapper")]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern bool LayerManager_load(IntPtr layerManager, String file);
+
+        [DllImport("MyGUIWrapper")]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern bool LayerManager_isExist(IntPtr layerManager, String name);
+
+        [DllImport("MyGUIWrapper")]
+        private static extern IntPtr LayerManager_getWidgetFromPoint(IntPtr layerManager, int left, int top);
+#endregion
+    }
+}
---
+++ MyGUIWrapper/Src/LayerManager.cpp
@@ -0,0 +1,36 @@
+#include "Stdafx.h"
+
+extern "C" _AnomalousExport MyGUI::LayerManager* LayerManager_getSingletonPtr()
+{
+	return MyGUI::LayerManager::getInstancePtr();
+}
+
+extern "C" _AnomalousExport void LayerManager_attachToLayerNode(MyGUI::LayerManager* layerManager, String name, MyGUI::Widget* item)
+{
+	layerManager->attachToLayerNode(name, item);
+}
+
+extern "C" _AnomalousExport void LayerManager_detachFromLayer(MyGUI::LayerManager* layerManager, MyGUI::Widget* item)
+{
+	layerManager->detachFromLayer(item);
+}
+
+extern "C" _AnomalousExport void LayerManager_upLayerItem(MyGUI::LayerManager* layerManager, MyGUI::Widget* item)
+{
+	layerManager->upLayerItem(item);
+}
+
+extern "C" _AnomalousExport bool LayerManager_load(MyGUI::LayerManager* layerManager, String file)
+{
+	return layerManager->load(file);
+}
+
+extern "C" _AnomalousExport bool LayerManager_isExist(MyGUI::LayerManager* layerManager, String name)
+{
+	return layerManager->isExist(name);
+}
+
+extern "C" _AnomalousExport MyGUI::Widget* LayerManager_getWidgetFromPoint(MyGUI::LayerManager* layerManager, int left, int top)
+{
+	return layerManager->getWidgetFromPoint(left, top);
+}
