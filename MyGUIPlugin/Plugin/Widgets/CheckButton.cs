99e41b0c53063f2339546c0f109bc117b2276033
Andrew Piper <threax@users.noreply.github.com>
2010-06-30 14:09:06 +0000

    Added a check button helper class.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@750 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Widgets/CheckButton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGUIPlugin
+{
+    /// <summary>
+    /// Helper class to make a button a check button.
+    /// </summary>
+    public class CheckButton
+    {
+        private Button button;
+        public event MyGUIEvent CheckedChanged;
+
+        public CheckButton(Button button)
+        {
+            this.button = button;
+            button.MouseButtonClick += new MyGUIEvent(button_MouseButtonClick);
+        }
+
+        void button_MouseButtonClick(Widget source, EventArgs e)
+        {
+            button.StateCheck = !button.StateCheck;
+            if (CheckedChanged != null)
+            {
+                CheckedChanged.Invoke(button, EventArgs.Empty);
+            }
+        }
+
+        public bool Checked
+        {
+            get
+            {
+                return button.StateCheck;
+            }
+            set
+            {
+                if (Checked != value)
+                {
+                    button.StateCheck = value;
+                    if (CheckedChanged != null)
+                    {
+                        CheckedChanged.Invoke(button, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public Button Button
+        {
+            get
+            {
+                return button;
+            }
+        }
+    }
+}
