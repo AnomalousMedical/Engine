4ea18d23500c8a95623295a388050c801fd30391
Andrew Piper <threax@users.noreply.github.com>
2010-07-14 14:57:04 +0000

    Added a MessageBox class that simplifies the use of Message.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@789 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Widgets/MessageBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGUIPlugin
+{
+    /// <summary>
+    /// This class wraps the Message class and makes using them a bit easier.
+    /// </summary>
+    public class MessageBox
+    {
+        public delegate void MessageClosedDelegate(MessageBoxStyle result);
+
+        private static Dictionary<Message, MessageClosedDelegate> delegates = new Dictionary<Message, MessageClosedDelegate>();
+
+        private MessageBox()
+        {
+
+        }
+
+        /// <summary>
+        /// This function will show a message box with the given options and call callback when it is closed.
+        /// </summary>
+        /// <param name="message">The message to go in the box.</param>
+        /// <param name="caption">The caption of the message box.</param>
+        /// <param name="style">The style to use for the message box.</param>
+        /// <param name="callback">The callback to call when the box is closed.</param>
+        public static void show(String message, String caption, MessageBoxStyle style, MessageClosedDelegate callback)
+        {
+            Message box = Message.createMessageBox("Message", caption, message, style);
+            box.MessageBoxResult += box_MessageBoxResult;
+            delegates.Add(box, callback);
+        }
+
+        static void box_MessageBoxResult(Widget source, EventArgs e)
+        {
+            Message srcMsg = (Message)source;
+            delegates[srcMsg](((MessageEventArgs)e).Result);
+            delegates.Remove(srcMsg);
+        }
+    }
+}
