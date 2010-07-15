eba0dc895fb79a4ed7f730e0e82ad9c1560e0d04
Andrew Piper <threax@users.noreply.github.com>
2010-07-15 17:46:20 +0000

    Added a function that takes no callback.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@796 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Plugin/Widgets/MessageBox.cs
+++ MyGUIPlugin/Plugin/Widgets/MessageBox.cs
@@ -25,6 +25,17 @@ namespace MyGUIPlugin
         /// <param name="message">The message to go in the box.</param>
         /// <param name="caption">The caption of the message box.</param>
         /// <param name="style">The style to use for the message box.</param>
+        public static void show(String message, String caption, MessageBoxStyle style)
+        {
+            show(message, caption, style, null);
+        }
+
+        /// <summary>
+        /// This function will show a message box with the given options and call callback when it is closed.
+        /// </summary>
+        /// <param name="message">The message to go in the box.</param>
+        /// <param name="caption">The caption of the message box.</param>
+        /// <param name="style">The style to use for the message box.</param>
         /// <param name="callback">The callback to call when the box is closed.</param>
         public static void show(String message, String caption, MessageBoxStyle style, MessageClosedDelegate callback)
         {
