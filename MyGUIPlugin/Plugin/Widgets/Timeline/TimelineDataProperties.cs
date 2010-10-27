0216c6da06cab79b44c0bfb9ab443de8cf45e57d
Andrew Piper <threax@users.noreply.github.com>
2010-10-27 13:01:56 +0000

    Added the timeline data stuff to the MyGUIPlugin.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@867 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Widgets/Timeline/TimelineDataProperties.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+
+namespace MyGUIPlugin
+{
+    public class TimelineDataProperties
+    {
+        private ScrollView propScroll;
+        private TimelineData data;
+
+        private NumericEdit startTime;
+        private NumericEdit duration;
+
+        private Dictionary<String, TimelineDataPanel> additionalProperties = new Dictionary<String, TimelineDataPanel>();
+        private Vector2 additionalPropertiesPosition;
+        private TimelineDataPanel currentPanel;
+
+        public TimelineDataProperties(ScrollView propScroll)
+        {
+            this.propScroll = propScroll;
+
+            startTime = new NumericEdit(propScroll.findWidget("StartTime") as Edit);
+            startTime.ValueChanged += new MyGUIEvent(startTime_ValueChanged);
+            startTime.MaxValue = float.MaxValue;
+            duration = new NumericEdit(propScroll.findWidget("Duration") as Edit);
+            duration.ValueChanged += new MyGUIEvent(duration_ValueChanged);
+            duration.MaxValue = float.MaxValue;
+
+            additionalPropertiesPosition = new Vector2(1, duration.Edit.Bottom + 2);
+        }
+
+        public void addPanel(String track, TimelineDataPanel panel)
+        {
+            panel.setPosition((int)additionalPropertiesPosition.x, (int)additionalPropertiesPosition.y);
+            additionalProperties.Add(track, panel);
+        }
+
+        public TimelineData CurrentData
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                Size2 canvasSize = propScroll.CanvasSize;
+                canvasSize.Height = additionalPropertiesPosition.y;
+                if (currentPanel != null)
+                {
+                    currentPanel.Visible = false;
+                }
+                data = value;
+                if (data != null)
+                {
+                    startTime.FloatValue = data.StartTime;
+                    duration.FloatValue = data.Duration;
+                    additionalProperties.TryGetValue(data.Track, out currentPanel);
+                    if (currentPanel != null)
+                    {
+                        currentPanel.Visible = true;
+                        currentPanel.setCurrentData(value);
+                        canvasSize.Height = currentPanel.Bottom;
+                    }
+                }
+                else
+                {
+                    startTime.FloatValue = 0.0f;
+                    duration.FloatValue = 0.0f;
+                    currentPanel = null;
+                }
+                propScroll.CanvasSize = canvasSize;
+            }
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return propScroll.Visible;
+            }
+            set
+            {
+                propScroll.Visible = value;
+            }
+        }
+
+        void duration_ValueChanged(Widget source, EventArgs e)
+        {
+            if (data != null)
+            {
+                data.Duration = duration.FloatValue;
+            }
+        }
+
+        void startTime_ValueChanged(Widget source, EventArgs e)
+        {
+            if (data != null)
+            {
+                data.StartTime = startTime.FloatValue;
+            }
+        }
+    }
+}
