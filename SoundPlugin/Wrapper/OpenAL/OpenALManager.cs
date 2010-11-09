cf9d4c491c62abb9dbf3a73d398cff6364d377b1
Andrew Piper <threax@users.noreply.github.com>
2010-11-09 20:22:57 +0000

    Added SoundPlugin project.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@887 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ SoundPlugin/Wrapper/OpenAL/OpenALManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace SoundPlugin
+{
+    public class OpenALManager : IDisposable
+    {
+        private IntPtr openALManager;
+
+        internal IntPtr Pointer
+        {
+            get
+            {
+                return openALManager;
+            }
+        }
+
+        public OpenALManager()
+        {
+            openALManager = OpenALManager_create();
+        }
+
+        public void Dispose()
+        {
+            OpenALManager_destroy(openALManager);
+        }
+
+        //public Sound createMemorySound(Stream stream)
+        //{
+
+        //}
+
+        #region PInvoke
+
+        [DllImport("SoundWrapper")]
+        private static extern IntPtr OpenALManager_create();
+
+        [DllImport("SoundWrapper")]
+        private static extern void OpenALManager_destroy(IntPtr openALManager);
+
+        [DllImport("SoundWrapper")]
+        private static extern IntPtr OpenALManager_createMemorySound(IntPtr openALManager, IntPtr stream);
+
+        [DllImport("SoundWrapper")]
+        private static extern IntPtr OpenALManager_createStreamingSound(IntPtr openALManager, IntPtr stream);
+
+        [DllImport("SoundWrapper")]
+        private static extern IntPtr OpenALManager_createStreamingSound2(IntPtr openALManager, IntPtr stream, int bufferSize, int numBuffers);
+
+        [DllImport("SoundWrapper")]
+        private static extern void OpenALManager_destroySound(IntPtr openALManager, IntPtr sound);
+
+        #endregion
+    }
+}
---
+++ SoundPlugin/Wrapper/Sound/Sound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoundPlugin
+{
+    public class Sound
+    {
+        public bool Repeat { get; set; }
+    }
+}
