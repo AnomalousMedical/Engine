23f61df6f2aa066c763b184447ffe784dc46911a
Andrew Piper <threax@users.noreply.github.com>
2010-11-15 15:56:45 +0000

    Added SoundPlugin interface classes.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@906 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ SoundPlugin/Plugin/SoundPluginInterface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+using Engine.Platform;
+
+namespace SoundPlugin
+{
+    public class SoundPluginInterface : PluginInterface
+    {
+        private OpenALManager openALManager = null;
+        private SoundUpdateListener soundUpdate;
+        private UpdateTimer mainTimer;
+
+        public SoundPluginInterface()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            if (openALManager != null)
+            {
+                openALManager.Dispose();
+                openALManager = null;
+            }
+        }
+
+        public void initialize(PluginManager pluginManager)
+        {
+            openALManager = new OpenALManager();
+            soundUpdate = new SoundUpdateListener(openALManager);
+        }
+
+        public void setPlatformInfo(UpdateTimer mainTimer, EventManager eventManager)
+        {
+            this.mainTimer = mainTimer;
+            mainTimer.addFixedUpdateListener(soundUpdate);
+        }
+
+        public string getName()
+        {
+            return "SoundPlugin";
+        }
+
+        public DebugInterface getDebugInterface()
+        {
+            return null;
+        }
+
+        public void createDebugCommands(List<CommandManager> commands)
+        {
+            
+        }
+    }
+}
---
+++ SoundPlugin/Plugin/SoundUpdateListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.Platform;
+
+namespace SoundPlugin
+{
+    class SoundUpdateListener : UpdateListener
+    {
+        private OpenALManager openALManager;
+
+        public SoundUpdateListener(OpenALManager openALManager)
+        {
+            this.openALManager = openALManager;
+        }
+
+        public void sendUpdate(Clock clock)
+        {
+            openALManager.update();
+        }
+
+        public void loopStarting()
+        {
+            
+        }
+
+        public void exceededMaxDelta()
+        {
+            
+        }
+    }
+}
