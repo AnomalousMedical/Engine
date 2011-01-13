c0f81b1aca2e527c5502b825a90c68fc8d215b92
Andrew Piper <threax@users.noreply.github.com>
2011-01-13 16:27:08 +0000

    Added a Tooltip and TooltipManager that simplify showing tooltips for various widgets.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@947 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Tooltip/Tooltip.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+
+namespace MyGUIPlugin
+{
+    class Tooltip : IDisposable
+    {
+        private StaticText tooltipWidget;
+
+        public Tooltip(String text, int x, int y)
+        {
+            tooltipWidget = Gui.Instance.createWidgetT("StaticText", "Tooltip", x, y, 1, 1, Align.Default, "Overlapped", "") as StaticText;
+            tooltipWidget.Caption = text;
+            Size2 textSize = tooltipWidget.getTextSize();
+            tooltipWidget.setSize((int)textSize.Width + 6, (int)textSize.Height + 4);
+            tooltipWidget.Visible = false;
+            int viewHeight = Gui.Instance.getViewHeight();
+            if (tooltipWidget.Bottom > viewHeight)
+            {
+                viewHeight -= tooltipWidget.Bottom;
+                tooltipWidget.setPosition(tooltipWidget.Left, tooltipWidget.Top + viewHeight);
+            }
+            int viewWidth = Gui.Instance.getViewWidth();
+            if (tooltipWidget.Right > viewWidth)
+            {
+                viewWidth -= tooltipWidget.Right;
+                tooltipWidget.setPosition(tooltipWidget.Left + viewWidth, tooltipWidget.Top);
+            }
+        }
+
+        public void Dispose()
+        {
+            Gui.Instance.destroyWidget(tooltipWidget);
+            tooltipWidget = null;
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return tooltipWidget.Visible;
+            }
+            set
+            {
+                tooltipWidget.Visible = value;
+            }
+        }
+    }
+}
---
+++ MyGUIPlugin/Plugin/Tooltip/TooltipManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+
+namespace MyGUIPlugin
+{
+    public class TooltipManager
+    {
+        private static TooltipManager instance;
+
+        public static TooltipManager Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new TooltipManager();
+                }
+                return instance;
+            }
+        }
+
+        private Dictionary<Widget, Tooltip> tooltips = new Dictionary<Widget, Tooltip>();
+
+        protected TooltipManager()
+        {
+
+        }
+
+        public void processTooltip(Widget hostWidget, String text, ToolTipEventArgs eventArgs)
+        {
+            if (eventArgs.Type == ToolTipType.Show)
+            {
+                showTooltip(hostWidget, text, eventArgs.Point);
+            }
+            else
+            {
+                destroyTooltip(hostWidget);
+            }
+        }
+
+        public void showTooltip(Widget hostWidget, String text, Vector2 location)
+        {
+            Tooltip tooltip = new Tooltip(text, (int)location.x, (int)location.y + 25);
+            tooltip.Visible = true;
+            tooltips.Add(hostWidget, tooltip);
+        }
+
+        public void destroyTooltip(Widget hostWidget)
+        {
+            Tooltip tooltip;
+            if (tooltips.TryGetValue(hostWidget, out tooltip))
+            {
+                tooltip.Dispose();
+                tooltips.Remove(hostWidget);
+            }
+        }
+    }
+}
