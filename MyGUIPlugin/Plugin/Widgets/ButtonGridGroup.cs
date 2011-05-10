75b6914cf9baba99166c82a20b7a09c2d3a8b2a9
Andrew Piper <threax@users.noreply.github.com>
2011-05-10 20:51:31 +0000

    Moved ButtonGrid classes into their own files.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1041 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Widgets/ButtonGridGroup.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+
+namespace MyGUIPlugin
+{
+    internal class ButtonGridGroup
+    {
+        private ButtonGrid grid;
+        private List<ButtonGridItem> items = new List<ButtonGridItem>();
+        private StaticText captionText;
+        private Widget separator;
+
+        public ButtonGridGroup(String name, ButtonGrid grid)
+        {
+            this.grid = grid;
+            this.Name = name;
+
+            if (grid.ShowGroupCaptions)
+            {
+                captionText = grid.ScrollView.createWidgetT("StaticText", grid.GroupCaptionSkin, 0, 0, 10, 10, Align.Left | Align.Top, "") as StaticText;
+                captionText.Font = grid.GroupCaptionFont;
+                captionText.Caption = name;
+                captionText.setSize((int)captionText.getTextSize().Width + 5, (int)captionText.FontHeight);
+
+                separator = grid.ScrollView.createWidgetT("Widget", grid.GroupSeparatorSkin, 0, 0, 10, 1, Align.Left | Align.Top, "");
+                separator.setSize((int)(grid.ScrollView.CanvasSize.Width - captionText.Width) - 10, 1);
+            }
+        }
+
+        public ButtonGridItem addItem(String caption)
+        {
+            ButtonGridItem item = new ButtonGridItem(this, grid);
+            item.Caption = caption;
+            items.Add(item);
+            return item;
+        }
+
+        public void removeItem(ButtonGridItem item)
+        {
+            items.Remove(item);
+            item.Dispose();
+        }
+
+        public void clear()
+        {
+            foreach (ButtonGridItem item in items)
+            {
+                item.Dispose();
+            }
+            items.Clear();
+        }
+
+        /// <summary>
+        /// Layout the items in the group. Returns the position the next group should start in.
+        /// </summary>
+        /// <param name="startPosition">The start position</param>
+        /// <returns>The position for the next group to start at.</returns>
+        public Vector2 layout(Vector2 startPosition, IComparer<ButtonGridItem> itemComparer)
+        {
+            Vector2 currentPosition = startPosition;
+
+            if (grid.ShowGroupCaptions)
+            {
+                currentPosition.y += 5;
+                captionText.setPosition((int)currentPosition.x, (int)currentPosition.y);
+                separator.setPosition((int)(currentPosition.x + captionText.Width), (int)(currentPosition.y + captionText.Height / 2));
+                separator.setSize((int)(grid.ScrollView.CanvasSize.Width - captionText.Width) - 10, 1);
+                currentPosition.y += captionText.Height + 5;
+            }
+
+            List<ButtonGridItem> sortedItems = items;
+            if (itemComparer != null)
+            {
+                sortedItems = new List<ButtonGridItem>(items);
+                sortedItems.Sort(itemComparer);
+            }
+
+            foreach (ButtonGridItem item in sortedItems)
+            {
+                if (currentPosition.x + item.Width > grid.ScrollView.CanvasSize.Width)
+                {
+                    currentPosition.x = 0;
+                    currentPosition.y += grid.ItemHeight + grid.ItemPaddingY;
+                }
+                item.setPosition(currentPosition);
+                currentPosition.x += item.Width + grid.ItemPaddingX;
+            }
+
+            currentPosition.x = 0;
+            currentPosition.y += grid.ItemHeight + grid.ItemPaddingY;
+
+            return currentPosition;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+
+        public String Name { get; private set; }
+
+        internal ButtonGrid Grid
+        {
+            get
+            {
+                return grid;
+            }
+        }
+
+        internal ButtonGridItem getItem(int index)
+        {
+            return items[index];
+        }
+
+        internal ButtonGridItem findItemByCaption(string caption)
+        {
+            foreach (ButtonGridItem item in items)
+            {
+                if (item.Caption == caption)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ MyGUIPlugin/Plugin/Widgets/ButtonGridItem.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+
+namespace MyGUIPlugin
+{
+    public class ButtonGridItem : IDisposable
+    {
+        private Button button;
+        private ButtonGrid grid;
+        public event EventHandler ItemClicked;
+
+        internal ButtonGridItem(ButtonGridGroup group, ButtonGrid list)
+        {
+            this.grid = list;
+            this.Group = group;
+            button = list.ScrollView.createWidgetT("Button", list.ButtonSkin, 0, 0, list.ItemWidth, list.ItemHeight, Align.Top | Align.Left, "") as Button;
+            button.MouseButtonClick += new MyGUIEvent(button_MouseButtonClick);
+            button.MouseButtonDoubleClick += new MyGUIEvent(button_MouseButtonDoubleClick);
+        }
+
+        void button_MouseButtonClick(Widget source, EventArgs e)
+        {
+            grid.SelectedItem = this;
+            if (ItemClicked != null)
+            {
+                ItemClicked.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        void button_MouseButtonDoubleClick(Widget source, EventArgs e)
+        {
+            grid.itemActivated(this);
+        }
+
+        public void Dispose()
+        {
+            Gui.Instance.destroyWidget(button);
+        }
+
+        public String Caption
+        {
+            get
+            {
+                return button.Caption;
+            }
+            set
+            {
+                button.Caption = value;
+            }
+        }
+
+        public Object UserObject { get; set; }
+
+        public ButtonGrid ButtonGrid
+        {
+            get
+            {
+                return Group.Grid;
+            }
+        }
+
+        internal int Width
+        {
+            get
+            {
+                return button.Width;
+            }
+        }
+
+        internal void setPosition(Vector2 position)
+        {
+            button.setPosition((int)position.x, (int)position.y);
+        }
+
+        internal ButtonGridGroup Group { get; private set; }
+
+        internal bool StateCheck
+        {
+            get
+            {
+                return button.StateCheck;
+            }
+            set
+            {
+                button.StateCheck = value;
+            }
+        }
+
+        public void setImage(String imageResource)
+        {
+            StaticImage image = button.StaticImage;
+            if (image != null)
+            {
+                image.setItemResource(imageResource);
+            }
+        }
+    }
+}
