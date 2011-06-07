f3e98bfc4fe3b6f2a89dbf2b7fb274d34cefee65
Andrew Piper <threax@users.noreply.github.com>
2011-06-07 17:58:42 +0000

    Removed debug printouts.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1081 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Plugin/Widgets/Tree/DefaultTreeNodeWidget.cs
+++ MyGUIPlugin/Plugin/Widgets/Tree/DefaultTreeNodeWidget.cs
@@ -23,8 +23,6 @@ namespace MyGUIPlugin
 
         public override void createWidget(Widget parent, String caption, String imageResource)
         {
-            Logging.Log.Debug("Creating widget node for {0}", caption);
-
             primaryWidget = parent.createWidgetT("Widget", "PanelEmpty", 0, 0, 26, 16, Align.Default, "") as Widget;
 
             plusMinusButton = primaryWidget.createWidgetT("Button", "ButtonMinusPlus", 0, 0, 16, 16, Align.Left | Align.HCenter, "") as Button;
@@ -44,8 +42,6 @@ namespace MyGUIPlugin
         {
             if (primaryWidget != null)
             {
-                Logging.Log.Debug("Destroying widget node for {0}", mainButton.Caption);
-
                 Gui.Instance.destroyWidget(primaryWidget);
                 primaryWidget = null;
                 plusMinusButton = null;
