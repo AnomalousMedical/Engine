5bc512df16c368551606ea18dd11126225e76c2a
Andrew Piper <threax@users.noreply.github.com>
2011-06-30 20:19:42 +0000

    Added a class to have common numeric functions in it. It has lerp rightnow.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1114 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Math/Numerics/NumberFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public class NumberFunctions
+    {
+        private NumberFunctions() { }
+
+        public static float lerp(float start, float end, float t)
+        {
+            return start + (end - start) * t;
+        }
+    }
+}
