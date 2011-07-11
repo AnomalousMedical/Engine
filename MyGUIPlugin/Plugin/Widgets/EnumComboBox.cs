a7f5cbebf13714e5cd7b29df818768f5feb733be
Andrew Piper <threax@users.noreply.github.com>
2011-07-11 14:14:59 +0000

    Added a combo box that makes using enums easier.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1115 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Widgets/EnumComboBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace MyGUIPlugin
+{
+    public class EnumComboBox<EnumType>
+    {
+        private ComboBox comboBox;
+
+        public EnumComboBox(ComboBox comboBox)
+        {
+            this.comboBox = comboBox;
+            uint index = 0;
+            Type enumType = typeof(EnumType);
+            foreach (FieldInfo fieldInfo in enumType.GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                comboBox.addItem(fieldInfo.Name);
+                comboBox.setItemDataAt(index++, (int)fieldInfo.GetValue(enumType));
+            }
+        }
+
+        public EnumType Value
+        {
+            get
+            {
+                return (EnumType)comboBox.SelectedItemData;
+            }
+            set
+            {
+                comboBox.SelectedIndex = comboBox.findItemIndexWith(value.ToString());
+            }
+        }
+    }
+}
