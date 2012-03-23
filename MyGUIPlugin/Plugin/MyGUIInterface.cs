9a23fc6978abed1626a2f6cec65bbbc0b9277929
Andrew Piper <threax@users.noreply.github.com>
2012-03-23 15:38:22 +0000

    Added a separate file for core skin templates. Defined the EditBox template in the mygui 3.2 style using the old skin texture definitions. The skin now works correctly.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1243 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Plugin/MyGUIInterface.cs
+++ MyGUIPlugin/Plugin/MyGUIInterface.cs
@@ -110,6 +110,7 @@ namespace MyGUIPlugin
             resourceManager.load(Font);
             resourceManager.load(Resource);
             resourceManager.load(Skin);
+            resourceManager.load("MyGUIPlugin.Resources.core_skin_template.xml");
             resourceManager.load("MyGUIPlugin.Resources.custom_skin.xml");
             resourceManager.load(Pointer);
             resourceManager.load(Layer);
