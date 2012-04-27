f50fbbdd7a0027c920be330f3ff2f0d38ead284a
Andrew Piper <threax@users.noreply.github.com>
2012-04-27 15:18:04 +0000

    Added a getClosestLargerPow2 function to find a larger power of two number from an input.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1369 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Engine/Math/Numerics/NumberFunctions.cs
+++ Engine/Math/Numerics/NumberFunctions.cs
@@ -5,13 +5,40 @@ using System.Text;
 
 namespace Engine
 {
-    public class NumberFunctions
+    public static class NumberFunctions
     {
-        private NumberFunctions() { }
-
         public static float lerp(float start, float end, float t)
         {
             return start + (end - start) * t;
+        }
+        
+        /// <summary>
+        /// Figure out the power of two number that is closest to but larger
+        /// than the given number. So if the input is 1023 you will get 1024 if
+        /// it is 1025 you get 2048.
+        /// </summary>
+        /// <param name="actualNumber">The number to compute the closest value for</param>
+        /// <param name="pow2Start">A starting power of two number. Allows for less iterations if you know a close starting value. Still works even if this number is too big.</param>
+        /// <returns>The closest power of two number to acutalNumber that is bigger than actualNumber</returns>
+        public static int computeClosestLargerPow2(int actualNumber, int pow2Start)
+        {
+            int pow2Size = pow2Start;
+            if (actualNumber < pow2Start)
+            {
+                while (pow2Size > actualNumber)
+                {
+                    pow2Size >>= 1;
+                }
+                pow2Size <<= 1;
+            }
+            else
+            {
+                while (pow2Size < actualNumber)
+                {
+                    pow2Size <<= 1;
+                }
+            }
+            return pow2Size;
         }
     }
 }
