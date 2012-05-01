d7bcbc73523f8e3a82300204ec98b238ff45318c
Andrew Piper <threax@users.noreply.github.com>
2012-05-01 13:22:31 +0000

    Added a generic assembly resource loading class. Removed the demo file.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1382 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ libRocketPlugin/Plugin/RocketAssemblyResourceLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace libRocketPlugin
+{
+    public class RocketAssemblyResourceLoader : RocketFileSystemExtension
+    {
+        private Assembly assembly;
+
+        public RocketAssemblyResourceLoader(Assembly assembly)
+        {
+            this.assembly = assembly;
+        }
+
+        public bool canOpenFile(string file)
+        {
+            return assembly.GetManifestResourceInfo(file) != null;
+        }
+
+        public Stream openFile(string file)
+        {
+            return assembly.GetManifestResourceStream(file);
+        }
+    }
+}
