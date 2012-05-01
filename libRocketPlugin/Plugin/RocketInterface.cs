e5186a1853ba079c97991ad4db4fe7ceb8184ee6
Andrew Piper <threax@users.noreply.github.com>
2012-05-01 20:46:36 +0000

    Made a function to fix up the librocket paths since they are only allowed to use /.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1387 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- libRocketPlugin/Plugin/RocketInterface.cs
+++ libRocketPlugin/Plugin/RocketInterface.cs
@@ -29,6 +29,16 @@ namespace libRocketPlugin
             }
         }
 
+        /// <summary>
+        /// libRocket expects its paths to have only / in it so this will fix it up for you.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static String MakeSafePath(String path)
+        {
+            return path.Replace('\\', '/');
+        }
+
         private RocketInterface()
         {
             
