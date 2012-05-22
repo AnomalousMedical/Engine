336ef27ed56731e9c30508b6d9512eaeb605afaf
Andrew Piper <threax@users.noreply.github.com>
2012-05-22 21:11:45 +0000

    Added a FileUtility class that can create regex strings from file wildcards.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1396 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Utility/FileUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public static class FileUtility
+    {
+        public static String wildcardToRegex(String wildcard)
+        {
+            String expression = "^.*";
+            for (int i = 0; i < wildcard.Length; i++)
+            {
+                switch (wildcard[i])
+                {
+                    case '?':
+                        expression += '.'; // regular expression notation.
+                        //mIsWild = true;
+                        break;
+                    case '*':
+                        expression += ".*";
+                        //mIsWild = true;
+                        break;
+                    case '.':
+                        expression += "\\";
+                        expression += wildcard[i];
+                        break;
+                    case ';':
+                        expression += "|";
+                        //mIsWild = true;
+                        break;
+                    default:
+                        expression += wildcard[i];
+                        break;
+                }
+            }
+            return expression;
+        }
+    }
+}
