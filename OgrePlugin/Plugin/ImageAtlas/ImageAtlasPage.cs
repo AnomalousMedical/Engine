f12df622130b3e2c24d1e40069cc4aaf52794cd4
Andrew Piper <threax@users.noreply.github.com>
2012-05-31 20:17:51 +0000

    Fixed the imageatlaspage to make its texture correctly on the mac.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1412 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgrePlugin/Plugin/ImageAtlas/ImageAtlasPage.cs
+++ OgrePlugin/Plugin/ImageAtlas/ImageAtlasPage.cs
@@ -21,7 +21,7 @@ namespace OgrePlugin
         public ImageAtlasPage(String textureName, String groupName, int width, int height)
         {
             rootNode = new ImagePackTreeNode(new Size(width, height));
-            texture = TextureManager.getInstance().createManual(textureName, groupName, TextureType.TEX_TYPE_2D, (uint)width, (uint)height, 0, 0, OgreWrapper.PixelFormat.PF_A8R8G8B8, TextureUsage.TU_STATIC_WRITE_ONLY, false, 0);
+            texture = TextureManager.getInstance().createManual(textureName, groupName, TextureType.TEX_TYPE_2D, (uint)width, (uint)height, 1, 1, OgreWrapper.PixelFormat.PF_A8R8G8B8, TextureUsage.TU_STATIC_WRITE_ONLY, false, 0);
             bufferPtr = texture.Value.getBuffer();
             this.TextureName = textureName;
             this.GroupName = groupName;
