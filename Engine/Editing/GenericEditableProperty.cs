5b938da2e978c3f8f4319a34ad6b944dec76cfe4
Andrew Piper <threax@users.noreply.github.com>
2012-06-18 15:00:59 +0000

    Added an editable property that can easily hold any type of object. Good for custom edit interface implementations.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1434 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Editing/GenericEditableProperty.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Editing
+{
+    public class GenericEditableProperty<T> : EditableProperty
+    {
+        private T obj;
+        private String name;
+
+        public GenericEditableProperty(String name, T obj)
+        {
+            this.name = name;
+            this.obj = obj;
+        }
+
+        public bool canParseString(int column, string value, out string errorMessage)
+        {
+            errorMessage = null;
+            return false;
+        }
+
+        public Browser getBrowser(int column)
+        {
+            return null;
+        }
+
+        public Type getPropertyType(int column)
+        {
+            switch (column)
+            {
+                case 0:
+                    return typeof(String);
+                case 1:
+                    return typeof(T);
+            }
+            return null;
+        }
+
+        public object getRealValue(int column)
+        {
+            switch (column)
+            {
+                case 0:
+                    return name;
+                case 1:
+                    return obj;
+            }
+            return null;
+        }
+
+        public string getValue(int column)
+        {
+            switch (column)
+            {
+                case 0:
+                    return name;
+                case 1:
+                    if (obj != null)
+                    {
+                        return obj.ToString();
+                    }
+                    return null;
+            }
+            return null;
+        }
+
+        public bool hasBrowser(int column)
+        {
+            return false;
+        }
+
+        public void setValue(int column, object value)
+        {
+            switch (column)
+            {
+                case 0:
+                    name = (String)value;
+                    break;
+                case 1:
+                    value = (T)value;
+                    break;
+            }
+        }
+
+        public void setValueStr(int column, string value)
+        {
+            switch (column)
+            {
+                case 0:
+                    name = value;
+                    break;
+                case 1:
+                    //value = (T)value;
+                    break;
+            }
+        }
+    }
+}
