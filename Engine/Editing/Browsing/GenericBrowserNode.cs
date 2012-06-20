08638e5142ab0fe2391a2772d83e6fb3d6e4f874
Andrew Piper <threax@users.noreply.github.com>
2012-06-20 16:05:08 +0000

    Added a GenericBrowserNode so lambdas can be used in browser nodes.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1438 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Editing/Browsing/GenericBrowserNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Editing
+{
+    public class GenericBrowserNode<T> : BrowserNode
+    {
+        public GenericBrowserNode(String text, T value, String defaultName = null)
+            : base(text, value, defaultName)
+        {
+
+        }
+    }
+}
