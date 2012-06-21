da3a3b3203283191344de0804776383b29ace9b7
Andrew Piper <threax@users.noreply.github.com>
2012-06-21 22:26:03 +0000

    Added the OgreArchiveManager as a wrapped class. This way it archives can be forced to be unloaded since ogre wont do this on its own.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1444 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ OgrePlugin/Wrapper/Resources/OgreArchiveManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OgreWrapper
+{
+    public class OgreArchiveManager
+    {
+        static OgreArchiveManager instance = null;
+
+        public static OgreArchiveManager getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new OgreArchiveManager();
+            }
+            return instance;
+        }
+
+        private OgreArchiveManager()
+        {
+
+        }
+
+        public void unload(String filename)
+        {
+            ArchiveManager_unload(filename);
+        }
+
+        [DllImport("OgreCWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ArchiveManager_unload(String filename);
+    }
+}
