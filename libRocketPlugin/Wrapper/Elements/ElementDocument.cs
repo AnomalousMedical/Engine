506dac8fd450426673262f41094ea523a9da0cff
Andrew Piper <threax@users.noreply.github.com>
2012-06-22 15:36:39 +0000

    Wrapped most of element document.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1446 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- libRocketPlugin/Wrapper/Elements/ElementDocument.cs
+++ libRocketPlugin/Wrapper/Elements/ElementDocument.cs
@@ -20,16 +20,114 @@ namespace libRocketPlugin
         {
 
         }
-
-        public void Show()
+        
+        public void PullToFront()
         {
-            ElementDocument_Show(ptr);
+            ElementDocument_PullToFront(ptr);
+        }
+        
+        public void PushToBack()
+        {
+            ElementDocument_PushToBack(ptr);
+        }
+
+        public void Show(FocusFlags focusFlags = FocusFlags.FOCUS)
+        {
+            ElementDocument_Show(ptr, focusFlags);
+        }
+
+        public void Hide()
+        {
+            ElementDocument_Hide(ptr);
+        }
+
+        public void Close()
+        {
+            ElementDocument_Close(ptr);
+        }
+
+        public Element CreateElement(String name)
+        {
+            return ElementManager.getElement(ElementDocument_CreateElement(ptr, name));
+        }
+
+        public Element/*Should be ElementText (not wrapped yet)*/ CreateTextNode(String text)
+        {
+            return ElementManager.getElement(ElementDocument_CreateTextNode(ptr, text));
+        }
+
+        public void UpdatePosition()
+        {
+            ElementDocument_UpdatePosition(ptr);
+        }
+
+        //properties
+        public String Title
+        {
+            get
+            {
+                return Marshal.PtrToStringAnsi(ElementDocument_GetTitle(ptr));
+            }
+            set
+            {
+                ElementDocument_SetTitle(ptr, value);
+            }
+        }
+
+        public String SourceURL
+        {
+            get
+            {
+                return Marshal.PtrToStringAnsi(ElementDocument_GetSourceURL(ptr));
+            }
+        }
+
+        public bool IsModal
+        {
+            get
+            {
+                return ElementDocument_IsModal(ptr);
+            }
         }
 
         #region PInvoke
+        
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ElementDocument_SetTitle(IntPtr elementDocument, String title);
+        
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr ElementDocument_GetTitle(IntPtr elementDocument);
+        
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr ElementDocument_GetSourceURL(IntPtr elementDocument);
+        
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ElementDocument_PullToFront(IntPtr elementDocument);
+        
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ElementDocument_PushToBack(IntPtr elementDocument);
 
         [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
-        private static extern void ElementDocument_Show(IntPtr elementDocument);
+        private static extern void ElementDocument_Show(IntPtr elementDocument, FocusFlags focusFlags);
+
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ElementDocument_Hide(IntPtr elementDocument);
+
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ElementDocument_Close(IntPtr elementDocument);
+
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr ElementDocument_CreateElement(IntPtr elementDocument, String name);
+
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr ElementDocument_CreateTextNode(IntPtr elementDocument, String text);
+
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern bool ElementDocument_IsModal(IntPtr elementDocument);
+
+        [DllImport("libRocketWrapper", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ElementDocument_UpdatePosition(IntPtr elementDocument);
 
         #endregion
     }
--- libRocketWrapper/Src/ElementDocument.cpp
+++ libRocketWrapper/Src/ElementDocument.cpp
@@ -1,6 +1,65 @@
 #include "StdAfx.h"
 
-extern "C" _AnomalousExport void ElementDocument_Show(Rocket::Core::ElementDocument* elementDocument)
+//void ProcessHeader(const DocumentHeader* header);
+
+extern "C" _AnomalousExport void ElementDocument_SetTitle(Rocket::Core::ElementDocument* elementDocument, String title)
 {
-	elementDocument->Show();
+	elementDocument->SetTitle(Rocket::Core::String(title));
+}
+	
+extern "C" _AnomalousExport String ElementDocument_GetTitle(Rocket::Core::ElementDocument* elementDocument)
+{
+	return elementDocument->GetTitle().CString();
+}
+
+extern "C" _AnomalousExport String ElementDocument_GetSourceURL(Rocket::Core::ElementDocument* elementDocument)
+{
+	return elementDocument->GetSourceURL().CString();
+}
+
+//void SetStyleSheet(StyleSheet* style_sheet);
+
+extern "C" _AnomalousExport void ElementDocument_PullToFront(Rocket::Core::ElementDocument* elementDocument)
+{
+	elementDocument->PullToFront();
+}
+	
+extern "C" _AnomalousExport void ElementDocument_PushToBack(Rocket::Core::ElementDocument* elementDocument)
+{
+	elementDocument->PushToBack();
+}
+
+extern "C" _AnomalousExport void ElementDocument_Show(Rocket::Core::ElementDocument* elementDocument, Rocket::Core::ElementDocument::FocusFlags focusFlags)
+{
+	elementDocument->Show(focusFlags);
+}
+
+extern "C" _AnomalousExport void ElementDocument_Hide(Rocket::Core::ElementDocument* elementDocument)
+{
+	elementDocument->Hide();
+}
+
+extern "C" _AnomalousExport void ElementDocument_Close(Rocket::Core::ElementDocument* elementDocument)
+{
+	elementDocument->Close();
+}
+
+extern "C" _AnomalousExport Rocket::Core::Element* ElementDocument_CreateElement(Rocket::Core::ElementDocument* elementDocument, String name)
+{
+	return elementDocument->CreateElement(name);
+}
+	
+extern "C" _AnomalousExport Rocket::Core::ElementText* ElementDocument_CreateTextNode(Rocket::Core::ElementDocument* elementDocument, String text)
+{
+	return elementDocument->CreateTextNode(text);
+}
+
+extern "C" _AnomalousExport bool ElementDocument_IsModal(Rocket::Core::ElementDocument* elementDocument)
+{
+	return elementDocument->IsModal();
+}
+
+extern "C" _AnomalousExport void ElementDocument_UpdatePosition(Rocket::Core::ElementDocument* elementDocument)
+{
+	elementDocument->UpdatePosition();
 }
