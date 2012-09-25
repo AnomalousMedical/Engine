67cf06fbdde92be883aed2e8e971296eea8e8e1d
Andrew Piper <threax@users.noreply.github.com>
2012-09-25 14:35:01 +0000

    Made it so the Anomaly resource editor can handle relative paths for its resource root in the engine config.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1491 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Anomaly/Config/ResourceSection.cs
+++ Anomaly/Config/ResourceSection.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Engine;
+using System.IO;
 
 namespace Anomaly
 {
     public class ResourceSection
     {
         private ConfigSection resourceSection;
+        private String relativeRootDirectory;
 
         public ResourceSection(ConfigFile configFile)
         {
+            relativeRootDirectory = Path.GetDirectoryName(configFile.BackingFile);
             resourceSection = configFile.createOrRetrieveConfigSection("Resources");
         }
 
@@ -19,7 +22,13 @@ namespace Anomaly
         {
             get
             {
-                return resourceSection.getValue("Root", ".");
+                String resourceRoot = resourceSection.getValue("Root", ".");
+                if (!Path.IsPathRooted(resourceRoot))
+                {
+                    resourceRoot = Path.Combine(relativeRootDirectory, resourceRoot);
+                    resourceRoot = Path.GetFullPath(resourceRoot);
+                }
+                return resourceRoot;
             }
             set
             {
