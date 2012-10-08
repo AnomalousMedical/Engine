921bd1fa6ccad97e86f4d1588dfb1386fa31cc77
Andrew Piper <threax@users.noreply.github.com>
2012-10-08 20:04:44 +0000

    Made a glsl ColorVertex version.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1496 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ OgrePlugin/Resources/glsl/ColorVertexFPAlpha.glsl
@@ -0,0 +1,12 @@
+uniform vec4 alpha;
+
+varying vec4 passColor;
+
+//----------------------------------
+//Measurement Fragment Program
+//----------------------------------
+void main(void)
+{
+	gl_FragColor = passColor;
+	gl_FragColor.a = alpha.a;
+}
