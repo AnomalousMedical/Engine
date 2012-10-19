64a00943231fadb91f9a658c34316eb106391494
Andrew Piper <threax@users.noreply.github.com>
2012-10-19 18:52:33 +0000

    Added the ability to get min and max values from the performance counter timelapse.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1508 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Engine/Performance/Timelapse.cs
+++ Engine/Performance/Timelapse.cs
@@ -7,9 +7,18 @@ namespace Engine.Performance
 {
     public class Timelapse
     {
+        private Int64 min = Int64.MaxValue;
+        private Int64 max = 0;
+
         public Timelapse(String name)
         {
             this.Name = name;
+        }
+
+        public void resetMinMax()
+        {
+            min = Int64.MaxValue;
+            max = 0;
         }
 
         public String Name { get; private set; }
@@ -23,6 +32,32 @@ namespace Engine.Performance
             get
             {
                 return EndTime - StartTime;
+            }
+        }
+
+        public Int64 Min
+        {
+            get
+            {
+                Int64 duration = Duration;
+                if (duration < min)
+                {
+                    min = duration;
+                }
+                return min;
+            }
+        }
+
+        public Int64 Max
+        {
+            get
+            {
+                Int64 duration = Duration;
+                if (duration > max)
+                {
+                    max = duration;
+                }
+                return max;
             }
         }
     }
