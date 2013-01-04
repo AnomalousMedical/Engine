b8b31605b96b683420b6ee15443e8f5aa3ff9af1
Andrew Piper <threax@users.noreply.github.com>
2013-01-04 13:40:25 +0000

    Added an easy way to get to mygui common icons.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1530 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/MyGUIResourceNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGUIPlugin
+{
+    public static class MyGUIResourceNames
+    {
+        public const String InfoIcon = "MyGUIIcons/Info";
+        public const String QuestionIcon = "MyGUIIcons/Quest";
+        public const String ErrorIcon = "MyGUIIcons/Error";
+        public const String WarningIcon = "MyGUIIcons/Warning";
+    }
+}
