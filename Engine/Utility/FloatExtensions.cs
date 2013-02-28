1ec1310ff774f8e5c0b61e14f7eecd272c5999f6
Andrew Piper <threax@users.noreply.github.com>
2013-02-28 16:09:14 +0000

    Added FloatExtensions that provide an easy way to do epsilon comparisons.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1569 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Utility/FloatExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public static class FloatExtensions
+    {
+        public static bool EpsilonEquals(this float number, float test, float epsilon = float.Epsilon)
+        {
+            return number < test + epsilon && number > test - epsilon;
+        }
+    }
+}
