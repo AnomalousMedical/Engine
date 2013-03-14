848df9a10221d3a7a96b0fca45757a5343abcc77
Andrew Piper <threax@users.noreply.github.com>
2013-03-14 15:12:25 +0000

    Added a class to get to message box icon names easily.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1571 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Resources/MessageBox/MessageBoxIcons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGUIPlugin
+{
+    public static class MessageBoxIcons
+    {
+        public const String Info = "MyGUIIcons/Info";
+        public const String Quest = "MyGUIIcons/Quest";
+        public const String Error = "MyGUIIcons/Error";
+        public const String Warning = "MyGUIIcons/Warning";
+    }
+}
