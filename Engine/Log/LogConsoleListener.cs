aa1a7daf8294d6a024cd604d360d180659a34988
Andrew Piper <threax@users.noreply.github.com>
2013-07-09 15:29:47 +0000

    Added a class that prints log messages to the console.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1579 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Log/LogConsoleListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logging
+{
+    public class LogConsoleListener : LogListener
+    {
+        public void sendMessage(string message, LogLevel logLevel, string subsystem)
+        {
+            Console.WriteLine(Log.formatMessage(message, subsystem));
+        }
+    }
+}
