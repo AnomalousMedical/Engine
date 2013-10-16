72ac2f3f4012146468a8cca3f662808e068e33b5
Andrew Piper <threax@users.noreply.github.com>
2013-10-16 16:04:19 +0000

    Added a class that enumerates a list and allows items to be removed.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1687 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Utility/RemovableListIterator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public class RemovableListIterator<T>
+    {
+        private int enumerationLength = -1;
+        private int enumerationPosition = -1;
+        private List<T> list;
+
+        public RemovableListIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public IEnumerable<T> Values
+        {
+            get
+            {
+                return enumerate();
+            }
+        }
+
+        public void RemoveCurrent()
+        {
+            list.RemoveAt(enumerationPosition);
+            --enumerationPosition;
+            --enumerationLength;
+        }
+
+        private IEnumerable<T> enumerate()
+        {
+            enumerationLength = list.Count;
+            for (enumerationPosition = 0; enumerationPosition < enumerationLength; ++enumerationPosition)
+            {
+                yield return list[enumerationPosition];
+            }
+            enumerationLength = -1;
+            enumerationPosition = -1;
+        }
+    }
+}
