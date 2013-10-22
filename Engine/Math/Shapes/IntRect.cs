ff378096d839408ad7dc5d877d9e3e4dd1429f11
Andrew Piper <threax@users.noreply.github.com>
2013-10-22 13:24:49 +0000

    Added an integer rectangle.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1695 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Math/Shapes/IntRect.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Engine
+{
+    [StructLayout(LayoutKind.Explicit, Size = 16)]
+    public struct IntRect
+    {
+        [FieldOffset(0)]
+        private int left;
+        [FieldOffset(4)]
+        private int top;
+        [FieldOffset(8)]
+        private int width;
+        [FieldOffset(12)]
+        private int height;
+
+        public IntRect(int left, int top, int width, int height)
+        {
+            this.left = left;
+            this.top = top;
+            this.width = width;
+            this.height = height;
+        }
+
+        public void setValues(int left, int top, int width, int height)
+        {
+            this.left = left;
+            this.top = top;
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+
+        public int Top
+        {
+            get
+            {
+                return top;
+            }
+            set
+            {
+                top = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+
+        private static readonly char[] SEPS = { ',' };
+
+        public void fromString(String str)
+        {
+            String[] subStrs = str.Split(SEPS);
+            if (subStrs.Length == 4)
+            {
+                NumberParser.TryParse(subStrs[0], out left);
+                NumberParser.TryParse(subStrs[1], out top);
+                NumberParser.TryParse(subStrs[2], out width);
+                NumberParser.TryParse(subStrs[3], out height);
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}, {3}", left, top, width, height);
+        }
+    }
+}
