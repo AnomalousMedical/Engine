833e96ca170dd4046e9745caababea30942245e6
Andrew Piper <threax@users.noreply.github.com>
2014-01-15 17:17:09 +0000

    Added an object that can act as a go between to offer multiple choice options for any type of object.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1715 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Editing/ChoiceObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Editing
+{
+    /// <summary>
+    /// This class represents an value that can have choices presented.
+    /// </summary>
+    public class ChoiceObject
+    {
+        List<Pair<String, Object>> choices = new List<Pair<string, Object>>();
+
+        public ChoiceObject()
+        {
+
+        }
+
+        public ChoiceObject(IEnumerable<Pair<String, Object>> choices)
+        {
+            addChoices(choices);
+        }
+
+        public void addChoice(String display, Object value)
+        {
+            choices.Add(new Pair<string, object>(display, value));
+        }
+
+        public void addChoices(IEnumerable<Pair<String, Object>> choices)
+        {
+            this.choices.AddRange(choices);
+        }
+
+        public void removeChoice(Object value)
+        {
+            foreach (var choice in choices)
+            {
+                if (choice.Second == value)
+                {
+                    choices.Remove(choice);
+                    break;
+                }
+            }
+        }
+
+        public IEnumerable<Pair<String, Object>> Choices
+        {
+            get
+            {
+                return choices;
+            }
+        }
+    }
+}
