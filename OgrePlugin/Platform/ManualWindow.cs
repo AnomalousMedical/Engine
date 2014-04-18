1717c08ba50457ec196d55c2642f3937a104d118
Andrew Piper <threax@users.noreply.github.com>
2014-04-18 13:06:33 +0000

    Replaced TextureWindow with ManualWindow.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1806 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ OgrePlugin/Platform/ManualWindow.cs
@@ -0,0 +1,32 @@
+﻿using OgreWrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrePlugin
+{
+    public class ManualWindow : OgreWindow
+    {
+        private RenderTarget renderTarget;
+
+        public ManualWindow(RenderTarget renderTarget)
+        {
+            this.renderTarget = renderTarget;
+        }
+
+        public override void Dispose()
+        {
+            
+        }
+
+        public override RenderTarget OgreRenderTarget
+        {
+            get
+            {
+                return renderTarget;
+            }
+        }
+    }
+}
