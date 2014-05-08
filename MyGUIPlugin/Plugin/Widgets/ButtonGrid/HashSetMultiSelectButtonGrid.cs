44e12ac144b8cbdde5df74724b5acd3a1f23651f
Andrew Piper <threax@users.noreply.github.com>
2014-05-08 14:34:35 +0000

    Added a buttongrid that uses a hash set to back multi select.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@1819 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ MyGUIPlugin/Plugin/Widgets/ButtonGrid/HashSetMultiSelectButtonGrid.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGUIPlugin
+{
+    /// <summary>
+    /// This class provides a nice interface to a button grid with a single item able to be seleted at a time.
+    /// </summary>
+    public class HashSetMultiSelectButtonGrid : ButtonGrid
+    {
+        private HashSetMultiSelectionStrategy multiSelection = new HashSetMultiSelectionStrategy();
+
+        public event EventHandler SelectedValueChanged
+        {
+            add
+            {
+                multiSelection.SelectedValueChanged += value;
+            }
+            remove
+            {
+                multiSelection.SelectedValueChanged -= value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="scrollView"></param>
+        public HashSetMultiSelectButtonGrid(ScrollView scrollView)
+            : this(scrollView, new ButtonGridGridLayout(), null, null)
+        {
+
+        }
+
+        public HashSetMultiSelectButtonGrid(ScrollView scrollView, IComparer<ButtonGridItem> itemComparer)
+            : this(scrollView, new ButtonGridGridLayout(), itemComparer, null)
+        {
+
+        }
+
+
+
+        public HashSetMultiSelectButtonGrid(ScrollView scrollView, ButtonGridLayout layoutEngine)
+            : this(scrollView, layoutEngine, null, null)
+        {
+
+        }
+
+        public HashSetMultiSelectButtonGrid(ScrollView scrollView, ButtonGridLayout layoutEngine, IComparer<ButtonGridItem> itemComparer)
+            : this(scrollView, layoutEngine, itemComparer, null)
+        {
+
+        }
+
+        public HashSetMultiSelectButtonGrid(ScrollView scrollView, ButtonGridLayout layoutEngine, IComparer<ButtonGridItem> itemComparer, CompareButtonGroupUserObjects groupComparer)
+            : base(scrollView, null, layoutEngine, itemComparer, groupComparer)
+        {
+            _workaroundSetSelectionStrategy(multiSelection);
+        }
+
+        public void setSelection(ButtonGridItem primary, IEnumerable<ButtonGridItem> secondary)
+        {
+            multiSelection.setSelection(primary, secondary);
+        }
+
+        public void setSelection(IEnumerable<ButtonGridItem> items)
+        {
+            multiSelection.setSelection(items);
+        }
+
+        public void addSelected(ButtonGridItem item)
+        {
+            multiSelection.addSelected(item);
+        }
+
+        public void removeSelected(ButtonGridItem item)
+        {
+            multiSelection.removeSelected(item);
+        }
+
+        /// <summary>
+        /// The currently selected item.
+        /// </summary>
+        public ButtonGridItem SelectedItem
+        {
+            get
+            {
+                return multiSelection.SelectedItem;
+            }
+            set
+            {
+                multiSelection.SelectedItem = value;
+            }
+        }
+
+        public IEnumerable<ButtonGridItem> SelectedItems
+        {
+            get
+            {
+                return multiSelection.SelectedItems;
+            }
+        }
+    }
+}
---
+++ MyGUIPlugin/Plugin/Widgets/ButtonGrid/HashSetMultiSelectionStrategy.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGUIPlugin
+{
+    public class HashSetMultiSelectionStrategy : ButtonGridSelectionStrategy
+    {
+        public event EventHandler SelectedValueChanged;
+
+        private ButtonGridItem selectedItem;
+        private HashSet<ButtonGridItem> selectedItems = new HashSet<ButtonGridItem>();
+
+        public HashSetMultiSelectionStrategy()
+        {
+            
+        }
+
+        public void itemChosen(ButtonGridItem item)
+        {
+            if (InputManager.Instance.isControlPressed())
+            {
+                if (selectedItems.Contains(item))
+                {
+                    removeSelected(item);
+                }
+                else
+                {
+                    addSelected(item);
+                }
+            }
+            else
+            {
+                uncheckSelectedAndClear();
+                item.StateCheck = true;
+                selectedItems.Add(item);
+                setSelected(item);
+            }
+        }
+
+        public void itemRemoved(ButtonGridItem item)
+        {
+            if (item == SelectedItem)
+            {
+                setSelected(null);
+            }
+            selectedItems.Remove(item);
+        }
+
+        public void itemsCleared()
+        {
+            setSelected(null);
+            selectedItems.Clear();
+        }
+
+        public void setSelection(IEnumerable<ButtonGridItem> items)
+        {
+            uncheckSelectedAndClear();
+            foreach (ButtonGridItem item in items)
+            {
+                selectedItems.Add(item);
+                item.StateCheck = true;
+            }
+            setSelected(selectedItems.FirstOrDefault());
+        }
+
+        public void setSelection(ButtonGridItem primary, IEnumerable<ButtonGridItem> secondary)
+        {
+            uncheckSelectedAndClear();
+            if (primary != null)
+            {
+                selectedItems.Add(primary);
+            }
+            foreach (ButtonGridItem item in secondary)
+            {
+                selectedItems.Add(item);
+                item.StateCheck = true;
+            }
+            setSelected(primary);
+        }
+
+        public void addSelected(ButtonGridItem item)
+        {
+            if (item != null && !selectedItems.Contains(item))
+            {
+                selectedItems.Add(item);
+                item.StateCheck = true;
+                setSelected(item);
+            }
+        }
+
+        public void removeSelected(ButtonGridItem item)
+        {
+            if (item != null)
+            {
+                selectedItems.Remove(item);
+                item.StateCheck = false;
+                if (selectedItem == item)
+                {
+                    setSelected(selectedItems.FirstOrDefault());
+                }
+            }
+        }
+
+        public ButtonGridItem SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                uncheckSelectedAndClear();
+                selectedItems.Add(value);
+                setSelected(value);
+            }
+        }
+
+        public IEnumerable<ButtonGridItem> SelectedItems
+        {
+            get
+            {
+                return selectedItems;
+            }
+        }
+
+        private void setSelected(ButtonGridItem value)
+        {
+            if (selectedItem != value)
+            {
+                selectedItem = value;
+                if (selectedItem != null)
+                {
+                    selectedItem.StateCheck = true;
+                }
+                if (SelectedValueChanged != null)
+                {
+                    SelectedValueChanged.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        private void uncheckSelectedAndClear()
+        {
+            foreach (ButtonGridItem uncheck in selectedItems)
+            {
+                uncheck.StateCheck = false;
+            }
+            selectedItems.Clear();
+        }
+    }
+}
