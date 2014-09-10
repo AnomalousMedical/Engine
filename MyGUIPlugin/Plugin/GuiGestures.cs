046b23fd0d89dabaf08340abb18217cc84b98125
Andrew Piper <threax@users.noreply.github.com>
2014-09-10 18:27:23 +0000

    Added momentum to mygui gestures.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2036 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Plugin/GuiGestures.cs
+++ MyGUIPlugin/Plugin/GuiGestures.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Platform;
+﻿using Engine;
+using Engine.Platform;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,22 @@ namespace MyGUIPlugin
     class GuiGestures : Gesture
     {
         private bool didGesture;
+        private bool gestureStarted = false;
 
-        public GuiGestures(Object eventLayerKey)
+        private Vector2[] averageSpeed;
+        private int averageSpeedCursor = 0;
+        private float decelerationTime = 0.5f;
+        private float minimumMomentum = 2;
+
+        private Vector2 momentum = new Vector2();
+        private Vector2 momentumDirection = new Vector2();
+        private Vector2 deceleration = new Vector2();
+        private IntVector2 lastAbsolutePosition;
+
+        public GuiGestures(Object eventLayerKey, int averageSpeedFrames = 5)
             :base(eventLayerKey)
         {
-
+            averageSpeed = new Vector2[averageSpeedFrames];
         }
 
         protected override bool processFingers(EventLayer eventLayer, Touches touches)
@@ -22,10 +34,16 @@ namespace MyGUIPlugin
             var fingers = touches.Fingers;
             if (Gui.Instance.HandledMouseButtons)
             {
-                didGesture = true;
                 if (fingers.Count == 1)
                 {
-                    InputManager.Instance.injectScrollGesture(fingers[0].PixelX, fingers[0].PixelY, fingers[0].PixelDeltaX, fingers[0].PixelDeltaY);
+                    didGesture = true;
+                    gestureStarted = true;
+                    Vector2 delta = new Vector2(fingers[0].PixelDeltaX, fingers[0].PixelDeltaY);
+                    averageSpeed[averageSpeedCursor] = delta;
+                    averageSpeedCursor++;
+                    averageSpeedCursor %= averageSpeed.Length;
+                    lastAbsolutePosition = new IntVector2(fingers[0].PixelX, fingers[0].PixelY);
+                    InputManager.Instance.injectScrollGesture(lastAbsolutePosition.x, lastAbsolutePosition.y, fingers[0].PixelDeltaX, fingers[0].PixelDeltaY);
                 }
             }
             return didGesture;
@@ -33,6 +51,55 @@ namespace MyGUIPlugin
 
         protected override void additionalProcessing(EventLayer eventLayer, Clock clock)
         {
+            if(!didGesture)
+            {
+                if(gestureStarted)
+                {
+                    momentum = new IntVector2(0, 0);
+                    for (int i = 0; i < averageSpeed.Length; ++i)
+                    {
+                        momentum += averageSpeed[i];
+                    }
+                    momentum /= averageSpeed.Length;
+
+                    momentumDirection = new Vector2(1.0f, 1.0f);
+                    if (momentum.x < 0.0f)
+                    {
+                        momentum.x = -momentum.x;
+                        momentumDirection.x = -1.0f;
+                    }
+                    if (momentum.y < 0.0f)
+                    {
+                        momentum.y = -momentum.y;
+                        momentumDirection.y = -1.0f;
+                    }
+                    if (momentum.x < minimumMomentum)
+                    {
+                        momentum.x = 0.0f;
+                    }
+                    if (momentum.y < minimumMomentum)
+                    {
+                        momentum.y = 0.0f;
+                    }
+                    deceleration = momentum / decelerationTime;
+                }
+                gestureStarted = false;
+
+                if (momentum.length2() != 0.0f)
+                {
+                    momentum -= deceleration * clock.DeltaSeconds;
+                    if (momentum.x < 0.0f)
+                    {
+                        momentum.x = 0.0f;
+                    }
+                    if (momentum.y <= 0.0f)
+                    {
+                        momentum.y = 0.0f;
+                    }
+                    Vector2 finalMomentum = momentum * momentumDirection;
+                    InputManager.Instance.injectScrollGesture(lastAbsolutePosition.x, lastAbsolutePosition.y, (int)finalMomentum.x, (int)finalMomentum.y);
+                }
+            }
             didGesture = false;
         }
     }
