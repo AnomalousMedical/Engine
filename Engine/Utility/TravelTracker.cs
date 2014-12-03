2066ddb98857c1f284d22c11f590456231ebe990
Andrew Piper <threax@users.noreply.github.com>
2014-12-03 14:49:04 +0000

    Moved the travel tracker into the engine.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2141 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Utility/TravelTracker.cs
@@ -0,0 +1,77 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    /// <summary>
+    /// This class can track how far a point has moved.
+    /// </summary>
+    public class TravelTracker
+    {
+        private int moveGracePixels = 3;
+
+        private IntVector2 travelDistance;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TravelTracker()
+            :this(3)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor, takes the number of grace pixels to allow.
+        /// </summary>
+        /// <param name="moveGracePixels">The number of pixels that can be moved in one direction without traveling over the limit.</param>
+        public TravelTracker(int moveGracePixels)
+        {
+            this.moveGracePixels = moveGracePixels;
+        }
+
+        /// <summary>
+        /// Reset to 0.
+        /// </summary>
+        public void reset()
+        {
+            travelDistance = new IntVector2(0, 0);
+        }
+
+        /// <summary>
+        /// Alert a travel amount.
+        /// </summary>
+        /// <param name="travelAmount">The IntVector3 to use, only uses x and y, makes it easy to pass in mouse positions.</param>
+        public void traveled(IntVector3 travelAmount)
+        {
+            travelDistance.x += Math.Abs(travelAmount.x);
+            travelDistance.y += Math.Abs(travelAmount.y);
+        }
+
+        /// <summary>
+        /// Alert a travel amount.
+        /// </summary>
+        /// <param name="x">The x delta.</param>
+        /// <param name="y">The y delta.</param>
+        public void traveled(int x, int y)
+        {
+            travelDistance.x += Math.Abs(x);
+            travelDistance.y += Math.Abs(y);
+        }
+
+        /// <summary>
+        /// True if the limit has been traveled.
+        /// </summary>
+        public bool TraveledOverLimit
+        {
+            get
+            {
+                return travelDistance.x > moveGracePixels || travelDistance.y > moveGracePixels;
+            }
+        }
+    }
+}
