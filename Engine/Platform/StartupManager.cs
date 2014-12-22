b8cb239b24f39cd1afd5fcce970cfa1a8dd5165e
Andrew Piper <threax@users.noreply.github.com>
2014-12-22 20:44:11 +0000

    Added a class that attempts to set the right folders for the program to load native libraries from depending on if it is running on 32 or 64 bit.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2177 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Platform/StartupManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Platform
+{
+    /// <summary>
+    /// This class has startup stuff for the engine.
+    /// </summary>
+    public class StartupManager
+    {
+        /// <summary>
+        /// This should be called as early as possible in a client program. It will setup the x86 and x64 library folders.
+        /// Even though it uses Windows P/Invoke libraries it should work ok on any os since all exceptions are caught.
+        /// </summary>
+        public static void SetupDllDirectories()
+        {
+            try
+            {
+                String executionPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                if (Environment.Is64BitProcess)
+                {
+                    SetDllDirectory(Path.Combine(executionPath, "x64"));
+                }
+                else
+                {
+                    SetDllDirectory(Path.Combine(executionPath, "x86"));
+                }
+            }
+            catch (Exception) { }
+        }
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetDllDirectory(string lpPathName);
+    }
+}
