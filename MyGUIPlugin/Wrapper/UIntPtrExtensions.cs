a8e8574b1fd99454972ce4f128d5509d3357199a
Andrew Piper <threax@users.noreply.github.com>
2014-12-22 19:02:57 +0000

    Added UIntPtrExtensions to the MyGUIPlugin project and comment clarifying the use of the unsafe way of converting uintptr.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2176 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Wrapper/UIntPtrExtensions.cs
+++ MyGUIPlugin/Wrapper/UIntPtrExtensions.cs
@@ -8,6 +8,14 @@ namespace MyGUIPlugin
 {
     internal static class UIntPtrExtensions
     {
+        /// <summary>
+        /// This function will convert the uintptr to a 32 bit uint in a way that
+        /// is safe on 32 and 64 bit clrs. However, on 64 bit you could be outside of
+        /// the valid range and get invalid values. This is unlikely to happen in practice,
+        /// but is something to be aware of.
+        /// </summary>
+        /// <param name="ptr"></param>
+        /// <returns></returns>
         public static uint horriblyUnsafeToUInt32(this UIntPtr ptr)
         {
             return (uint)ptr.ToUInt64();
