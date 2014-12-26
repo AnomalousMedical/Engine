082642508e579a9047606d6a8267e3a5f4e39b31
Andrew Piper <threax@users.noreply.github.com>
2014-12-26 17:02:57 +0000

    Added an auto build for engine.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2198 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ AutoBuild.bat
@@ -0,0 +1,22 @@
+::Configuration Settings
+set SolutionName=Engine.sln
+
+::Less likely to need to change these.
+set ThisFolder=%~dp0
+set RootDependencyFolder=%ThisFolder%..\
+set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" /m
+set CurrentDirectory=%CD%
+
+set SolutionPath=%ThisFolder%%SolutionName%
+
+%BuildCommand% "%SolutionPath%" /property:Configuration=Debug;Platform="Any CPU" /target:Clean
+%BuildCommand% "%SolutionPath%" /property:Configuration=Debug;Platform="Any CPU"
+
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU" /target:Clean
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU"
+
+%BuildCommand% "%SolutionPath%" /property:Configuration=Debug;Platform="x64" /target:Clean
+%BuildCommand% "%SolutionPath%" /property:Configuration=Debug;Platform="x64"
+
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="x64" /target:Clean
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="x64"
