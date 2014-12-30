626d9121d9e3450fc358e6b0aceb4e760d0ec45e
Andrew Piper <threax@users.noreply.github.com>
2014-12-30 20:22:14 +0000

    Moved notifications to the GuiFramework.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2235 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ GuiFramework/GUI/Notification/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medical.GUI
+{
+    public interface Notification
+    {
+        void clicked();
+
+        String ImageKey { get; }
+
+        String Text { get; }
+
+        double Timeout { get; }
+    }
+}
