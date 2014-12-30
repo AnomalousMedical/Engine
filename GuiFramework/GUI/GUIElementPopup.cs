d21fd5749eb0a16d8dbd494244a9f5e0c75e9fa6
Andrew Piper <threax@users.noreply.github.com>
2014-12-30 19:49:32 +0000

    Moved task menu and recent documents to the gui framework.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2231 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ GuiFramework/GUI/GUIElementPopup.cs
@@ -0,0 +1,47 @@
+﻿
+using MyGUIPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical.GUI
+{
+    /// <summary>
+    /// This is a popup that will popup on a named location on the screen.
+    /// </summary>
+    public class GUIElementPopup : PopupContainer
+    {
+        private GUIManager guiManager;
+        private MyGUILayoutContainer layoutContainer;
+        private LayoutElementName elementName;
+
+        public GUIElementPopup(String layout, GUIManager guiManager, LayoutElementName elementName)
+            : base(layout)
+        {
+            this.guiManager = guiManager;
+            this.elementName = elementName;
+            layoutContainer = new MyGUILayoutContainer(widget);
+            layoutContainer.LayoutChanged += new Action(layoutUpdated);
+
+            this.Showing += new EventHandler(ChooseSceneDialog_Showing);
+            this.Hidden += new EventHandler(ChooseSceneDialog_Hidden);
+        }
+
+        protected virtual void layoutUpdated()
+        {
+
+        }
+
+        void ChooseSceneDialog_Hidden(object sender, EventArgs e)
+        {
+            guiManager.closeElement(elementName, layoutContainer);
+        }
+
+        void ChooseSceneDialog_Showing(object sender, EventArgs e)
+        {
+            guiManager.changeElement(elementName, layoutContainer, null);
+        }
+    }
+}
