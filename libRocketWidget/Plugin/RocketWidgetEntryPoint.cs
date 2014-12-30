b0d4ed4d8f9f0f1d0eced1aeb5acdb83f8a99d9c
Andrew Piper <threax@users.noreply.github.com>
2014-12-30 20:52:10 +0000

    Moved the libRocketWidget stuff to its own plugin.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2236 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ libRocketWidget/Plugin/RocketWidgetEntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Engine;
+using Medical.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: libRocketWidget.Plugin.RocketWidgetEntryPoint()]
+
+namespace libRocketWidget.Plugin
+{
+    class RocketWidgetEntryPoint : PluginEntryPointAttribute
+    {
+        public override void createPluginInterfaces(PluginManager pluginManager)
+        {
+            pluginManager.addPlugin(new RocketWidgetInterface());
+        }
+    }
+}
