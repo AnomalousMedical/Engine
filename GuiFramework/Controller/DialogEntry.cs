9559e1cad509ff4195593bb49403ea7bbac610b3
Andrew Piper <threax@users.noreply.github.com>
2014-12-30 17:32:13 +0000

    Moved dialog manager classes to GuiFramework.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2222 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ GuiFramework/Controller/DialogEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Medical.GUI
+{
+    interface DialogEntry
+    {
+        void deserialize(Engine.ConfigFile file);
+        void ensureVisible();
+        void openMainGUIDialog();
+        void serialize(Engine.ConfigFile file);
+        void closeMainGUIDialog();
+        void disposeDialog();
+    }
+}
