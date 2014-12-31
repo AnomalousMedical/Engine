633761d38fee5ecec2dfe6b05cdfceb6704b30e3
Andrew Piper <threax@users.noreply.github.com>
2014-12-31 18:57:57 +0000

    Moved WorkQueue, IdleHandler and ObjectBuffer to engine.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2258 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Threads/WorkQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Engine.Threads
+{
+    public class WorkQueue : IDisposable
+    {
+        private BlockingCollection<Action> queue = new BlockingCollection<Action>();
+        private bool running = true;
+
+        public WorkQueue()
+        {
+            Thread t = new Thread(() =>
+                {
+                    while (running)
+                    {
+                        Action action = queue.Take();
+                        action();
+                    }
+                });
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        public void Dispose()
+        {
+            running = false;
+            enqueue(() =>
+            {
+
+            });
+        }
+
+        public void enqueue(Action action)
+        {
+            queue.Add(action);
+        }
+    }
+}
