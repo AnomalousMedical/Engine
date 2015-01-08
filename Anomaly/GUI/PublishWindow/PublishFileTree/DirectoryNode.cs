bf7c4306d33a6e4941fa7a26c3b406016e336499
Andrew Piper <threax@users.noreply.github.com>
2015-01-08 15:08:07 +0000

    Made sort only happen when there are real changes.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2330 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Anomaly/GUI/PublishWindow/PublishFileTree/DirectoryNode.cs
+++ Anomaly/GUI/PublishWindow/PublishFileTree/DirectoryNode.cs
@@ -10,6 +10,7 @@ namespace Anomaly.GUI
     class DirectoryNode : TreeNode
     {
         private FileBrowserTree projectExplorer;
+        private bool sortStatusDirty = true;
 
         public DirectoryNode(String directory, FileBrowserTree projectExplorer)
         {
@@ -24,18 +25,44 @@ namespace Anomaly.GUI
             DirectoryPath = directory;
         }
 
+        protected override void expandedStatusChanged(bool expanding)
+        {
+            if(sortStatusDirty)
+            {
+                Children.sort(FileTreeSorter.Compare);
+                sortStatusDirty = false;
+            }
+            base.expandedStatusChanged(expanding);
+        }
+
         public String DirectoryPath { get; set; }
 
         public void addDirectoryNode(DirectoryNode node)
         {
             Children.add(node);
-            Children.sort(FileTreeSorter.Compare);
+            if(Expanded)
+            {
+                sortStatusDirty = false;
+                Children.sort(FileTreeSorter.Compare);
+            }
+            else
+            {
+                sortStatusDirty = true;
+            }
         }
 
         public void addFileNode(FileNode node)
         {
             Children.add(node);
-            Children.sort(FileTreeSorter.Compare);
+            if (Expanded)
+            {
+                sortStatusDirty = false;
+                Children.sort(FileTreeSorter.Compare);
+            }
+            else
+            {
+                sortStatusDirty = true;
+            }
         }
     }
 }
