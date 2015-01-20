6aa3f3f1573285c46cb7fa20bea4e8e0d499c78e
Andrew Piper <threax@users.noreply.github.com>
2015-01-20 19:14:16 +0000

    Added the MonoPInvokeCallbackAttribute, we can make our own and it still works.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2427 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Utility/MonoPInvokeCallbackAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoTouch
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MonoPInvokeCallbackAttribute : Attribute
+    {
+        private Type type;
+        public MonoPInvokeCallbackAttribute(Type t) { type = t; }
+    }
+}
