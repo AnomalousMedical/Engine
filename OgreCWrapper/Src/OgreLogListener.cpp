4e1cc83ae7da180060d09be34032af5421c49b94
Andrew Piper <threax@users.noreply.github.com>
2015-01-21 17:54:25 +0000

    More c++ tweaks.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2449 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgreCWrapper/Src/OgreLogListener.cpp
+++ OgreCWrapper/Src/OgreLogListener.cpp
@@ -15,7 +15,7 @@ OgreLogListener::~OgreLogListener(void)
 
 void OgreLogListener::messageLogged(const Ogre::String& message, Ogre::LogMessageLevel lml, bool maskDebug, const Ogre::String &logName, bool &skipThisMessage)
 {
-	messageLoggedCallback(message.c_str(), lml HANDLE_ARG);
+	messageLoggedCallback(message.c_str(), lml PASS_HANDLE_ARG);
 }
 
 extern "C" _AnomalousExport OgreLogListener* OgreLogListener_Create(MessageLoggedDelegate messageLoggedCallback HANDLE_ARG)
