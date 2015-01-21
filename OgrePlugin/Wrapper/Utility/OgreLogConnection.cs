f8b32145d435d38bf895e8347162bd99d54129c5
Andrew Piper <threax@users.noreply.github.com>
2015-01-21 17:30:41 +0000

    Added a aot compatible mode for OgreLogConnection.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2445 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgrePlugin/Wrapper/Utility/OgreLogConnection.cs
+++ OgrePlugin/Wrapper/Utility/OgreLogConnection.cs
@@ -16,20 +16,19 @@ namespace OgrePlugin
 
     class OgreLogConnection : IDisposable
     {
-        private MessageLoggedDelegate messageLoggedCallback;
-
         IntPtr nativeLogListener;
+        CallbackHandler callbackHandler;
 
         public OgreLogConnection()
         {
-            messageLoggedCallback = new MessageLoggedDelegate(messageLogged);
-            nativeLogListener = OgreLogListener_Create(messageLoggedCallback);
+            callbackHandler = new CallbackHandler();
+            nativeLogListener = callbackHandler.createInstance(this);
         }
 
         public void Dispose()
         {
             OgreLogListener_Delete(nativeLogListener);
-            messageLoggedCallback = null;
+            callbackHandler.Dispose();
         }
 
         public void subscribe()
@@ -58,16 +57,72 @@ namespace OgrePlugin
         #region PInvoke
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate void MessageLoggedDelegate(String message, LogMessageLevel lml);
+        private delegate void MessageLoggedDelegate(String message, LogMessageLevel lml
+#if FULL_AOT_COMPILE
+    , IntPtr instanceHandle
+#endif
+);
 
         [DllImport(LibraryInfo.Name, CallingConvention=CallingConvention.Cdecl)]
-        private static extern IntPtr OgreLogListener_Create(MessageLoggedDelegate messageLoggedCallback);
+        private static extern IntPtr OgreLogListener_Create(MessageLoggedDelegate messageLoggedCallback
+#if FULL_AOT_COMPILE
+    , IntPtr instanceHandle
+#endif
+);
 
         [DllImport(LibraryInfo.Name, CallingConvention=CallingConvention.Cdecl)]
         private static extern void OgreLogListener_Delete(IntPtr logListener);
 
         [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
         private static extern void OgreLogListener_subscribe(IntPtr logListener);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static MessageLoggedDelegate messageLoggedCallback;
+
+            static CallbackHandler()
+            {
+                messageLoggedCallback = new MessageLoggedDelegate(messageLogged);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(MessageLoggedDelegate))]
+            private void messageLogged(String message, LogMessageLevel lml, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as OgreLogConnection).messageLogged(message, lml);
+            }
+
+            private GCHandle gcHandle;
+
+            public IntPtr createInstance(OgreLogConnection obj)
+            {
+                gcHandle = GCHandle.Alloc(obj);
+                return OgreLogListener_Create(messageLoggedCallback, GCHandle.ToIntPtr(gcHandle));
+            }
+
+            public void Dispose()
+            {
+                gcHandle.Free();
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            private MessageLoggedDelegate messageLoggedCallback;
+
+            public IntPtr createInstance(OgreLogConnection obj)
+            {
+                messageLoggedCallback = new MessageLoggedDelegate(obj.messageLogged);
+                return OgreLogListener_Create(messageLoggedCallback);
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+#endif
 
         #endregion
     }
