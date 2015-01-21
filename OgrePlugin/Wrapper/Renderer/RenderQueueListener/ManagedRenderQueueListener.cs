bb71e5911f0fa213db4a1a9de583f3ac4b065f02
Andrew Piper <threax@users.noreply.github.com>
2015-01-21 22:09:44 +0000

    Added aot callbacks for ManagedRenderQueueListener.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2468 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgrePlugin/Wrapper/Renderer/RenderQueueListener/ManagedRenderQueueListener.cs
+++ OgrePlugin/Wrapper/Renderer/RenderQueueListener/ManagedRenderQueueListener.cs
@@ -3,42 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Runtime.InteropServices;
+using Anomalous.Interop;
 
 namespace OgrePlugin
 {
     class ManagedRenderQueueListener
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate void EmptyRenderQueueEvent();
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate bool ByteStringBoolRenderQueueEvent(byte queueGroupId, IntPtr invocation);
-
         List<RenderQueueListener> listeners = new List<RenderQueueListener>();
 
         IntPtr nativeRenderQueueListener;
+        CallbackHandler callbackHandler;
         SceneManager sceneManager;
-        EmptyRenderQueueEvent preRenderQueuesCallback;
-        EmptyRenderQueueEvent postRenderQueuesCallback;
-        ByteStringBoolRenderQueueEvent renderQueueStartedCallback;
-        ByteStringBoolRenderQueueEvent renderQueueEndedCallback;
 
         public ManagedRenderQueueListener(SceneManager sceneManager)
         {
             this.sceneManager = sceneManager;
-            preRenderQueuesCallback = new EmptyRenderQueueEvent(preRenderQueues);
-            postRenderQueuesCallback = new EmptyRenderQueueEvent(postRenderQueues);
-            renderQueueStartedCallback = new ByteStringBoolRenderQueueEvent(renderQueueStarted);
-            renderQueueEndedCallback = new ByteStringBoolRenderQueueEvent(renderQueueEnded);
-            nativeRenderQueueListener = NativeRenderQueue_Create(preRenderQueuesCallback, postRenderQueuesCallback, renderQueueStartedCallback, renderQueueEndedCallback);
+            callbackHandler = new CallbackHandler();
+            nativeRenderQueueListener = callbackHandler.create(this);
         }
 
         public void Dispose()
         {
-            preRenderQueuesCallback = null;
-            postRenderQueuesCallback = null;
-            renderQueueStartedCallback = null;
-            renderQueueEndedCallback = null;
+            callbackHandler.Dispose();
             if (listeners.Count != 0)
             {
                 NativeRenderQueue_RemoveListener(sceneManager.OgreSceneManager, nativeRenderQueueListener);
@@ -115,20 +101,112 @@ namespace OgrePlugin
             return repeat;
         }
 
-#region PInvoke
-        
-        [DllImport(LibraryInfo.Name, CallingConvention=CallingConvention.Cdecl)]
-        private static extern IntPtr NativeRenderQueue_Create(EmptyRenderQueueEvent preRender, EmptyRenderQueueEvent postRender, ByteStringBoolRenderQueueEvent renderStarted, ByteStringBoolRenderQueueEvent renderEnded);
+        #region PInvoke
 
-        [DllImport(LibraryInfo.Name, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr NativeRenderQueue_Create(NativeAction preRender, NativeAction postRender, ByteStringBoolRenderQueueEvent renderStarted, ByteStringBoolRenderQueueEvent renderEnded
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
         private static extern void NativeRenderQueue_Delete(IntPtr nativeRenderQueueListener);
 
-        [DllImport(LibraryInfo.Name, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
         private static extern void NativeRenderQueue_AddListener(IntPtr sceneManager, IntPtr nativeRenderQueueListener);
 
-        [DllImport(LibraryInfo.Name, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
         private static extern void NativeRenderQueue_RemoveListener(IntPtr sceneManager, IntPtr nativeRenderQueueListener);
 
-#endregion
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate bool ByteStringBoolRenderQueueEvent(byte queueGroupId, IntPtr invocation
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            static NativeAction preRenderQueuesCallback;
+            static NativeAction postRenderQueuesCallback;
+            static ByteStringBoolRenderQueueEvent renderQueueStartedCallback;
+            static ByteStringBoolRenderQueueEvent renderQueueEndedCallback;
+
+            static CallbackHandler()
+            {
+                preRenderQueuesCallback = new NativeAction(preRenderQueues);
+                postRenderQueuesCallback = new NativeAction(postRenderQueues);
+                renderQueueStartedCallback = new ByteStringBoolRenderQueueEvent(renderQueueStarted);
+                renderQueueEndedCallback = new ByteStringBoolRenderQueueEvent(renderQueueEnded);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(NativeAction))]
+            static void preRenderQueues(IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as ManagedRenderQueueListener).preRenderQueues();
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(NativeAction))]
+            static void postRenderQueues(IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as ManagedRenderQueueListener).postRenderQueues();
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(ByteStringBoolRenderQueueEvent))]
+            static bool renderQueueStarted(byte queueGroupId, IntPtr invocation, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                return (handle.Target as ManagedRenderQueueListener).renderQueueStarted(queueGroupId, invocation);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(ByteStringBoolRenderQueueEvent))]
+            static bool renderQueueEnded(byte queueGroupId, IntPtr invocation, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                return (handle.Target as ManagedRenderQueueListener).renderQueueEnded(queueGroupId, invocation);
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(ManagedRenderQueueListener obj)
+            {
+                handle = GCHandle.Alloc(obj);
+                return NativeRenderQueue_Create(preRenderQueuesCallback, postRenderQueuesCallback, renderQueueStartedCallback, renderQueueEndedCallback, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            NativeAction preRenderQueuesCallback;
+            NativeAction postRenderQueuesCallback;
+            ByteStringBoolRenderQueueEvent renderQueueStartedCallback;
+            ByteStringBoolRenderQueueEvent renderQueueEndedCallback;
+
+            public IntPtr create(ManagedRenderQueueListener obj)
+            {
+                preRenderQueuesCallback = new NativeAction(obj.preRenderQueues);
+                postRenderQueuesCallback = new NativeAction(obj.postRenderQueues);
+                renderQueueStartedCallback = new ByteStringBoolRenderQueueEvent(obj.renderQueueStarted);
+                renderQueueEndedCallback = new ByteStringBoolRenderQueueEvent(obj.renderQueueEnded);
+                return NativeRenderQueue_Create(preRenderQueuesCallback, postRenderQueuesCallback, renderQueueStartedCallback, renderQueueEndedCallback);
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+#endif
+
+        #endregion
     }
 }
