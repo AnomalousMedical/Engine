84e06e2ab740937bdb9e15d5987f4001aaf6187b
Andrew Piper <threax@users.noreply.github.com>
2015-01-21 16:02:06 +0000

    Made it so we can hijack EngineMac to build for iOS, not ideal but gets the job done for now.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2437 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ OgreCWrapper/iOS/AutoBuild_iOS.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Discover some stuff about where we are.
+START_PATH=$(pwd)
+THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+
+#Build with XCode
+xcodebuild -configuration Release -sdk iphonesimulator8.1 ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH_sim"
+xcodebuild -configuration Release -sdk iphoneos8.1 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH_device"
+
+mkdir build
+mkdir build/Release
+
+lipo -create build/Release-iphoneos/libOgreCWrapper.a build/Release-iphonesimulator/libOgreCWrapper.a -output build/Release/libOgreCWrapper.a
+
+#Finish up
+cd "$START_PATH"
