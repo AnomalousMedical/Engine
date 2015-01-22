b633c1267f72b0abf9639ac8e5c09bb405bfb062
Andrew Piper <threax@users.noreply.github.com>
2015-01-22 18:35:51 +0000

    Added AOT for ManagedLogListener.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2493 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OSPlatform/ManagedLogListener.cs
+++ OSPlatform/ManagedLogListener.cs
@@ -10,18 +10,19 @@ namespace Anomalous.OSPlatform
     class ManagedLogListener : IDisposable
     {
         private IntPtr managedLogListener;
-        private LogMessageDelegate logCB;
+        private CallbackHandler callbackHandler;
 
         public ManagedLogListener()
         {
-            logCB = logMessage;
-            managedLogListener = ManagedLogListener_create(logCB);
+            callbackHandler = new CallbackHandler();
+            managedLogListener = callbackHandler.create(this);
             NativeLog_addLogListener(managedLogListener);
         }
 
         public void Dispose()
         {
             ManagedLogListener_destroy(managedLogListener);
+            callbackHandler.Dispose();
         }
 
         private void logMessage(IntPtr message, LogLevel logLevel, IntPtr subsystem)
@@ -32,16 +33,72 @@ namespace Anomalous.OSPlatform
         #region PInvoke
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate void LogMessageDelegate(IntPtr message, LogLevel logLevel, IntPtr subsystem);
+        delegate void LogMessageDelegate(IntPtr message, LogLevel logLevel, IntPtr subsystem
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
 
         [DllImport(NativePlatformPlugin.LibraryName, CallingConvention = CallingConvention.Cdecl)]
-        private static extern IntPtr ManagedLogListener_create(LogMessageDelegate logDelegate);
+        private static extern IntPtr ManagedLogListener_create(LogMessageDelegate logDelegate
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
 
         [DllImport(NativePlatformPlugin.LibraryName, CallingConvention = CallingConvention.Cdecl)]
         private static extern void ManagedLogListener_destroy(IntPtr managedLogListener);
 
         [DllImport(NativePlatformPlugin.LibraryName, CallingConvention = CallingConvention.Cdecl)]
         private static extern void NativeLog_addLogListener(IntPtr logListener);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static LogMessageDelegate logCB;
+
+            static CallbackHandler()
+            {
+                logCB = logMessage;
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(LogMessageDelegate))]
+            private static void logMessage(IntPtr message, LogLevel logLevel, IntPtr subsystem, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as ManagedLogListener).logMessage(message, logLevel, subsystem);
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(ManagedLogListener obj)
+            {
+                handle = GCHandle.Alloc(obj);
+                return ManagedLogListener_create(logCB, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            private LogMessageDelegate logCB;
+
+            public IntPtr create(ManagedLogListener obj)
+            {
+                logCB = obj.logMessage;
+                return ManagedLogListener_create(logCB);
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+#endif
 
         #endregion
     }
