4667a9d4dfbb8685a49897e9a67f1736dd4f0eef
Andrew Piper <threax@users.noreply.github.com>
2015-01-22 15:28:57 +0000

    Added aot for EventMouseButtonPressedTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2476 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Wrapper/Events/Widget/EventMouseButtonPressedTranslator.cs
+++ MyGUIPlugin/Wrapper/Events/Widget/EventMouseButtonPressedTranslator.cs
@@ -11,26 +11,24 @@ namespace MyGUIPlugin
 {
     class EventMouseButtonPressedTranslator : MyGUIWidgetEventTranslator
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate void NativeEventDelegate(IntPtr widget, int left, int top, MouseButtonCode id);
         static MouseEventArgs eventArgs = new MouseEventArgs();
 
-        private NativeEventDelegate nativeEventCallback;
+        private CallbackHandler callbackHandler;
 
         public EventMouseButtonPressedTranslator()
         {
-            nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            callbackHandler = new CallbackHandler();
         }
 
         public override void Dispose()
         {
             base.Dispose();
-            nativeEventCallback = null;
+            callbackHandler.Dispose();
         }
 
         protected override IntPtr doInitialize(Widget widget)
         {
-            return EventMouseButtonPressedTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            return callbackHandler.create(this, widget);
         }
 
         private void nativeEvent(IntPtr widget, int left, int top, MouseButtonCode id)
@@ -44,7 +42,67 @@ namespace MyGUIPlugin
         #region PInvoke
 
         [DllImport(MyGUIInterface.LibraryName, CallingConvention=CallingConvention.Cdecl)]
-        private static extern IntPtr EventMouseButtonPressedTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback);
+        private static extern IntPtr EventMouseButtonPressedTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback
+#if FULL_AOT_COMPILE
+        , IntPtr instanceHandle
+#endif
+);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void NativeEventDelegate(IntPtr widget, int left, int top, MouseButtonCode id
+#if FULL_AOT_COMPILE
+        , IntPtr instanceHandle
+#endif
+);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static NativeEventDelegate nativeEventCallback;
+
+            static CallbackHandler()
+            {
+                nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(NativeEventDelegate))]
+            private static void nativeEvent(IntPtr widget, int left, int top, MouseButtonCode id, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as EventMouseButtonPressedTranslator).nativeEvent(widget, left, top, id);
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(EventMouseButtonPressedTranslator obj, Widget widget)
+            {
+                handle = GCHandle.Alloc(obj);
+                return EventMouseButtonPressedTranslator_Create(widget.WidgetPtr, nativeEventCallback, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+                nativeEventCallback = null;
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            private NativeEventDelegate nativeEventCallback;
+
+            public IntPtr create(EventMouseDragTranslator obj, Widget widget)
+            {
+                nativeEventCallback = new NativeEventDelegate(obj.nativeEvent);
+                return EventMouseButtonPressedTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            }
+
+            public void Dispose()
+            {
+                nativeEventCallback = null;
+            }
+        }
+#endif
 
         #endregion
     }
--- MyGUIWrapper/Src/EventMouseButtonPressedTranslator.cpp
+++ MyGUIWrapper/Src/EventMouseButtonPressedTranslator.cpp
@@ -4,16 +4,23 @@
 class EventMouseButtonPressedTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, int left, int top, MyGUI::MouseButton id);
+	typedef void(*NativeEventDelegate)(MyGUI::Widget* sender, int left, int top, MyGUI::MouseButton id HANDLE_ARG);
 
 private:
 	MyGUI::Widget* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+	void fireEvent(MyGUI::Widget* sender, int left, int top, MyGUI::MouseButton id)
+	{
+		nativeEvent(sender, left, top, id PASS_HANDLE_ARG);
+	}
 
 public:
-	EventMouseButtonPressedTranslator(MyGUI::Widget* widget, EventMouseButtonPressedTranslator::NativeEventDelegate nativeEventCallback)
+	EventMouseButtonPressedTranslator(MyGUI::Widget* widget, EventMouseButtonPressedTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +32,7 @@ public:
 
 	virtual void bindEvent()
 	{
-		widget->eventMouseButtonPressed = MyGUI::newDelegate(nativeEvent);
+		widget->eventMouseButtonPressed = MyGUI::newDelegate(this, &EventMouseButtonPressedTranslator::fireEvent);
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +41,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventMouseButtonPressedTranslator* EventMouseButtonPressedTranslator_Create(MyGUI::Widget* widget, EventMouseButtonPressedTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventMouseButtonPressedTranslator* EventMouseButtonPressedTranslator_Create(MyGUI::Widget* widget, EventMouseButtonPressedTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventMouseButtonPressedTranslator(widget, nativeEventCallback);
+	return new EventMouseButtonPressedTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
