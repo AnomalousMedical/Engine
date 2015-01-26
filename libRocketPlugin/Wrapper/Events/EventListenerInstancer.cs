82de8fc4e162c63459781202730c9ee6fd3d99e6
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 15:25:57 +0000

    Added AOT for EventListenerInstancer.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2527 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- libRocketPlugin/Wrapper/Events/EventListenerInstancer.cs
+++ libRocketPlugin/Wrapper/Events/EventListenerInstancer.cs
@@ -8,15 +8,18 @@ namespace libRocketPlugin
 {
     public abstract class EventListenerInstancer : ReferenceCountable
     {
-        InstanceEventListenerCb instanceEventListenerCb;
-        ReleaseCb releaseCb;
+        private CallbackHandler callbackHandler;
 
         public EventListenerInstancer()
         {
-            instanceEventListenerCb = new InstanceEventListenerCb(InstanceEventListenerCbImpl);
-            releaseCb = new ReleaseCb(ReleaseCbImpl);
+            callbackHandler = new CallbackHandler();
+            setPtr(callbackHandler.create(this));
+        }
 
-            setPtr(ManagedEventListenerInstancer_Create(instanceEventListenerCb, releaseCb));
+        public override void Dispose()
+        {
+            base.Dispose();
+            callbackHandler.Dispose();
         }
 
         public abstract EventListener InstanceEventListener(String name, Element element);
@@ -29,7 +32,7 @@ namespace libRocketPlugin
         private IntPtr InstanceEventListenerCbImpl(String name, IntPtr element)
         {
             EventListener evtListener = InstanceEventListener(name, ElementManager.getElement(element));
-            if(evtListener != null)
+            if (evtListener != null)
             {
                 return evtListener.Ptr;
             }
@@ -45,16 +48,86 @@ namespace libRocketPlugin
         #region PInvoke
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate IntPtr InstanceEventListenerCb(String name, IntPtr element);
+        delegate IntPtr InstanceEventListenerCb(String name, IntPtr element
+#if FULL_AOT_COMPILE
+        , IntPtr instanceHandle
+#endif
+);
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	    delegate void ReleaseCb();
+        delegate void ReleaseCb(
+#if FULL_AOT_COMPILE
+        IntPtr instanceHandle
+#endif
+);
 
         [DllImport(RocketInterface.LibraryName, CallingConvention = CallingConvention.Cdecl)]
-        private static extern IntPtr ManagedEventListenerInstancer_Create(InstanceEventListenerCb instanceEventListener, ReleaseCb release);
+        private static extern IntPtr ManagedEventListenerInstancer_Create(InstanceEventListenerCb instanceEventListener, ReleaseCb release
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
 
         [DllImport(RocketInterface.LibraryName, CallingConvention = CallingConvention.Cdecl)]
         private static extern void ManagedEventListenerInstancer_Delete(IntPtr eventListenerInstancer);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static InstanceEventListenerCb instanceEventListenerCb;
+            private static ReleaseCb releaseCb;
+
+            static CallbackHandler()
+            {
+                instanceEventListenerCb = new InstanceEventListenerCb(InstanceEventListenerCbImpl);
+                releaseCb = new ReleaseCb(ReleaseCbImpl);
+            }
+
+            private static IntPtr InstanceEventListenerCbImpl(String name, IntPtr element, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                return (handle.Target as EventListenerInstancer).InstanceEventListenerCbImpl(name, element);
+            }
+
+            private static void ReleaseCbImpl(IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as EventListenerInstancer).ReleaseCbImpl();
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(EventListenerInstancer obj)
+            {
+                handle = GCHandle.Alloc(obj);
+                return ManagedEventListenerInstancer_Create(instanceEventListenerCb, releaseCb, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            InstanceEventListenerCb instanceEventListenerCb;
+            ReleaseCb releaseCb;
+
+            public IntPtr create(EventListenerInstancer obj)
+            {
+                instanceEventListenerCb = new InstanceEventListenerCb(obj.InstanceEventListenerCbImpl);
+                releaseCb = new ReleaseCb(obj.ReleaseCbImpl);
+
+                return ManagedEventListenerInstancer_Create(instanceEventListenerCb, releaseCb);
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+#endif
 
         #endregion
     }
--- libRocketWrapper/Src/ManagedEventListenerInstancer.cpp
+++ libRocketWrapper/Src/ManagedEventListenerInstancer.cpp
@@ -3,12 +3,13 @@
 class ManagedEventListenerInstancer : public Rocket::Core::EventListenerInstancer
 {
 public:
-	typedef Rocket::Core::EventListener* (*InstanceEventListenerCb)(String name, Rocket::Core::Element* element);
-	typedef void (*ReleaseCb)();
+	typedef Rocket::Core::EventListener* (*InstanceEventListenerCb)(String name, Rocket::Core::Element* element HANDLE_ARG);
+	typedef void(*ReleaseCb)(HANDLE_FIRST_ARG);
 
-	ManagedEventListenerInstancer(InstanceEventListenerCb instanceEventListener, ReleaseCb release)
+	ManagedEventListenerInstancer(InstanceEventListenerCb instanceEventListener, ReleaseCb release HANDLE_ARG)
 		:instanceEventListener(instanceEventListener),
 		release(release)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -19,23 +20,24 @@ public:
 	/// @param value Value of the event	
 	virtual Rocket::Core::EventListener* InstanceEventListener(const Rocket::Core::String& value, Rocket::Core::Element* element)
 	{
-		return instanceEventListener(value.CString(), element);
+		return instanceEventListener(value.CString(), element PASS_HANDLE_ARG);
 	}
 
 	/// Releases this event listener instancer
 	virtual void Release()
 	{
-		release();
+		release(PASS_HANDLE);
 	}
 
 private:
 	InstanceEventListenerCb instanceEventListener;
 	ReleaseCb release;
+	HANDLE_INSTANCE
 };
 
-extern "C" _AnomalousExport ManagedEventListenerInstancer* ManagedEventListenerInstancer_Create(ManagedEventListenerInstancer::InstanceEventListenerCb instanceEventListener, ManagedEventListenerInstancer::ReleaseCb release)
+extern "C" _AnomalousExport ManagedEventListenerInstancer* ManagedEventListenerInstancer_Create(ManagedEventListenerInstancer::InstanceEventListenerCb instanceEventListener, ManagedEventListenerInstancer::ReleaseCb release HANDLE_ARG)
 {
-	return new ManagedEventListenerInstancer(instanceEventListener, release);
+	return new ManagedEventListenerInstancer(instanceEventListener, release PASS_HANDLE_ARG);
 }
 
 extern "C" _AnomalousExport void ManagedEventListenerInstancer_Delete(ManagedEventListenerInstancer* eventListenerInstancer)
