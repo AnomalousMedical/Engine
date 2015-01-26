8e4f1de12250b13b7d8b912ebdd03c6e258175b1
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 15:30:26 +0000

    Added missing attribute.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2528 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- libRocketPlugin/Wrapper/Events/EventListenerInstancer.cs
+++ libRocketPlugin/Wrapper/Events/EventListenerInstancer.cs
@@ -83,12 +83,14 @@ namespace libRocketPlugin
                 releaseCb = new ReleaseCb(ReleaseCbImpl);
             }
 
+            [MonoTouch.MonoPInvokeCallback(typeof(InstanceEventListenerCb))]
             private static IntPtr InstanceEventListenerCbImpl(String name, IntPtr element, IntPtr instanceHandle)
             {
                 GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
                 return (handle.Target as EventListenerInstancer).InstanceEventListenerCbImpl(name, element);
             }
 
+            [MonoTouch.MonoPInvokeCallback(typeof(ReleaseCb))]
             private static void ReleaseCbImpl(IntPtr instanceHandle)
             {
                 GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
