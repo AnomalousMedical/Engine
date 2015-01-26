e4895be7e5c4eef6cad71d420e01241274a30c46
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 21:06:24 +0000

    Added AOT for EventCanvasPositionChanged.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2557 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIWrapper/Src/EventCanvasPositionChanged.cpp
+++ MyGUIWrapper/Src/EventCanvasPositionChanged.cpp
@@ -4,16 +4,25 @@
 class EventCanvasPositionChangedTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, int x, int y);
+	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, int x, int y HANDLE_ARG);
 
 private:
 	MyGUI::ScrollView* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+	void fireEvent(MyGUI::Widget* sender, int x, int y)
+	{
+		nativeEvent(sender, x, y PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventCanvasPositionChangedTranslator(MyGUI::ScrollView* widget, EventCanvasPositionChangedTranslator::NativeEventDelegate nativeEventCallback)
+	EventCanvasPositionChangedTranslator(MyGUI::ScrollView* widget, EventCanvasPositionChangedTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventCanvasPositionChanged = MyGUI::newDelegate(this, &EventCanvasPositionChangedTranslator::fireEvent);
+#else
 		widget->eventCanvasPositionChanged = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventCanvasPositionChangedTranslator* EventCanvasPositionChangedTranslator_Create(MyGUI::ScrollView* widget, EventCanvasPositionChangedTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventCanvasPositionChangedTranslator* EventCanvasPositionChangedTranslator_Create(MyGUI::ScrollView* widget, EventCanvasPositionChangedTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventCanvasPositionChangedTranslator(widget, nativeEventCallback);
+	return new EventCanvasPositionChangedTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
