6d8ec2f42db5a35dc029002de656e249c7705884
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 21:24:47 +0000

    Added AOT for EventKeyLostFocusTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2560 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Wrapper/Events/Widget/EventKeyLostFocusTranslator.cs
+++ MyGUIPlugin/Wrapper/Events/Widget/EventKeyLostFocusTranslator.cs
@@ -9,26 +9,24 @@ namespace MyGUIPlugin
 {
     class EventKeyLostFocusTranslator : MyGUIWidgetEventTranslator
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate void NativeEventDelegate(IntPtr widget, IntPtr newFocus);
         static FocusEventArgs eventArgs = new FocusEventArgs();
 
-        private NativeEventDelegate nativeEventCallback;
+        private CallbackHandler callbackHandler;
 
         public EventKeyLostFocusTranslator()
         {
-            nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            callbackHandler = new CallbackHandler();
         }
 
         public override void Dispose()
         {
             base.Dispose();
-            nativeEventCallback = null;
+            callbackHandler.Dispose();
         }
 
         protected override IntPtr doInitialize(Widget widget)
         {
-            return EventKeyLostFocusTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            return callbackHandler.create(this, widget);
         }
 
         private void nativeEvent(IntPtr widget, IntPtr newFocus)
@@ -41,7 +39,67 @@ namespace MyGUIPlugin
         #region PInvoke
 
         [DllImport(MyGUIInterface.LibraryName, CallingConvention=CallingConvention.Cdecl)]
-        private static extern IntPtr EventKeyLostFocusTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback);
+        private static extern IntPtr EventKeyLostFocusTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void NativeEventDelegate(IntPtr widget, IntPtr newFocus
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static NativeEventDelegate nativeEventCallback;
+
+            static CallbackHandler()
+            {
+                nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(NativeEventDelegate))]
+            private static void nativeEvent(IntPtr widget, IntPtr newFocus, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as EventKeyLostFocusTranslator).nativeEvent(widget, newFocus);
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(EventKeyLostFocusTranslator obj, Widget widget)
+            {
+                handle = GCHandle.Alloc(obj);
+                return EventKeyLostFocusTranslator_Create(widget.WidgetPtr, nativeEventCallback, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+                nativeEventCallback = null;
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            private NativeEventDelegate nativeEventCallback;
+
+            public IntPtr create(EventKeyLostFocusTranslator obj, Widget widget)
+            {
+                nativeEventCallback = new NativeEventDelegate(obj.nativeEvent);
+                return EventKeyLostFocusTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            }
+
+            public void Dispose()
+            {
+                nativeEventCallback = null;
+            }
+        }
+#endif
 
         #endregion
     }
--- MyGUIWrapper/Src/EventKeyLostFocusTranslator.cpp
+++ MyGUIWrapper/Src/EventKeyLostFocusTranslator.cpp
@@ -4,16 +4,25 @@
 class EventKeyLostFocusTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, MyGUI::Widget* newFocus);
+	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, MyGUI::Widget* newFocus HANDLE_ARG);
 
 private:
 	MyGUI::Widget* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+		void fireEvent(MyGUI::Widget* sender, MyGUI::Widget* newFocus)
+	{
+		nativeEvent(sender, newFocus PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventKeyLostFocusTranslator(MyGUI::Widget* widget, EventKeyLostFocusTranslator::NativeEventDelegate nativeEventCallback)
+	EventKeyLostFocusTranslator(MyGUI::Widget* widget, EventKeyLostFocusTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventKeyLostFocus = MyGUI::newDelegate(this, &EventKeyLostFocusTranslator::fireEvent);
+#else
 		widget->eventKeyLostFocus = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventKeyLostFocusTranslator* EventKeyLostFocusTranslator_Create(MyGUI::Widget* widget, EventKeyLostFocusTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventKeyLostFocusTranslator* EventKeyLostFocusTranslator_Create(MyGUI::Widget* widget, EventKeyLostFocusTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventKeyLostFocusTranslator(widget, nativeEventCallback);
+	return new EventKeyLostFocusTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
