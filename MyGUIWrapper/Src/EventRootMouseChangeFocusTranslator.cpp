d86c0fa26f6d884c500dee768ba139eed7380be8
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 21:37:07 +0000

    Added AOT for EventRootMouseChangeFocusTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2563 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIWrapper/Src/EventRootMouseChangeFocusTranslator.cpp
+++ MyGUIWrapper/Src/EventRootMouseChangeFocusTranslator.cpp
@@ -4,16 +4,25 @@
 class EventRootMouseChangeFocusTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, bool focus);
+	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, bool focus HANDLE_ARG);
 
 private:
 	MyGUI::Widget* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+	void fireEvent(MyGUI::Widget* sender, bool focus)
+	{
+		nativeEvent(sender, focus PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventRootMouseChangeFocusTranslator(MyGUI::Widget* widget, EventRootMouseChangeFocusTranslator::NativeEventDelegate nativeEventCallback)
+	EventRootMouseChangeFocusTranslator(MyGUI::Widget* widget, EventRootMouseChangeFocusTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventRootMouseChangeFocus = MyGUI::newDelegate(this, &EventRootMouseChangeFocusTranslator::fireEvent);
+#else
 		widget->eventRootMouseChangeFocus = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventRootMouseChangeFocusTranslator* EventRootMouseChangeFocusTranslator_Create(MyGUI::Widget* widget, EventRootMouseChangeFocusTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventRootMouseChangeFocusTranslator* EventRootMouseChangeFocusTranslator_Create(MyGUI::Widget* widget, EventRootMouseChangeFocusTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventRootMouseChangeFocusTranslator(widget, nativeEventCallback);
+	return new EventRootMouseChangeFocusTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
