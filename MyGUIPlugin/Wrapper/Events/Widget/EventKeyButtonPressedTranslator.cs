0d03687692fa320dcdfabffc5d8b607343c6ee1f
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 16:41:00 +0000

    Added AOT for EventKeyButtonPressedTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2533 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Wrapper/Events/Widget/EventKeyButtonPressedTranslator.cs
+++ MyGUIPlugin/Wrapper/Events/Widget/EventKeyButtonPressedTranslator.cs
@@ -10,26 +10,24 @@ namespace MyGUIPlugin
 {
     class EventKeyButtonPressedTranslator : MyGUIWidgetEventTranslator
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate void NativeEventDelegate(IntPtr widget, KeyboardButtonCode key, char _char);
         static KeyEventArgs eventArgs = new KeyEventArgs();
 
-        private NativeEventDelegate nativeEventCallback;
+        private CallbackHandler callbackHandler;
 
         public EventKeyButtonPressedTranslator()
         {
-            nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            callbackHandler = new CallbackHandler();
         }
 
         public override void Dispose()
         {
             base.Dispose();
-            nativeEventCallback = null;
+            callbackHandler.Dispose();
         }
 
         protected override IntPtr doInitialize(Widget widget)
         {
-            return EventKeyButtonPressedTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            return callbackHandler.create(this, widget);
         }
 
         private void nativeEvent(IntPtr widget, KeyboardButtonCode key, char _char)
@@ -43,7 +41,67 @@ namespace MyGUIPlugin
         #region PInvoke
 
         [DllImport(MyGUIInterface.LibraryName, CallingConvention=CallingConvention.Cdecl)]
-        private static extern IntPtr EventKeyButtonPressedTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback);
+        private static extern IntPtr EventKeyButtonPressedTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void NativeEventDelegate(IntPtr widget, KeyboardButtonCode key, char _char
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static NativeEventDelegate nativeEventCallback;
+
+            static CallbackHandler()
+            {
+                nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(NativeEventDelegate))]
+            private static void nativeEvent(IntPtr widget, KeyboardButtonCode key, char _char, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as EventKeyButtonPressedTranslator).nativeEvent(widget, key, _char);
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(EventKeyButtonPressedTranslator obj, Widget widget)
+            {
+                handle = GCHandle.Alloc(obj);
+                return EventKeyButtonPressedTranslator_Create(widget.WidgetPtr, nativeEventCallback, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+                nativeEventCallback = null;
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            private NativeEventDelegate nativeEventCallback;
+
+            public IntPtr create(EventKeyButtonPressedTranslator obj, Widget widget)
+            {
+                nativeEventCallback = new NativeEventDelegate(obj.nativeEvent);
+                return EventKeyButtonPressedTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            }
+
+            public void Dispose()
+            {
+                nativeEventCallback = null;
+            }
+        }
+#endif
 
         #endregion
     }
--- MyGUIWrapper/Src/EventKeyButtonPressedTranslator.cpp
+++ MyGUIWrapper/Src/EventKeyButtonPressedTranslator.cpp
@@ -4,16 +4,25 @@
 class EventKeyButtonPressedTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, MyGUI::KeyCode key, MyGUI::Char _char);
+	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, MyGUI::KeyCode key, MyGUI::Char _char HANDLE_ARG);
 
 private:
 	MyGUI::Widget* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+	void fireEvent(MyGUI::Widget* sender, MyGUI::KeyCode key, MyGUI::Char _char)
+	{
+		nativeEvent(sender, key, _char PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventKeyButtonPressedTranslator(MyGUI::Widget* widget, EventKeyButtonPressedTranslator::NativeEventDelegate nativeEventCallback)
+	EventKeyButtonPressedTranslator(MyGUI::Widget* widget, EventKeyButtonPressedTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventKeyButtonPressed = MyGUI::newDelegate(this, &EventKeyButtonPressedTranslator::fireEvent);
+#else
 		widget->eventKeyButtonPressed = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventKeyButtonPressedTranslator* EventKeyButtonPressedTranslator_Create(MyGUI::Widget* widget, EventKeyButtonPressedTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventKeyButtonPressedTranslator* EventKeyButtonPressedTranslator_Create(MyGUI::Widget* widget, EventKeyButtonPressedTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventKeyButtonPressedTranslator(widget, nativeEventCallback);
+	return new EventKeyButtonPressedTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
