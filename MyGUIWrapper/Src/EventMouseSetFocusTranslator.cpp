782b7b23da9ff22f5c9933711ec4a79c11ee6818
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 17:02:47 +0000

    Added AOT for EventMouseSetFocusTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2536 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIWrapper/Src/EventMouseSetFocusTranslator.cpp
+++ MyGUIWrapper/Src/EventMouseSetFocusTranslator.cpp
@@ -4,16 +4,25 @@
 class EventMouseSetFocusTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, MyGUI::Widget* old);
+	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender, MyGUI::Widget* old HANDLE_ARG);
 
 private:
 	MyGUI::Widget* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+	void fireEvent(MyGUI::Widget* sender, MyGUI::Widget* old)
+	{
+		nativeEvent(sender, old PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventMouseSetFocusTranslator(MyGUI::Widget* widget, EventMouseSetFocusTranslator::NativeEventDelegate nativeEventCallback)
+	EventMouseSetFocusTranslator(MyGUI::Widget* widget, EventMouseSetFocusTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventMouseSetFocus = MyGUI::newDelegate(this, &EventMouseSetFocusTranslator::fireEvent);
+#else
 		widget->eventMouseSetFocus = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventMouseSetFocusTranslator* EventMouseSetFocusTranslator_Create(MyGUI::Widget* widget, EventMouseSetFocusTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventMouseSetFocusTranslator* EventMouseSetFocusTranslator_Create(MyGUI::Widget* widget, EventMouseSetFocusTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventMouseSetFocusTranslator(widget, nativeEventCallback);
+	return new EventMouseSetFocusTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
