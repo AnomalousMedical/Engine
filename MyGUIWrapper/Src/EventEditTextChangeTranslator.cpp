f8a31e83e8dbf907ceac296bc73b7eeccf53b7f7
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 20:34:39 +0000

    Added AOT for EventEditTextChangeTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2551 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIWrapper/Src/EventEditTextChangeTranslator.cpp
+++ MyGUIWrapper/Src/EventEditTextChangeTranslator.cpp
@@ -4,16 +4,25 @@
 class EventEditTextChangeTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::EditBox* sender);
+	typedef void (*NativeEventDelegate)(MyGUI::EditBox* sender HANDLE_ARG);
 
 private:
 	MyGUI::EditBox* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+		void fireEvent(MyGUI::EditBox* sender)
+	{
+		nativeEvent(sender PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventEditTextChangeTranslator(MyGUI::EditBox* widget, EventEditTextChangeTranslator::NativeEventDelegate nativeEventCallback)
+	EventEditTextChangeTranslator(MyGUI::EditBox* widget, EventEditTextChangeTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventEditTextChange = MyGUI::newDelegate(this, &EventEditTextChangeTranslator::fireEvent);
+#else
 		widget->eventEditTextChange = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventEditTextChangeTranslator* EventEditTextChangeTranslator_Create(MyGUI::EditBox* widget, EventEditTextChangeTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventEditTextChangeTranslator* EventEditTextChangeTranslator_Create(MyGUI::EditBox* widget, EventEditTextChangeTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventEditTextChangeTranslator(widget, nativeEventCallback);
+	return new EventEditTextChangeTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
