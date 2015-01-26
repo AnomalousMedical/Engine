5566f7246399935ccabfdb67b406df6e36ac9dee
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 20:53:49 +0000

    Added AOT for EventListSelectAcceptTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2555 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIWrapper/Src/EventListSelectAcceptTranslator.cpp
+++ MyGUIWrapper/Src/EventListSelectAcceptTranslator.cpp
@@ -4,16 +4,25 @@
 class EventListSelectAcceptTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::MultiListBox* sender, size_t index);
+	typedef void (*NativeEventDelegate)(MyGUI::MultiListBox* sender, size_t index HANDLE_ARG);
 
 private:
 	MyGUI::MultiListBox* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+	void fireEvent(MyGUI::MultiListBox* sender, size_t index)
+	{
+		nativeEvent(sender, index PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventListSelectAcceptTranslator(MyGUI::MultiListBox* widget, EventListSelectAcceptTranslator::NativeEventDelegate nativeEventCallback)
+	EventListSelectAcceptTranslator(MyGUI::MultiListBox* widget, EventListSelectAcceptTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventListSelectAccept = MyGUI::newDelegate(this, &EventListSelectAcceptTranslator::fireEvent);
+#else
 		widget->eventListSelectAccept = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventListSelectAcceptTranslator* EventListSelectAcceptTranslator_Create(MyGUI::MultiListBox* widget, EventListSelectAcceptTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventListSelectAcceptTranslator* EventListSelectAcceptTranslator_Create(MyGUI::MultiListBox* widget, EventListSelectAcceptTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventListSelectAcceptTranslator(widget, nativeEventCallback);
+	return new EventListSelectAcceptTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
