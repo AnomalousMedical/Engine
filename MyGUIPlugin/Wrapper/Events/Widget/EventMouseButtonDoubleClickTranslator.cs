1fa353b1681319c44636d62ded1a13a7465ad98e
Andrew Piper <threax@users.noreply.github.com>
2015-01-26 21:33:19 +0000

    Added AOT for EventMouseButtonDoubleClickTranslator.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2562 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Wrapper/Events/Widget/EventMouseButtonDoubleClickTranslator.cs
+++ MyGUIPlugin/Wrapper/Events/Widget/EventMouseButtonDoubleClickTranslator.cs
@@ -9,25 +9,22 @@ namespace MyGUIPlugin
 {
     class EventMouseButtonDoubleClickTranslator : MyGUIWidgetEventTranslator
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        delegate void NativeEventDelegate(IntPtr widget);
-
-        private NativeEventDelegate nativeEventCallback;
+        private CallbackHandler callbackHandler;
 
         public EventMouseButtonDoubleClickTranslator()
         {
-            nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            callbackHandler = new CallbackHandler();
         }
 
         public override void Dispose()
         {
             base.Dispose();
-            nativeEventCallback = null;
+            callbackHandler.Dispose();
         }
 
         protected override IntPtr doInitialize(Widget widget)
         {
-            return EventMouseButtonDoubleClickTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            return callbackHandler.create(this, widget);
         }
 
         private void nativeEvent(IntPtr widget)
@@ -38,7 +35,67 @@ namespace MyGUIPlugin
         #region PInvoke
 
         [DllImport(MyGUIInterface.LibraryName, CallingConvention=CallingConvention.Cdecl)]
-        private static extern IntPtr EventMouseButtonDoubleClickTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback);
+        private static extern IntPtr EventMouseButtonDoubleClickTranslator_Create(IntPtr widget, NativeEventDelegate nativeEventCallback
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void NativeEventDelegate(IntPtr widget
+#if FULL_AOT_COMPILE
+, IntPtr instanceHandle
+#endif
+);
+
+#if FULL_AOT_COMPILE
+        class CallbackHandler : IDisposable
+        {
+            private static NativeEventDelegate nativeEventCallback;
+
+            static CallbackHandler()
+            {
+                nativeEventCallback = new NativeEventDelegate(nativeEvent);
+            }
+
+            [MonoTouch.MonoPInvokeCallback(typeof(NativeEventDelegate))]
+            private static void nativeEvent(IntPtr widget, IntPtr instanceHandle)
+            {
+                GCHandle handle = GCHandle.FromIntPtr(instanceHandle);
+                (handle.Target as EventMouseButtonDoubleClickTranslator).nativeEvent(widget);
+            }
+
+            private GCHandle handle;
+
+            public IntPtr create(EventMouseButtonDoubleClickTranslator obj, Widget widget)
+            {
+                handle = GCHandle.Alloc(obj);
+                return EventMouseButtonDoubleClickTranslator_Create(widget.WidgetPtr, nativeEventCallback, GCHandle.ToIntPtr(handle));
+            }
+
+            public void Dispose()
+            {
+                handle.Free();
+                nativeEventCallback = null;
+            }
+        }
+#else
+        class CallbackHandler : IDisposable
+        {
+            private NativeEventDelegate nativeEventCallback;
+
+            public IntPtr create(EventMouseButtonDoubleClickTranslator obj, Widget widget)
+            {
+                nativeEventCallback = new NativeEventDelegate(obj.nativeEvent);
+                return EventMouseButtonDoubleClickTranslator_Create(widget.WidgetPtr, nativeEventCallback);
+            }
+
+            public void Dispose()
+            {
+                nativeEventCallback = null;
+            }
+        }
+#endif
 
         #endregion
     }
--- MyGUIWrapper/Src/EventMouseButtonDoubleClickTranslator.cpp
+++ MyGUIWrapper/Src/EventMouseButtonDoubleClickTranslator.cpp
@@ -4,16 +4,25 @@
 class EventMouseButtonDoubleClickTranslator : public MyGUIEventTranslator
 {
 public:
-	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender);
+	typedef void (*NativeEventDelegate)(MyGUI::Widget* sender HANDLE_ARG);
 
 private:
 	MyGUI::Widget* widget;
 	NativeEventDelegate nativeEvent;
+	HANDLE_INSTANCE
+
+#ifdef FULL_AOT_COMPILE
+	void fireEvent(MyGUI::Widget* sender)
+	{
+		nativeEvent(sender PASS_HANDLE_ARG);
+	}
+#endif
 
 public:
-	EventMouseButtonDoubleClickTranslator(MyGUI::Widget* widget, EventMouseButtonDoubleClickTranslator::NativeEventDelegate nativeEventCallback)
+	EventMouseButtonDoubleClickTranslator(MyGUI::Widget* widget, EventMouseButtonDoubleClickTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 		:widget(widget),
 		nativeEvent(nativeEventCallback)
+		ASSIGN_HANDLE_INITIALIZER
 	{
 
 	}
@@ -25,7 +34,11 @@ public:
 
 	virtual void bindEvent()
 	{
+#ifdef FULL_AOT_COMPILE
+		widget->eventMouseButtonDoubleClick = MyGUI::newDelegate(this, &EventMouseButtonDoubleClickTranslator::fireEvent);
+#else
 		widget->eventMouseButtonDoubleClick = MyGUI::newDelegate(nativeEvent);
+#endif
 	}
 
 	virtual void unbindEvent()
@@ -34,7 +47,7 @@ public:
 	}
 };
 
-extern "C" _AnomalousExport EventMouseButtonDoubleClickTranslator* EventMouseButtonDoubleClickTranslator_Create(MyGUI::Widget* widget, EventMouseButtonDoubleClickTranslator::NativeEventDelegate nativeEventCallback)
+extern "C" _AnomalousExport EventMouseButtonDoubleClickTranslator* EventMouseButtonDoubleClickTranslator_Create(MyGUI::Widget* widget, EventMouseButtonDoubleClickTranslator::NativeEventDelegate nativeEventCallback HANDLE_ARG)
 {
-	return new EventMouseButtonDoubleClickTranslator(widget, nativeEventCallback);
+	return new EventMouseButtonDoubleClickTranslator(widget, nativeEventCallback PASS_HANDLE_ARG);
 }
