bf15aa32b7ff3379a4b63ea9ab24338c7da4a0be
Andrew Piper <threax@users.noreply.github.com>
2015-01-27 16:43:05 +0000

    Added AOT stringretriever to ConnectionValidator_iOS.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2576 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OSHelper/Src/ConnectionValidator_iOS.mm
+++ OSHelper/Src/ConnectionValidator_iOS.mm
@@ -6,20 +6,20 @@ extern "C" _AnomalousExport bool CertificateValidator_ValidateSSLCertificate(uns
     return true;
 }
 
-extern "C" _AnomalousExport void MacPlatformConfig_getLocalUserDocumentsFolder(StringRetrieverCallback retrieve)
+extern "C" _AnomalousExport void MacPlatformConfig_getLocalUserDocumentsFolder(StringRetrieverCallback retrieve, void* handle)
 {
     NSString* path = @"Documents/";
-    retrieve([path UTF8String]);
+    retrieve([path UTF8String], handle);
 }
 
-extern "C" _AnomalousExport void MacPlatformConfig_getLocalDataFolder(StringRetrieverCallback retrieve)
+extern "C" _AnomalousExport void MacPlatformConfig_getLocalDataFolder(StringRetrieverCallback retrieve, void* handle)
 {
     NSString* path = @"Library/Application Support";
-    retrieve([path UTF8String]);
+    retrieve([path UTF8String], handle);
 }
 
-extern "C" _AnomalousExport void MacPlatformConfig_getLocalPrivateDataFolder(StringRetrieverCallback retrieve)
+extern "C" _AnomalousExport void MacPlatformConfig_getLocalPrivateDataFolder(StringRetrieverCallback retrieve, void* handle)
 {
     NSString* path = @"Library/Application Support";
-    retrieve([path UTF8String]);
+    retrieve([path UTF8String], handle);
 }
