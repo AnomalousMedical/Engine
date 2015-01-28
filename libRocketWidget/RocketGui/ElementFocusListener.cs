4794ad3ab4c6b5be426478af45d8d50dcb15469a
Andrew Piper <threax@users.noreply.github.com>
2015-01-28 13:43:20 +0000

    Added missing ElementFocusListener from previous commit.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2583 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ libRocketWidget/RocketGui/ElementFocusListener.cs
@@ -0,0 +1,17 @@
+﻿using libRocketPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anomalous.libRocketWidget
+{
+    class ElementFocusListener : EventListener
+    {
+        public override void ProcessEvent(Event evt)
+        {
+            RocketWidget.fireElementFocused(evt.TargetElement);
+        }
+    }
+}
