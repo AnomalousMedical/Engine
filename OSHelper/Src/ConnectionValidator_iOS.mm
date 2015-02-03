62df3ed051ca2de0a24695c2366c95741c91d677
Andrew Piper <threax@users.noreply.github.com>
2015-02-03 19:37:57 +0000

    Validating connections works pretty much the same as on osx. Used that code.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2599 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OSHelper/Src/ConnectionValidator_iOS.mm
+++ OSHelper/Src/ConnectionValidator_iOS.mm
@@ -1,9 +1,76 @@
 #include "StdAfx.h"
 #import <Foundation/Foundation.h>
 
+OSStatus EvaluateCert (SecCertificateRef cert, CFTypeRef policyRef, SecTrustResultType *result, SecTrustRef *pTrustRef)
+{
+    OSStatus status1;
+    OSStatus status2;
+    
+    const void* evalCertArray[] = { cert };
+    CFArrayRef cfCertRef = CFArrayCreate(kCFAllocatorDefault, evalCertArray, 1, NULL);
+    
+//    if (!cfCertRef)
+//        return memFullErr;
+    
+    status1 = SecTrustCreateWithCertificates(cfCertRef, policyRef, pTrustRef);
+    if (status1)
+        return status1;
+    
+    status2 = SecTrustEvaluate (*pTrustRef, result);
+    
+    // Release the objects we allocated
+    if (cfCertRef)
+        CFRelease(cfCertRef);
+    //if (cfDate)
+    //    CFRelease(cfDate);
+    
+    return (status2);
+}
+
 extern "C" _AnomalousExport bool CertificateValidator_ValidateSSLCertificate(unsigned char* certBytes, unsigned int certBytesLength, String hostName)
 {
-    return true; //IOS_FIXLATER Need to actually implement, but need this to work for now.
+    //NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    SecTrustRef trustRef = nil;
+    SecTrustResultType result = kSecTrustResultRecoverableTrustFailure;
+    bool success = false;
+    
+    CFStringRef cfHostName = NULL;
+    if(hostName != NULL)
+    {
+        cfHostName = CFStringCreateWithFormat(kCFAllocatorDefault, NULL, CFSTR("%S"), hostName);
+        if(cfHostName == NULL)
+        {
+            return false; //Fail if a host is provided, but cannot be translated. This prevents us trusting something we shouldn't if there is an error here.
+        }
+    }
+    
+    NSData *certData = [NSData dataWithBytes:certBytes length:certBytesLength];
+    SecCertificateRef cert = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)certData);
+    if(cert != nil)
+    {
+        SecPolicyRef policyRef = SecPolicyCreateSSL(false, cfHostName);
+        EvaluateCert(cert, policyRef, &result, &trustRef);
+        success = result == kSecTrustResultUnspecified;
+        
+        if(trustRef != nil)
+        {
+            CFRelease(trustRef);
+        }
+        
+        CFRelease(policyRef);
+    }
+    
+    if(cfHostName != NULL)
+    {
+        CFRelease(cfHostName);
+    }
+    
+    CFRelease(cert);
+    
+    //[pool drain];
+    
+    return success;
 }
 
 extern "C" _AnomalousExport void MacPlatformConfig_getLocalUserDocumentsFolder(StringRetrieverCallback retrieve, void* handle)
