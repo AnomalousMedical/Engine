451aa66ce1b44b55fcf95bf1aaa1329509145af7
Andrew Piper <threax@users.noreply.github.com>
2015-02-10 19:06:39 +0000

    Made autobuild work for entire engine. Fixed header search paths for zip project on ios, was looking in osx directories.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2614 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- AutoBuild_iOS.sh
+++ AutoBuild_iOS.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#The iOS version has to be built through this script for now.
+
 #Discover some stuff about where we are.
 START_PATH=$(pwd)
 THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -13,6 +15,14 @@ SOLUTION_PATH=$THIS_FOLDER/$SOLUTION_NAME
 #cleaning doesn't work for some reason
 #xbuild "$SOLUTION_PATH" /p:Configuration=Release /t:clean
 xbuild "$SOLUTION_PATH" /p:Configuration=Release /p:DefineConstants="STATIC_LINK FULL_AOT_COMPILE LITE_EMBEDDED_LIB"
+
+sh $THIS_FOLDER/BulletWrapper/iOS/AutoBuild_iOS.sh
+sh $THIS_FOLDER/libRocketWrapper/iOS/AutoBuild_iOS.sh
+sh $THIS_FOLDER/MyGUIWrapper/iOS/AutoBuild_iOS.sh
+sh $THIS_FOLDER/OgreCWrapper/iOS/AutoBuild_iOS.sh
+sh $THIS_FOLDER/OSHelper/iOS/AutoBuild_iOS.sh
+sh $THIS_FOLDER/SoundWrapper/iOS/AutoBuild_iOS.sh
+sh $THIS_FOLDER/Zip/iOS/AutoBuild_iOS.sh
 
 #Finish up
 cd "$START_PATH"
