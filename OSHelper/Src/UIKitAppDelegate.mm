c8b8abc12ee0ca098eacb836103e49afa330c917
Andrew Piper <threax@users.noreply.github.com>
2015-02-16 14:47:49 +0000

    Fired foreground and background events.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2624 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OSHelper/Src/UIKitAppDelegate.mm
+++ OSHelper/Src/UIKitAppDelegate.mm
@@ -26,7 +26,6 @@ void UIKitAppDelegate_setPrimaryUIKitApp(UIKitApp* app)
 
 @implementation UIKitAppDelegate
 
-//Better but not quite right yet.
 -(void) doFrame:(id)data
 {
     primaryUiKitApp->fireIdle();
@@ -53,6 +52,7 @@ void UIKitAppDelegate_setPrimaryUIKitApp(UIKitApp* app)
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     [self.mFrameLink removeFromRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    primaryUiKitApp->fireMovedToBackground();
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -67,6 +67,7 @@ void UIKitAppDelegate_setPrimaryUIKitApp(UIKitApp* app)
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     [self.mFrameLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    primaryUiKitApp->fireMovedToForeground();
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
