d6ea5c51f1ab228a6572aa8ffdb612bee0611ef1
Andrew Piper <threax@users.noreply.github.com>
2015-03-18 20:34:24 +0000

    Removed opengl references from android app.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2684 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OSHelper/Android/jni/Android.mk
+++ OSHelper/Android/jni/Android.mk
@@ -5,7 +5,7 @@ PRJ_SRC		    := /../../Src
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libOSHelper
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_LDLIBS    := -llog -landroid
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
 LOCAL_SRC_FILES := \
--- OSHelper/Src/AndroidApp.cpp
+++ OSHelper/Src/AndroidApp.cpp
@@ -19,9 +19,6 @@
 #include <jni.h>
 #include <errno.h>
 
-#include <EGL/egl.h>
-#include <GLES/gl.h>
-
 #include <android/log.h>
 #include <android_native_app_glue.h>
 
@@ -34,7 +31,6 @@
 struct engine 
 {
 	struct android_app* app;
-
 	int animating;
 };
 
