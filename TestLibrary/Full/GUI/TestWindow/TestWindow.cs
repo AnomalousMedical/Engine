a2aede32ae669ae2f289394312c2d2d9dc8c5cfb
Andrew Piper <threax@users.noreply.github.com>
2015-03-27 16:59:27 +0000

    Added test sound playback.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2740 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- TestLibrary/Full/GUI/TestWindow/TestWindow.cs
+++ TestLibrary/Full/GUI/TestWindow/TestWindow.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Anomalous.GuiFramework;
+using MyGUIPlugin;
+using SoundPlugin;
 
 namespace Anomalous.Minimus.Full.GUI
 {
@@ -8,7 +10,14 @@ namespace Anomalous.Minimus.Full.GUI
 		public TestWindow ()
             : base("Anomalous.Minimus.Full.GUI.TestWindow.TestWindow.layout")
 		{
+            Button button = window.findWidget("PlayTestSound") as Button;
+            button.MouseButtonClick += button_MouseButtonClick;
 		}
+
+        void button_MouseButtonClick(Widget source, EventArgs e)
+        {
+            SoundPlugin.SoundPluginInterface.Instance.SoundManager.streamPlayAndForgetSound(GetType().Assembly.GetManifestResourceStream("Anomalous.Minimus.Full.GUI.TestWindow.PiperVb.ogg"));
+        }
 	}
 }
 
