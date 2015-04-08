01f2116ffca8bc88270daf2981cda56960e3638a
Andrew Piper <threax@users.noreply.github.com>
2015-04-08 14:41:00 +0000

    Moved TouchMouseGuiForwarder to OSPlatform.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2772 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- MyGUIPlugin/Plugin/MyGUIOnscreenKeyboardManager.cs
+++ MyGUIPlugin/Plugin/MyGUIOnscreenKeyboardManager.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Anomalous.Minimus
+namespace MyGUIPlugin
 {
     public class MyGUIOnscreenKeyboardManager
     {
--- OSPlatform/TouchMouseGuiForwarder.cs
+++ OSPlatform/TouchMouseGuiForwarder.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Anomalous.Minimus
+namespace Anomalous.OSPlatform
 {
     public class TouchMouseGuiForwarder : OnscreenKeyboardManager
     {
