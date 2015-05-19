ff67af1f263acae8d1e0645635249711b00c5ab9
Andrew Piper <threax@users.noreply.github.com>
2015-05-19 15:50:13 +0000

    Added a nice function to prettify strings.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2822 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Math/Numerics/Prettify.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Prettify
+    {
+        /// <summary>
+        /// Returns the human-readable file size for an arbitrary, 64-bit file size
+        /// The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
+        /// </summary>
+        /// <param name="i">The input size</param>
+        /// <returns>The string with the appropriate format.</returns>
+        /// <remarks>Taken from http://www.somacon.com/p576.php </remarks>
+        public static string GetSizeReadable(long i)
+        {
+            string sign = (i < 0 ? "-" : "");
+            double readable = (i < 0 ? -i : i);
+            string suffix;
+            if (i >= 0x1000000000000000) // Exabyte
+            {
+                suffix = "EB";
+                readable = (double)(i >> 50);
+            }
+            else if (i >= 0x4000000000000) // Petabyte
+            {
+                suffix = "PB";
+                readable = (double)(i >> 40);
+            }
+            else if (i >= 0x10000000000) // Terabyte
+            {
+                suffix = "TB";
+                readable = (double)(i >> 30);
+            }
+            else if (i >= 0x40000000) // Gigabyte
+            {
+                suffix = "GB";
+                readable = (double)(i >> 20);
+            }
+            else if (i >= 0x100000) // Megabyte
+            {
+                suffix = "MB";
+                readable = (double)(i >> 10);
+            }
+            else if (i >= 0x400) // Kilobyte
+            {
+                suffix = "KB";
+                readable = (double)i;
+            }
+            else
+            {
+                return i.ToString(sign + "0 B"); // Byte
+            }
+            readable = readable / 1024;
+
+            return sign + readable.ToString("0.### ") + suffix;
+        }
+    }
+}
