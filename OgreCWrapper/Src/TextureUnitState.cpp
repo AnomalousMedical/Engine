c48d3479462b445843399f80a51744da9976b74d
Andrew Piper <threax@users.noreply.github.com>
2015-06-17 20:14:17 +0000

    Added wrappers to set texture filtering modes.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2854 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgreCWrapper/Src/TextureUnitState.cpp
+++ OgreCWrapper/Src/TextureUnitState.cpp
@@ -14,3 +14,18 @@ extern "C" _AnomalousExport String TextureUnitState_getName(Ogre::TextureUnitSta
 {
 	return textureUnit->getName().c_str();
 }
+
+extern "C" _AnomalousExport Ogre::FilterOptions TextureUnitState_getTextureFiltering(Ogre::TextureUnitState* textureUnit, Ogre::FilterType filterType)
+{
+	return textureUnit->getTextureFiltering(filterType);
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureFiltering(Ogre::TextureUnitState* textureUnit, Ogre::FilterType filterType, Ogre::FilterOptions option)
+{
+	return textureUnit->setTextureFiltering(filterType, option);
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureFiltering2(Ogre::TextureUnitState* textureUnit, Ogre::FilterOptions minFilter, Ogre::FilterOptions magFilter, Ogre::FilterOptions mipFilter)
+{
+	return textureUnit->setTextureFiltering(minFilter, magFilter, mipFilter);
+}
--- OgrePlugin/Wrapper/Resources/Materials/TextureUnitState.cs
+++ OgrePlugin/Wrapper/Resources/Materials/TextureUnitState.cs
@@ -6,8 +6,23 @@ using System.Runtime.InteropServices;
 
 namespace OgrePlugin
 {
+    public enum FilterOptions
+    {
+        None,
+        Point,
+        Linear,
+        Anisotropic
+    }
+
     public class TextureUnitState : IDisposable
     {
+        enum FilterType
+        {
+            Min,
+            Mag,
+            Mip
+        }
+
         internal static TextureUnitState createWrapper(IntPtr nativeObject, object[] args)
         {
             return new TextureUnitState(nativeObject);
@@ -45,6 +60,47 @@ namespace OgrePlugin
             }
         }
 
+        public FilterOptions MinFilter
+        {
+            get
+            {
+                return TextureUnitState_getTextureFiltering(textureUnit, FilterType.Min);
+            }
+            set
+            {
+                TextureUnitState_setTextureFiltering(textureUnit, FilterType.Min, value);
+            }
+        }
+
+        public FilterOptions MagFilter
+        {
+            get
+            {
+                return TextureUnitState_getTextureFiltering(textureUnit, FilterType.Mag);
+            }
+            set
+            {
+                TextureUnitState_setTextureFiltering(textureUnit, FilterType.Mag, value);
+            }
+        }
+
+        public FilterOptions MipFilter
+        {
+            get
+            {
+                return TextureUnitState_getTextureFiltering(textureUnit, FilterType.Mip);
+            }
+            set
+            {
+                TextureUnitState_setTextureFiltering(textureUnit, FilterType.Mip, value);
+            }
+        }
+
+        public void setFilteringOptions(FilterOptions minFilter, FilterOptions magFilter, FilterOptions mipFilter)
+        {
+            TextureUnitState_setTextureFiltering2(textureUnit, minFilter, magFilter, mipFilter);
+        }
+
         internal IntPtr OgreState
         {
             get
@@ -63,6 +119,15 @@ namespace OgrePlugin
 
         [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr TextureUnitState_getName(IntPtr textureUnit);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern FilterOptions TextureUnitState_getTextureFiltering(IntPtr textureUnit, FilterType filterType);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureFiltering(IntPtr textureUnit, FilterType filterType, FilterOptions option);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureFiltering2(IntPtr textureUnit, FilterOptions minFilter, FilterOptions magFilter, FilterOptions mipFilter);
 
 #endregion
     }
