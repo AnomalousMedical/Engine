7177294eeec18df87aa3a10000bbfc2be818a2d9
Andrew Piper <threax@users.noreply.github.com>
2015-07-10 18:05:55 +0000

    Added an exception to be used to easily cancel a thread.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2880 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

---
+++ Engine/Threads/CancelThreadException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Threads
+{
+    /// <summary>
+    /// A convience class to cancel a thread with an exception. You can catch this in your outermost
+    /// thread function and use it to cancel those calls.
+    /// </summary>
+    public class CancelThreadException : Exception
+    {
+        public CancelThreadException()
+        {
+
+        }
+    }
+}
