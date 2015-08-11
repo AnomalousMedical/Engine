72bd1923789c37cfd2d6cb85b3b0ac0ce182a65d
Andrew Piper <threax@users.noreply.github.com>
2015-08-11 18:52:52 +0000

    Added virtual texturing to gles shaders.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@2958 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgrePlugin/Resources/Unified/glsles/NormalMapSpecularOpacityMapFP.glsl
+++ OgrePlugin/Resources/Unified/glsles/NormalMapSpecularOpacityMapFP.glsl
@@ -11,6 +11,14 @@ uniform vec4 emissiveColor;				//The emissive color of the surface
 	uniform vec4 alpha;
 #endif
 
+#ifdef VIRTUAL_TEXTURE
+	uniform sampler2D indirectionTex;
+	uniform vec2 physicalSizeRecip;
+	uniform vec2 mipBiasSize;
+	uniform vec2 pagePaddingScale;
+	uniform vec2 pagePaddingOffset;
+#endif
+
 //Textures
 uniform sampler2D normalTexture;	//The normal map
 uniform sampler2D colorTexture;  //The color map
@@ -74,23 +82,68 @@ vec3 unpack(vec3 packValue)
 	return 2.0 * (packValue - 0.5);
 }
 
+#ifdef VIRTUAL_TEXTURE
+
+#extension GL_OES_standard_derivatives : require
+
+float texMipLevel(vec2 coord, vec2 texSize)
+{
+	vec2 dxScaled, dyScaled;
+	vec2 coord_scaled = coord * texSize;
+
+	dxScaled = dFdx(coord_scaled);
+	dyScaled = dFdy(coord_scaled);
+
+	vec2 dtex = dxScaled*dxScaled + dyScaled*dyScaled;
+	float minDelta = max(dtex.x, dtex.y);
+	float miplevel = max(0.5 * log2(minDelta), 0.0);
+
+	return miplevel;
+}
+
+vec2 vtexCoord(vec2 address, sampler2D indirectionTex, vec2 physicalSizeRecip, vec2 mipBiasSize, vec2 pagePaddingScale, vec2 pagePaddingOffset)
+{
+	float mipLevel = texMipLevel(address, mipBiasSize);
+
+	//Need to add bias for mip levels, the bias adjusts the size of the indirection texture to the size of the physical texture
+	vec4 redirectInfo = texture2D(indirectionTex, address.xy); //Need to do mip level here
+
+	float mip2 = floor(exp2(redirectInfo.b * 255.0) + 0.5); //Figure out how far to shift the original address, based on the mip level, highest mip level (1x1 indirection texture) is 0 counting up from there
+	vec2 coordLow = fract(address * mip2); //Get fractional part of page location, this is shifted left by the mip level
+
+	vec2 page = floor(redirectInfo.rg * 255.0 + 0.5); //Get the page number on the physical texture
+
+	vec2 finalCoord = page + coordLow * pagePaddingScale + pagePaddingOffset; //Add these together to get the coords in page space 64.0 / 66.0  1.0 / 66.0
+	finalCoord = finalCoord * physicalSizeRecip; //Multiple by the physical texture page size to get back to uv space, that is our final coord
+	
+	return finalCoord;
+}
+
+#endif
+
 //----------------------------Main Shader----------------------------
 
 void main()
 {
+	vec2 derivedCoords = texCoords;
+
+	#ifdef VIRTUAL_TEXTURE
+		derivedCoords = vtexCoord(texCoords, indirectionTex, physicalSizeRecip, mipBiasSize, pagePaddingScale, pagePaddingOffset);
+	#endif
+
 	///Get color value
-	vec4 colorMap = texture2D(colorTexture, texCoords.xy);
+	vec4 colorMap = texture2D(colorTexture, derivedCoords.xy);
 
 	//Unpack the normal map.
 	vec3 normal;
-	normal.rg = 2.0 * (texture2D(normalTexture, texCoords).ag - 0.5);
+	normal.rg = 2.0 * (texture2D(normalTexture, derivedCoords).ag - 0.5);
 	normal.b = sqrt(1.0 - normal.r * normal.r - normal.g * normal.g);
 
 	gl_FragColor = doLighting(unpack(lightVector), unpack(halfVector), lightDiffuseColor, attenuation, colorMap, specularColor, colorMap.a, glossyness, emissiveColor, normal);
 
 #ifdef ALPHA
-	gl_FragColor.a = texture2D(opacityTexture, texCoords).r - (1.0 - alpha.a);
+	gl_FragColor.a = texture2D(opacityTexture, derivedCoords).r - (1.0 - alpha.a);
 #else
-	gl_FragColor.a = texture2D(opacityTexture, texCoords).r;
+	gl_FragColor.a = texture2D(opacityTexture, derivedCoords).r;
 #endif
 }
