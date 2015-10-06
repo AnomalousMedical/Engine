eae317a85d5f40f5d3f308f0ebd9e5c7ca2b9164
Andrew Piper <threax@users.noreply.github.com>
2015-10-06 14:08:28 +0000

    Changed vignette to use uv coords instead of screen space.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@3177 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgrePlugin/Resources/Basic/D3D11/Vignette.hlsl
+++ OgrePlugin/Resources/Basic/D3D11/Vignette.hlsl
@@ -1,19 +1,9 @@
 void main( uniform float4 bgColor,
-		   uniform float resX,
-		   uniform float resY,
-
-		   float4 fragCoord : SV_POSITION,
-			
-			out float4 outColor : SV_TARGET ) 
+		   in float4 inPosition : SV_POSITION, 
+		   in float2 inTexcoord : TEXCOORD0, 
+		   out float4 outColor : SV_TARGET ) 
 {
-	float2 resolution = float2(resX, resY);
-
-    //determine origin
-    float2 position = (fragCoord.xy / resolution.xy) - float2(0.5, 0.5);
-
-    //determine the vector length of the center position
+    float2 position = inTexcoord - float2(0.5, 0.5);
     float len = length(position);
-
-    //show our length for debugging
     outColor = float4( bgColor.rgb * (1.0 - len), 1.0 );
 }
--- OgrePlugin/Resources/Basic/glsles/Vignette.glsl
+++ OgrePlugin/Resources/Basic/glsles/Vignette.glsl
@@ -1,22 +1,14 @@
 precision mediump float;
 
 uniform vec4 bgColor;
-uniform float resX;
-uniform float resY;
+varying vec2 texCoords;
 
 //----------------------------------
 //Measurement Fragment Program
 //----------------------------------
 void main(void)
 {
-	vec2 resolution = vec2(resX, resY);
-
-    //determine origin
-    vec2 position = (gl_FragCoord.xy / resolution.xy) - vec2(0.5);
-
-    //determine the vector length of the center position
+    vec2 position = texCoords - vec2(0.5);
     float len = length(position);
-
-    //show our length for debugging
     gl_FragColor = vec4( bgColor.rgb * (1.0 - len), 1.0 );
 }
