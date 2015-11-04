a195e9a5d89e15ff78ab88ce8b694439bf065a93
Andrew Piper <threax@users.noreply.github.com>
2015-11-04 16:38:19 +0000

    Added adressing modes to TextureUnitState.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@3226 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- OgreCWrapper/Src/TextureUnitState.cpp
+++ OgreCWrapper/Src/TextureUnitState.cpp
@@ -29,3 +29,46 @@ extern "C" _AnomalousExport void TextureUnitState_setTextureFiltering2(Ogre::Tex
 {
 	return textureUnit->setTextureFiltering(minFilter, magFilter, mipFilter);
 }
+
+extern "C" _AnomalousExport Ogre::TextureUnitState::TextureAddressingMode TextureUnitState_getTextureAddressingModeU(Ogre::TextureUnitState* textureUnit)
+{
+	return textureUnit->getTextureAddressingMode().u;
+}
+
+extern "C" _AnomalousExport Ogre::TextureUnitState::TextureAddressingMode TextureUnitState_getTextureAddressingModeV(Ogre::TextureUnitState* textureUnit)
+{
+	return textureUnit->getTextureAddressingMode().v;
+}
+
+extern "C" _AnomalousExport Ogre::TextureUnitState::TextureAddressingMode TextureUnitState_getTextureAddressingModeW(Ogre::TextureUnitState* textureUnit)
+{
+	return textureUnit->getTextureAddressingMode().w;
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureAddressingModeU(Ogre::TextureUnitState* textureUnit, Ogre::TextureUnitState::TextureAddressingMode tam)
+{
+	Ogre::TextureUnitState::UVWAddressingMode mode = textureUnit->getTextureAddressingMode();
+	return textureUnit->setTextureAddressingMode(tam, mode.v, mode.w);
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureAddressingModeV(Ogre::TextureUnitState* textureUnit, Ogre::TextureUnitState::TextureAddressingMode tam)
+{
+	Ogre::TextureUnitState::UVWAddressingMode mode = textureUnit->getTextureAddressingMode();
+	return textureUnit->setTextureAddressingMode(mode.u, tam, mode.w);
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureAddressingModeW(Ogre::TextureUnitState* textureUnit, Ogre::TextureUnitState::TextureAddressingMode tam)
+{
+	Ogre::TextureUnitState::UVWAddressingMode mode = textureUnit->getTextureAddressingMode();
+	return textureUnit->setTextureAddressingMode(mode.u, mode.v, tam);
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureAddressingModeUVW(Ogre::TextureUnitState* textureUnit, Ogre::TextureUnitState::TextureAddressingMode u, Ogre::TextureUnitState::TextureAddressingMode v, Ogre::TextureUnitState::TextureAddressingMode w)
+{
+	return textureUnit->setTextureAddressingMode(u, v, w);
+}
+
+extern "C" _AnomalousExport void TextureUnitState_setTextureAddressingMode(Ogre::TextureUnitState* textureUnit, Ogre::TextureUnitState::TextureAddressingMode tam)
+{
+	return textureUnit->setTextureAddressingMode(tam);
+}
--- OgrePlugin/Wrapper/Resources/Materials/TextureUnitState.cs
+++ OgrePlugin/Wrapper/Resources/Materials/TextureUnitState.cs
@@ -6,6 +6,20 @@ using System.Runtime.InteropServices;
 
 namespace OgrePlugin
 {
+    public enum TextureAddressingMode
+    {
+        /// Texture wraps at values over 1.0.
+        TAM_WRAP,
+        /// Texture mirrors (flips) at joins over 1.0.
+        TAM_MIRROR,
+        /// Texture clamps at 1.0.
+        TAM_CLAMP,
+        /// Texture coordinates outside the range [0.0, 1.0] are set to the border colour.
+        TAM_BORDER,
+        /// Unknown
+        TAM_UNKNOWN = 99
+    };
+
     public enum FilterOptions
     {
         None,
@@ -96,6 +110,52 @@ namespace OgrePlugin
             }
         }
 
+        public TextureAddressingMode AddressModeU
+        {
+            get
+            {
+                return TextureUnitState_getTextureAddressingModeU(textureUnit);
+            }
+            set
+            {
+                TextureUnitState_setTextureAddressingModeU(textureUnit, value);
+            }
+        }
+
+        public TextureAddressingMode AddressModeV
+        {
+            get
+            {
+                return TextureUnitState_getTextureAddressingModeV(textureUnit);
+            }
+            set
+            {
+                TextureUnitState_setTextureAddressingModeV(textureUnit, value);
+            }
+        }
+
+        public TextureAddressingMode AddressModeW
+        {
+            get
+            {
+                return TextureUnitState_getTextureAddressingModeW(textureUnit);
+            }
+            set
+            {
+                TextureUnitState_setTextureAddressingModeW(textureUnit, value);
+            }
+        }
+
+        public void setAdressingMode(TextureAddressingMode u, TextureAddressingMode v, TextureAddressingMode w)
+        {
+            TextureUnitState_setTextureAddressingModeUVW(textureUnit, u, v, w);
+        }
+
+        public void setAdressingMode(TextureAddressingMode tam)
+        {
+            TextureUnitState_setTextureAddressingMode(textureUnit, tam);
+        }
+
         public void setFilteringOptions(FilterOptions minFilter, FilterOptions magFilter, FilterOptions mipFilter)
         {
             TextureUnitState_setTextureFiltering2(textureUnit, minFilter, magFilter, mipFilter);
@@ -128,6 +188,30 @@ namespace OgrePlugin
 
         [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
         private static extern void TextureUnitState_setTextureFiltering2(IntPtr textureUnit, FilterOptions minFilter, FilterOptions magFilter, FilterOptions mipFilter);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern TextureAddressingMode TextureUnitState_getTextureAddressingModeU(IntPtr textureUnit);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern TextureAddressingMode TextureUnitState_getTextureAddressingModeV(IntPtr textureUnit);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern TextureAddressingMode TextureUnitState_getTextureAddressingModeW(IntPtr textureUnit);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureAddressingModeU(IntPtr textureUnit, TextureAddressingMode tam);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureAddressingModeV(IntPtr textureUnit, TextureAddressingMode tam);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureAddressingModeW(IntPtr textureUnit, TextureAddressingMode tam);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureAddressingModeUVW(IntPtr textureUnit, TextureAddressingMode u, TextureAddressingMode v, TextureAddressingMode w);
+
+        [DllImport(LibraryInfo.Name, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TextureUnitState_setTextureAddressingMode(IntPtr textureUnit, TextureAddressingMode tam);
 
 #endregion
     }
