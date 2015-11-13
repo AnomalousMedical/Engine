070d8d80269d84c0ddf4eb11d5001a9d10245942
Andrew Piper <threax@users.noreply.github.com>
2015-11-13 16:04:41 +0000

    Added mesh volume calculation.
    git-svn-id: https://192.168.3.139:8443/svn/Engine/trunk@3246 146ae76c-2b19-ef4c-9a69-67eb2b888ad7

--- Engine/Math/Shapes/Geometry.cs
+++ Engine/Math/Shapes/Geometry.cs
@@ -78,5 +78,32 @@ namespace Engine
         {
             return Math.Abs(SignedAreaOfPolygon(points));
         }
+
+        public static float SignedVolumeOfTetrahedron(Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            float v321 = p3.x * p2.y * p1.z;
+            float v231 = p2.x * p3.y * p1.z;
+            float v312 = p3.x * p1.y * p2.z;
+            float v132 = p1.x * p3.y * p2.z;
+            float v213 = p2.x * p1.y * p3.z;
+            float v123 = p1.x * p2.y * p3.z;
+            return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
+        }
+        public static float VolumeOfMesh(Vector3[] vertices, int[] indices)
+        {
+            float volume = 0;
+            int numTriangles = indices.Length;
+            for (int i = 0; i < numTriangles; i += 3)
+         {
+                Vector3 p1 = vertices[indices[i + 0]];
+                Vector3 p2 = vertices[indices[i + 1]];
+                Vector3 p3 = vertices[indices[i + 2]];
+                volume += SignedVolumeOfTetrahedron(p1, p2, p3);
+            }
+            return Math.Abs(volume);
+        }
+
+
+
     }
 }
--- EngineTest/GeometryTests.cs
+++ EngineTest/GeometryTests.cs
@@ -13,8 +13,8 @@ namespace EngineTest
 
             //Clockwise triangle
             Assert.AreEqual(2.5f, Geometry.SignedAreaOfTriangle(
-                new Vector2(0, 0), 
-                new Vector2(3, 1), 
+                new Vector2(0, 0),
+                new Vector2(3, 1),
                 new Vector2(1, 2)));
 
             Assert.AreEqual(2.5f, Geometry.SignedAreaOfTriangle(
@@ -35,8 +35,8 @@ namespace EngineTest
         [TestMethod]
         public void AreaOfPolygon()
         {
-            //Clockwise rectangle
-            Assert.AreEqual(-4.0f, Geometry.SignedAreaOfPolygon(new Vector2[] 
+            //Clockwise rectangle 2x2
+            Assert.AreEqual(-4.0f, Geometry.SignedAreaOfPolygon(new Vector2[]
             {
                 new Vector2(0, 2),
                 new Vector2(2, 2),
@@ -44,8 +44,8 @@ namespace EngineTest
                 new Vector2(0, 0)
             }));
 
-            //Counter-Clockwise rectangle
-            Assert.AreEqual(4.0f, Geometry.SignedAreaOfPolygon(new Vector2[] 
+            //Counter-Clockwise rectangle 2x2
+            Assert.AreEqual(4.0f, Geometry.SignedAreaOfPolygon(new Vector2[]
             {
                 new Vector2(0, 2),
                 new Vector2(0, 0),
@@ -53,7 +53,7 @@ namespace EngineTest
                 new Vector2(2, 2),
             }));
 
-            //Counter-Clockwise rectangle
+            //Counter-Clockwise rectangle 2x2
             Assert.AreEqual(4.0f, Geometry.SignedAreaOfPolygon(new Vector2[]
             {
                 new Vector2(-1, 1),
@@ -61,6 +61,61 @@ namespace EngineTest
                 new Vector2(1, -1),
                 new Vector2(1, 1),
             }));
+
+            //Clockwise parallelogram 3x1
+            Assert.AreEqual(-3.0f, Geometry.SignedAreaOfPolygon(new Vector2[]
+            {
+                new Vector2(-1, 1),
+                new Vector2(2, 1),
+                new Vector2(1, 0),
+                new Vector2(-2, 0)
+            }));
+        }
+
+        [TestMethod]
+        public void VolumeOfMesh()
+        {
+            //Cube 2x2x2
+            var vertices = new Vector3[]
+            {
+                new Vector3(-1,  1, -1), //0
+                new Vector3( 1,  1, -1), //1
+                new Vector3( 1,  1,  1), //2
+                new Vector3(-1,  1,  1), //3
+                new Vector3(-1, -1, -1), //4
+                new Vector3( 1, -1, -1), //5
+                new Vector3( 1, -1,  1), //6
+                new Vector3(-1, -1,  1), //7
+            };
+
+            var indices = new int[]
+            {
+                //Top
+                0, 1, 2,
+                2, 3, 0,
+
+                //Back
+                1, 0, 4,
+                4, 5, 1,
+
+                //Right
+                2, 1, 5,
+                5, 6, 2,
+
+                //Left
+                0, 3, 7,
+                7, 4, 0,
+
+                //Front
+                3, 2, 6,
+                6, 7, 3,
+
+                //Bottom
+                7, 6, 5,
+                5, 4, 7
+            };
+
+            Assert.IsTrue(Math.Abs(8.0f - Geometry.VolumeOfMesh(vertices, indices)) < 0.0001f);
         }
     }
 }
