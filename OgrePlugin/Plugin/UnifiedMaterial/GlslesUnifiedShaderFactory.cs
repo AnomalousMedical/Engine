a9240d6e464682db34aa1c9f5c67b240a0069186
Andrew Piper <threax@users.noreply.github.com>
2016-05-17 18:24:13 -0400

    Added try catch to glsles factory.

--- OgrePlugin/Plugin/UnifiedMaterial/GlslesUnifiedShaderFactory.cs
+++ OgrePlugin/Plugin/UnifiedMaterial/GlslesUnifiedShaderFactory.cs
@@ -179,60 +179,67 @@ namespace OgrePlugin
 
             using (var defaultParams = program.Value.getDefaultParameters())
             {
-                int textureIndex = 0;
-                if ((maps & TextureMaps.Normal) == TextureMaps.Normal)
+                try
                 {
-                    defaultParams.Value.setNamedConstant("normalTexture", textureIndex++);
-                }
-                if ((maps & TextureMaps.Diffuse) == TextureMaps.Diffuse)
-                {
-                    defaultParams.Value.setNamedConstant("colorTexture", textureIndex++);
-                }
-                if ((maps & TextureMaps.Specular) == TextureMaps.Specular)
-                {
-                    defaultParams.Value.setNamedConstant("specularTexture", textureIndex++);
-                }
-                if ((maps & TextureMaps.Opacity) == TextureMaps.Opacity && separateOpacityMap)
-                {
-                    defaultParams.Value.setNamedConstant("opacityTexture", textureIndex++);
-                }
-                if (maps != TextureMaps.None)
-                {
-                    defaultParams.Value.setNamedConstant("indirectionTex", textureIndex++);
-                }
+                    int textureIndex = 0;
+                    if ((maps & TextureMaps.Normal) == TextureMaps.Normal)
+                    {
+                        defaultParams.Value.setNamedConstant("normalTexture", textureIndex++);
+                    }
+                    if ((maps & TextureMaps.Diffuse) == TextureMaps.Diffuse)
+                    {
+                        defaultParams.Value.setNamedConstant("colorTexture", textureIndex++);
+                    }
+                    if ((maps & TextureMaps.Specular) == TextureMaps.Specular)
+                    {
+                        defaultParams.Value.setNamedConstant("specularTexture", textureIndex++);
+                    }
+                    if ((maps & TextureMaps.Opacity) == TextureMaps.Opacity && separateOpacityMap)
+                    {
+                        defaultParams.Value.setNamedConstant("opacityTexture", textureIndex++);
+                    }
+                    if (maps != TextureMaps.None)
+                    {
+                        defaultParams.Value.setNamedConstant("indirectionTex", textureIndex++);
+                    }
 
-                defaultParams.Value.setNamedAutoConstant("lightDiffuseColor", AutoConstantType.ACT_LIGHT_DIFFUSE_COLOUR, 0);
-                defaultParams.Value.setNamedAutoConstant("emissiveColor", AutoConstantType.ACT_SURFACE_EMISSIVE_COLOUR);
+                    defaultParams.Value.setNamedAutoConstant("lightDiffuseColor", AutoConstantType.ACT_LIGHT_DIFFUSE_COLOUR, 0);
+                    defaultParams.Value.setNamedAutoConstant("emissiveColor", AutoConstantType.ACT_SURFACE_EMISSIVE_COLOUR);
 
-                if (description.HasGlossMap)
-                {
-                    defaultParams.Value.setNamedConstant("glossyStart", 40.0f);
-                    defaultParams.Value.setNamedConstant("glossyRange", 0.0f);
-                }
-                else
-                {
-                    defaultParams.Value.setNamedAutoConstant("glossyness", AutoConstantType.ACT_SURFACE_SHININESS);
-                }
+                    if (description.HasGlossMap)
+                    {
+                        defaultParams.Value.setNamedConstant("glossyStart", 40.0f);
+                        defaultParams.Value.setNamedConstant("glossyRange", 0.0f);
+                    }
+                    else
+                    {
+                        defaultParams.Value.setNamedAutoConstant("glossyness", AutoConstantType.ACT_SURFACE_SHININESS);
+                    }
 
-                if ((maps & TextureMaps.Diffuse) == 0)
-                {
-                    defaultParams.Value.setNamedAutoConstant("diffuseColor", AutoConstantType.ACT_SURFACE_DIFFUSE_COLOUR);
-                }
+                    if ((maps & TextureMaps.Diffuse) == 0)
+                    {
+                        defaultParams.Value.setNamedAutoConstant("diffuseColor", AutoConstantType.ACT_SURFACE_DIFFUSE_COLOUR);
+                    }
 
-                //Check for need to pass specular color
-                if ((maps & TextureMaps.Specular) == 0)
-                {
-                    defaultParams.Value.setNamedAutoConstant("specularColor", AutoConstantType.ACT_SURFACE_SPECULAR_COLOUR);
-                }
+                    //Check for need to pass specular color
+                    if ((maps & TextureMaps.Specular) == 0)
+                    {
+                        defaultParams.Value.setNamedAutoConstant("specularColor", AutoConstantType.ACT_SURFACE_SPECULAR_COLOUR);
+                    }
 
-                if (description.IsHighlight)
-                {
-                    defaultParams.Value.setNamedAutoConstant("highlightColor", AutoConstantType.ACT_CUSTOM, 1);
-                }
+                    if (description.IsHighlight)
+                    {
+                        defaultParams.Value.setNamedAutoConstant("highlightColor", AutoConstantType.ACT_CUSTOM, 1);
+                    }
 
-                if (alpha)
+                    if (alpha)
+                    {
+                        defaultParams.Value.setNamedAutoConstant("alpha", AutoConstantType.ACT_CUSTOM, 0);
+                    }
+                }
+                catch (Exception)
                 {
-                    defaultParams.Value.setNamedAutoConstant("alpha", AutoConstantType.ACT_CUSTOM, 0);
+                    Logging.Log.Warning($"Could not set params for frag program '{name}'");
                 }
             }
 
