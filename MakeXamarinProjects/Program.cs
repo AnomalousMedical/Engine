cc8d0b1bc28ce4472237f1aa1e7089d7ea48a379
Andrew Piper <threax@users.noreply.github.com>
2016-07-07 18:58:31 -0400

    Added portable class library conversion.

--- MakeXamarinProjects/Program.cs
+++ MakeXamarinProjects/Program.cs
@@ -10,11 +10,28 @@ namespace MakeXamarinProjects
 {
     class Program
     {
+        //Android and ios are supported with this, but they still work with shared projects,
+        //so mac only for now, but if you get trouble later these should work (might have to tweak project file some)
         static void Main(string[] args)
         {
-            convertToMac(args[0]);
-            convertToAndroid(args[0]);
-            convertToiOS(args[0]);
+            var inFile = args[0];
+            if (Directory.Exists(inFile))
+            {
+                foreach(var file in Directory.EnumerateFiles(inFile, "*" + Path.GetExtension(inFile), SearchOption.AllDirectories))
+                {
+                    convertToMac(file);
+                    //convertToAndroid(file);
+                    //convertToiOS(file);
+                    convertToPortable(file);
+                }
+            }
+            else
+            {
+                convertToMac(inFile);
+                //convertToAndroid(inFile);
+                //convertToiOS(inFile);
+                convertToPortable(inFile);
+            }
         }
 
         static void convertToAndroid(String file)
@@ -23,7 +40,8 @@ namespace MakeXamarinProjects
             convertProject(file, output,
                 "{EFBA0AD7-5A72-4C68-AF49-83D382785DCF};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}",
                 "$(MSBuildExtensionsPath)\\Xamarin\\Android\\Xamarin.Android.CSharp.targets",
-                "6.0"
+                "6.0",
+                new Tuple<String, String>[] { }
                 );
         }
 
@@ -33,7 +51,8 @@ namespace MakeXamarinProjects
             convertProject(file, output,
                 "{FEACFBD2-3405-455C-9665-78FE426C6842};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}",
                 "$(MSBuildExtensionsPath)\\Xamarin\\iOS\\Xamarin.iOS.CSharp.targets",
-                "1.0"
+                "1.0",
+                new Tuple<String, String>[] { }
                 );
         }
 
@@ -43,13 +62,29 @@ namespace MakeXamarinProjects
             convertProject(file, output,
                 "{A3F8F2AB-B479-4A4A-A458-A89E7DC349F1};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}",
                 "$(MSBuildExtensionsPath)\\Xamarin\\Mac\\Xamarin.Mac.CSharp.targets",
-                "1.0"
+                "2.0",
+                new Tuple<String, String>[] {
+                    Tuple.Create("TargetFrameworkIdentifier", "Xamarin.Mac")
+                }
+                );
+        }
+
+        static void convertToPortable(String file)
+        {
+            String output = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file) + "Portable" + Path.GetExtension(file));
+            convertProject(file, output,
+                "{786C830F-07A1-408B-BD7F-6EE04809D6DB};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}",
+                "$(MSBuildExtensionsPath)\\Xamarin\\Mac\\Xamarin.Mac.CSharp.targets",
+                "4.0",
+                new Tuple<String, String>[] {
+                    Tuple.Create("TargetFrameworkProfile", "Profile24")
+                }
                 );
         }
 
         static String VsXmlNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
 
-        static void convertProject(String file, String output, String guids, String project, String targetFrameworkVersion)
+        static void convertProject(String file, String output, String guids, String project, String targetFrameworkVersion, IEnumerable<Tuple<String, String>> additionalElements)
         {
             XDocument xDoc = XDocument.Load(file);
             var root = xDoc.Root;
@@ -59,6 +94,10 @@ namespace MakeXamarinProjects
                 {
                     item.SetValue(targetFrameworkVersion);
                     propGroup.Add(new XElement(XName.Get("ProjectTypeGuids", VsXmlNamespace), guids));
+                    foreach(var additional in additionalElements)
+                    {
+                        propGroup.Add(new XElement(XName.Get(additional.Item1, VsXmlNamespace), additional.Item2));
+                    }
                     break;
                 }
             }
