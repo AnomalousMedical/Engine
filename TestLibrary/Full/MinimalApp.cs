a8ebe3b18e9ec12806584971954252bdb7774f05
Andrew Piper <andrew@threax.com>
2017-07-07 11:03:47 -0400

    Moved pluginmanager construction back out of di, but di the results.

--- TestLibrary/Full/MinimalApp.cs
+++ TestLibrary/Full/MinimalApp.cs
@@ -76,6 +76,7 @@ namespace Anomalous.Minimus.Full
             Log.ImportantInfo("Running from directory {0}", FolderFinder.ExecutableFolder);
             RegisterEngine();
             RegisterGui();
+            BuildPluginManager();
 
             //Create containers
             container = builder.Build();
@@ -127,106 +128,8 @@ namespace Anomalous.Minimus.Full
 
         private void RegisterEngine()
         {
-            builder.Register(c => new PluginManager(CoreConfig.ConfigFile))
-                .SingleInstance()
-                .OnActivated(a =>
-                {
-                    var pluginManager = a.Instance;
-                    var mainWindow = a.Context.Resolve<NativeOSWindow>();
-                    var mainTimer = a.Context.Resolve<UpdateTimer>();
-
-                    MyGUIInterface.EventLayerKey = EventLayers.Gui;
-                    MyGUIInterface.CreateGuiGestures = CoreConfig.EnableMultitouch && PlatformConfig.TouchType == TouchType.Screen;
-
-                    //Configure plugins
-                    pluginManager.OnConfigureDefaultWindow = delegate (out WindowInfo defaultWindow)
-                    {
-                        //Setup main window
-                        defaultWindow = new WindowInfo(mainWindow, "Primary");
-                        defaultWindow.Fullscreen = CoreConfig.EngineConfig.Fullscreen;
-                        defaultWindow.MonitorIndex = 0;
-
-                        if (CoreConfig.EngineConfig.Fullscreen)
-                        {
-                            mainWindow.setSize(CoreConfig.EngineConfig.HorizontalRes, CoreConfig.EngineConfig.VerticalRes);
-                            mainWindow.ExclusiveFullscreen = true;
-                            defaultWindow.Width = CoreConfig.EngineConfig.HorizontalRes;
-                            defaultWindow.Height = CoreConfig.EngineConfig.VerticalRes;
-                        }
-                        else
-                        {
-                            mainWindow.Maximized = true;
-                        }
-                        mainWindow.show();
-                    };
-
-                    GuiFrameworkCamerasInterface.MoveCameraEventLayer = EventLayers.Cameras;
-                    GuiFrameworkCamerasInterface.SelectWindowEventLayer = EventLayers.AfterGui;
-                    GuiFrameworkCamerasInterface.ShortcutEventLayer = EventLayers.AfterGui;
-
-                    pluginManager.addPluginAssembly(typeof(OgreInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(BulletInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(NativePlatformPlugin).Assembly);
-                    pluginManager.addPluginAssembly(typeof(MyGUIInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(RocketInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(SoundPluginInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(BEPUikInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(GuiFrameworkInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(RocketWidgetInterface).Assembly);
-                    pluginManager.addPluginAssembly(typeof(GuiFrameworkCamerasInterface).Assembly);
-                    pluginManager.initializePlugins();
-
-                    //Intialize the platform
-                    BulletInterface.Instance.ShapeMargin = 0.005f;
-
-                    if (OgreConfig.VSync && CoreConfig.EngineConfig.FPSCap < 300)
-                    {
-                        //Use a really high framerate cap if vsync is on since it will cap our 
-                        //framerate for us. If the user has requested a higher rate use it anyway.
-                        mainTimer.FramerateCap = 300;
-                    }
-                    else
-                    {
-                        mainTimer.FramerateCap = CoreConfig.EngineConfig.FPSCap;
-                    }
-
-                    var eventManager = a.Context.Resolve<EventManager>();
-
-                    pluginManager.setPlatformInfo(mainTimer, eventManager);
-
-                    SoundConfig.initialize(CoreConfig.ConfigFile);
-
-                    GuiFrameworkInterface.Instance.handleCursors(mainWindow);
-                    SoundPluginInterface.Instance.setResourceWindow(mainWindow);
-
-                    var touchMouseGuiForwarder = new TouchMouseGuiForwarder(eventManager, a.Context.Resolve<InputHandler>(), a.Context.Resolve<SystemTimer>(), mainWindow, EventLayers.Last);
-                    touchMouseGuiForwarder.ForwardTouchesAsMouse = PlatformConfig.ForwardTouchAsMouse;
-                    var myGuiKeyboard = new MyGUIOnscreenKeyboardManager(touchMouseGuiForwarder);
-                    var rocketKeyboard = new RocketWidgetOnscreenKeyboardManager(touchMouseGuiForwarder);
-                });
-
             builder.RegisterType<SceneController>()
                 .SingleInstance();
-
-            builder.RegisterType<NativeSystemTimer>()
-                .SingleInstance()
-                .As<SystemTimer>();
-
-            builder.RegisterType<NativeUpdateTimer>()
-                .SingleInstance()
-                .As<UpdateTimer>()
-                .As<NativeUpdateTimer>();
-
-            builder.Register(c => new NativeInputHandler(c.Resolve<NativeOSWindow>(), CoreConfig.EnableMultitouch))
-                .SingleInstance()
-                .As<InputHandler>();
-
-            builder.Register(c => new EventManager(c.Resolve<InputHandler>(), Enum.GetValues(typeof(EventLayers))))
-                .SingleInstance()
-                .OnActivated(a =>
-                {
-                    a.Context.Resolve<UpdateTimer>().addUpdateListener(new EventUpdateListener(a.Instance));
-                });
 
             builder.Register<FrameClearManager>(c => new FrameClearManager(OgreInterface.Instance.OgrePrimaryWindow.OgreRenderTarget, Color.Blue))
                .SingleInstance();
@@ -248,39 +151,129 @@ namespace Anomalous.Minimus.Full
                 .ExternallyOwned();
         }
 
+        private void BuildPluginManager()
+        {
+            var mainWindow = new NativeOSWindow("Anomalous Minimus", new IntVector2(-1, -1), new IntSize2(CoreConfig.EngineConfig.HorizontalRes, CoreConfig.EngineConfig.VerticalRes));
+            builder.RegisterInstance(mainWindow)
+                .As<OSWindow>()
+                .As<NativeOSWindow>();
+
+            mainWindow.Closed += w =>
+            {
+                if (PlatformConfig.CloseMainWindowOnShutdown)
+                {
+                    mainWindow.close();
+                }
+                this.exit();
+            };
+
+            //Setup DPI
+            float pixelScale = mainWindow.WindowScaling;
+            switch (CoreConfig.ExtraScaling)
+            {
+                case UIExtraScale.Smaller:
+                    pixelScale -= .15f;
+                    break;
+                case UIExtraScale.Larger:
+                    pixelScale += .25f;
+                    break;
+            }
+
+            ScaleHelper._setScaleFactor(pixelScale);
+
+            var pluginManager = new PluginManager(CoreConfig.ConfigFile);
+            builder.RegisterInstance(pluginManager)
+                .As<PluginManager>();
+
+            var systemTimer = new NativeSystemTimer();
+            builder.RegisterInstance(systemTimer)
+                .As<SystemTimer>();
+
+            var mainTimer = new NativeUpdateTimer(systemTimer);
+            builder.RegisterInstance(mainTimer)
+                .As<UpdateTimer>()
+                .As<NativeUpdateTimer>();
+
+            var inputHandler = new NativeInputHandler(mainWindow, CoreConfig.EnableMultitouch);
+            builder.RegisterInstance(inputHandler)
+                .As<InputHandler>();
+
+            var eventManager = new EventManager(inputHandler, Enum.GetValues(typeof(EventLayers)));
+            builder.RegisterInstance(eventManager)
+                .As<EventManager>();
+
+            MyGUIInterface.EventLayerKey = EventLayers.Gui;
+            MyGUIInterface.CreateGuiGestures = CoreConfig.EnableMultitouch && PlatformConfig.TouchType == TouchType.Screen;
+
+            //Configure plugins
+            pluginManager.OnConfigureDefaultWindow = delegate (out WindowInfo defaultWindow)
+            {
+                //Setup main window
+                defaultWindow = new WindowInfo(mainWindow, "Primary");
+                defaultWindow.Fullscreen = CoreConfig.EngineConfig.Fullscreen;
+                defaultWindow.MonitorIndex = 0;
+
+                if (CoreConfig.EngineConfig.Fullscreen)
+                {
+                    mainWindow.setSize(CoreConfig.EngineConfig.HorizontalRes, CoreConfig.EngineConfig.VerticalRes);
+                    mainWindow.ExclusiveFullscreen = true;
+                    defaultWindow.Width = CoreConfig.EngineConfig.HorizontalRes;
+                    defaultWindow.Height = CoreConfig.EngineConfig.VerticalRes;
+                }
+                else
+                {
+                    mainWindow.Maximized = true;
+                }
+                mainWindow.show();
+            };
+
+            GuiFrameworkCamerasInterface.MoveCameraEventLayer = EventLayers.Cameras;
+            GuiFrameworkCamerasInterface.SelectWindowEventLayer = EventLayers.AfterGui;
+            GuiFrameworkCamerasInterface.ShortcutEventLayer = EventLayers.AfterGui;
+
+            pluginManager.addPluginAssembly(typeof(OgreInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(BulletInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(NativePlatformPlugin).Assembly);
+            pluginManager.addPluginAssembly(typeof(MyGUIInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(RocketInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(SoundPluginInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(BEPUikInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(GuiFrameworkInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(RocketWidgetInterface).Assembly);
+            pluginManager.addPluginAssembly(typeof(GuiFrameworkCamerasInterface).Assembly);
+            pluginManager.initializePlugins();
+
+            //Intialize the platform
+            BulletInterface.Instance.ShapeMargin = 0.005f;
+
+            if (OgreConfig.VSync && CoreConfig.EngineConfig.FPSCap < 300)
+            {
+                //Use a really high framerate cap if vsync is on since it will cap our 
+                //framerate for us. If the user has requested a higher rate use it anyway.
+                mainTimer.FramerateCap = 300;
+            }
+            else
+            {
+                mainTimer.FramerateCap = CoreConfig.EngineConfig.FPSCap;
+            }
+
+            pluginManager.setPlatformInfo(mainTimer, eventManager);
+            mainTimer.addUpdateListener(new EventUpdateListener(eventManager));
+
+            SoundConfig.initialize(CoreConfig.ConfigFile);
+
+            GuiFrameworkInterface.Instance.handleCursors(mainWindow);
+            SoundPluginInterface.Instance.setResourceWindow(mainWindow);
+
+            var touchMouseGuiForwarder = new TouchMouseGuiForwarder(eventManager, inputHandler, systemTimer, mainWindow, EventLayers.Last);
+            touchMouseGuiForwarder.ForwardTouchesAsMouse = PlatformConfig.ForwardTouchAsMouse;
+            var myGuiKeyboard = new MyGUIOnscreenKeyboardManager(touchMouseGuiForwarder);
+            var rocketKeyboard = new RocketWidgetOnscreenKeyboardManager(touchMouseGuiForwarder);
+        }
+
         private void RegisterGui()
         {
             //Register gui services
-            builder.Register(c => new NativeOSWindow("Anomalous Minimus", new IntVector2(-1, -1), new IntSize2(CoreConfig.EngineConfig.HorizontalRes, CoreConfig.EngineConfig.VerticalRes)))
-                .SingleInstance()
-                .As<OSWindow>()
-                .As<NativeOSWindow>()
-                .OnActivated(a =>
-                {
-                    var mainWindow = a.Instance;
-                    a.Instance.Closed += w =>
-                    {
-                        if (PlatformConfig.CloseMainWindowOnShutdown)
-                        {
-                            mainWindow.close();
-                        }
-                        this.exit();
-                    };
-
-                    //Setup DPI
-                    float pixelScale = mainWindow.WindowScaling;
-                    switch (CoreConfig.ExtraScaling)
-                    {
-                        case UIExtraScale.Smaller:
-                            pixelScale -= .15f;
-                            break;
-                        case UIExtraScale.Larger:
-                            pixelScale += .25f;
-                            break;
-                    }
-
-                    ScaleHelper._setScaleFactor(pixelScale);
-                });
 
             builder.RegisterType<DocumentController>()
                 .SingleInstance();
