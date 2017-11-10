f68f0fbf410dd97fa124fd4628448857f56b4449
Andrew Piper <andrew@threax.com>
2017-11-10 07:01:43 -0500

    Windows nuget style figured out.

--- BulletWrapper/Nuget/Win32/BuildNuget.bat
+++ BulletWrapper/Nuget/Win32/BuildNuget.bat
@@ -1,3 +1,10 @@
 pushd %~dp0
-dotnet pack --include-symbols --configuration Release
+
+mkdir contentFiles\any\any\x86
+mkdir contentFiles\any\any\x64
+
+copy ..\..\..\Release\x86\BulletWrapper.dll contentFiles\any\any\x86\BulletWrapper.dll
+copy ..\..\..\Release\x64\BulletWrapper.dll contentFiles\any\any\x64\BulletWrapper.dll
+
+nuget pack
 popd
