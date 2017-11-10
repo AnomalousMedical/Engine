5c29ba2acd087ad4ff95c8a5c2358204f42e00d7
Andrew Piper <andrew@threax.com>
2017-11-10 07:09:03 -0500

    Got ogre nuget package working on win32.

--- OgreCWrapper/Nuget/Win32/BuildNuget.bat
+++ OgreCWrapper/Nuget/Win32/BuildNuget.bat
@@ -1,3 +1,18 @@
 pushd %~dp0
-dotnet pack --include-symbols --configuration Release
+
+mkdir contentFiles\any\any\x86
+mkdir contentFiles\any\any\x64
+
+copy ..\..\..\..\Dependencies\Ogre\WindowsBuild\bin\release\OgreMain.dll                contentFiles\any\any\x86\OgreMain.dll
+copy ..\..\..\..\Dependencies\Ogre\WindowsBuild\bin\release\RenderSystem_Direct3D11.dll contentFiles\any\any\x86\RenderSystem_Direct3D11.dll
+copy ..\..\..\..\Dependencies\Ogre\WindowsBuild\bin\release\RenderSystem_GL.dll         contentFiles\any\any\x86\RenderSystem_GL.dll
+copy ..\..\..\..\Dependencies\Ogre\WindowsBuild\bin\release\RenderSystem_GLES2.dll      contentFiles\any\any\x86\RenderSystem_GLES2.dll
+copy ..\..\..\Release\x86\OgreCWrapper.dll                                              contentFiles\any\any\x86\OgreCWrapper.dll
+
+copy ..\..\..\..\Dependencies\Ogre\Win64Build\bin\release\OgreMain.dll                  contentFiles\any\any\x64\OgreMain.dll
+copy ..\..\..\..\Dependencies\Ogre\Win64Build\bin\release\RenderSystem_Direct3D11.dll   contentFiles\any\any\x64\RenderSystem_Direct3D11.dll
+copy ..\..\..\..\Dependencies\Ogre\Win64Build\bin\release\RenderSystem_GL.dll           contentFiles\any\any\x64\RenderSystem_GL.dll
+copy ..\..\..\Release\x64\OgreCWrapper.dll                                              contentFiles\any\any\x64\OgreCWrapper.dll
+
+nuget pack
 popd
