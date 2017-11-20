728b12b6ae53ccd823db34cfbbb5f92821001a37
Andrew Piper <andrew@threax.com>
2017-11-20 13:17:12 -0500

    Refactored bullet wrapper nuget packages.

--- BulletWrapper/Nuget/Win32/BuildNuget.bat
+++ BulletWrapper/Nuget/Win32/BuildNuget.bat
@@ -1,12 +1,3 @@
 pushd %~dp0
-
-mkdir contentFiles\any\any\x86
-mkdir contentFiles\any\any\x64
-
-copy ..\..\..\Release\x86\BulletWrapper.dll contentFiles\any\any\x86\BulletWrapper.dll
-copy ..\..\..\Release\x64\BulletWrapper.dll contentFiles\any\any\x64\BulletWrapper.dll
-
 dotnet pack --configuration Release
-REM move *.nupkg  ..\..\..\Release
-
 popd
