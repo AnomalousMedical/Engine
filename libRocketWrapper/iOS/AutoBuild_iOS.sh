eeacc2a000a0002616eef36525404544cdc4f6fd
Andrew Piper <andrewpiper@Andrews-Mini.threax.com>
2017-11-23 11:44:21 -0500

    Removed 32 bit ios targets.

--- libRocketWrapper/iOS/AutoBuild_iOS.sh
+++ libRocketWrapper/iOS/AutoBuild_iOS.sh
@@ -7,7 +7,7 @@ THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$THIS_FOLDER"
 
 #Build with XCode
-xcodebuild -configuration Release -sdk iphoneos ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH_device"
+xcodebuild -configuration Release -sdk iphoneos ONLY_ACTIVE_ARCH=NO ARCHS="arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH_device"
 
 #Finish up
 cd "$START_PATH"
