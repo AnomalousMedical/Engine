93875fc64b4cf71563e42caa9f382826dd3e9f46
Andrew Piper <andrew@threax.com>
2018-05-06 08:26:54 -0400

    Added a .net core output, runs but can't detect dpi.

---
+++ GameAppTestNetCore/Program.cs
@@ -0,0 +1,51 @@
+﻿using Anomalous.GameApp;
+using Anomalous.OSPlatform;
+using Anomalous.OSPlatform.Win32;
+using GameAppTest;
+using Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAppTestWin32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsRuntimePlatformInfo.Initialize();
+
+            GameApp app = null;
+            try
+            {
+                app = new GameApp(new Startup());
+                app.PrimaryArchivePath = "C:\\Development\\Anomalous\\RpgArt";
+                app.run();
+            }
+            catch (Exception e)
+            {
+                Log.Default.printException(e);
+                if (app != null)
+                {
+                    app.saveCrashLog();
+                }
+                String errorMessage = e.Message + "\n" + e.StackTrace;
+                while (e.InnerException != null)
+                {
+                    e = e.InnerException;
+                    errorMessage += "\n" + e.Message + "\n" + e.StackTrace;
+                }
+                MessageDialog.showErrorDialog(errorMessage, "Exception");
+            }
+            finally
+            {
+                if (app != null)
+                {
+                    app.Dispose();
+                }
+            }
+        }
+    }
+}
