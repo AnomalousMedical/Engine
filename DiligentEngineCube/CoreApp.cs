dcc8b28369575cfe19abe87265b40483ed11d712
Andrew Piper <andrew@threax.com>
2021-02-03 17:13:14 -0500

    Made a copy to start working on the cube in.

---
+++ DiligentEngineCube/CoreApp.cs
@@ -0,0 +1,81 @@
+﻿using Anomalous.OSPlatform;
+using DiligentEnginePlugin;
+using Engine;
+using Engine.Platform;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using SoundPlugin;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace DiligentEngineCube
+{
+    public class CoreApp : App
+    {
+        private NativeOSWindow mainWindow;
+        private UpdateTimer mainTimer;
+
+        public CoreApp()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+            PerformanceMonitor.destroyEnabledState();
+
+            base.DisposeGlobalScope();
+            EasyNativeWindow.Destroy(mainWindow);
+            base.FinalDispose();
+        }
+
+        public override bool OnInit(IServiceCollection services, PluginManager pluginManager)
+        {
+            mainWindow = EasyNativeWindow.Create(services, this, o =>
+            {
+                o.Title = "Diligent Engine Cube";
+            });
+
+            services.AddLogging(o =>
+            {
+                o.AddConsole();
+            });
+
+            services.AddDiligentEngine(pluginManager);
+            services.AddOSPlatform(pluginManager);
+            services.AddSoundPlugin(pluginManager);
+
+            //Add this app's services
+            services.TryAddSingleton<CubeUpdateListener>();
+
+            return true;
+        }
+
+        public override bool OnLink(IServiceScope globalScope)
+        {
+            var log = globalScope.ServiceProvider.GetRequiredService<ILogger<CoreApp>>();
+            log.LogInformation("Running from directory {0}", FolderFinder.ExecutableFolder);
+
+            mainTimer = globalScope.ServiceProvider.GetRequiredService<UpdateTimer>();
+
+            var updateListener = globalScope.ServiceProvider.GetRequiredService<CubeUpdateListener>();
+            mainTimer.addUpdateListener(updateListener);
+
+            PerformanceMonitor.setupEnabledState(globalScope.ServiceProvider.GetRequiredService<SystemTimer>());
+
+            return true;
+        }
+
+        public override int OnExit()
+        {
+            return 0;
+        }
+
+        public override void OnIdle()
+        {
+            mainTimer?.OnIdle();
+        }
+    }
+}
