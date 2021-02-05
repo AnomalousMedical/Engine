b12d0a194bc62b299991add3d5653347c46d075b
Andrew Piper <andrew@threax.com>
2021-02-05 17:42:35 -0500

    Brought back hacks for render device.

--- DiligentEngine/Structs/ImmutableSamplerDesc.PassStruct.cs
+++ DiligentEngine/Structs/ImmutableSamplerDesc.PassStruct.cs
@@ -20,10 +20,24 @@ namespace DiligentEngine
     {
         public SHADER_TYPE ShaderStages;
         public String SamplerOrTextureName;
-        public SamplerDesc Desc;
+        public FILTER_TYPE MinFilter;
+        public FILTER_TYPE MagFilter;
+        public FILTER_TYPE MipFilter;
+        public TEXTURE_ADDRESS_MODE AddressU;
+        public TEXTURE_ADDRESS_MODE AddressV;
+        public TEXTURE_ADDRESS_MODE AddressW;
+        public Float32 MipLODBias;
+        public Uint32 MaxAnisotropy;
+        public COMPARISON_FUNCTION ComparisonFunc;
+        public Float32 BorderColor_0;
+        public Float32 BorderColor_1;
+        public Float32 BorderColor_2;
+        public Float32 BorderColor_3;
+        public float MinLOD;
+        public float MaxLOD;
         public static ImmutableSamplerDescPassStruct[] ToStruct(IEnumerable<ImmutableSamplerDesc> vals)
         {
-            if(vals == null)
+            if (vals == null)
             {
                 return null;
             }
@@ -32,7 +46,21 @@ namespace DiligentEngine
             {
                 ShaderStages = i.ShaderStages,
                 SamplerOrTextureName = i.SamplerOrTextureName,
-                Desc = i.Desc,
+                MinFilter = i.Desc.MinFilter,
+                MagFilter = i.Desc.MagFilter,
+                MipFilter = i.Desc.MipFilter,
+                AddressU = i.Desc.AddressU,
+                AddressV = i.Desc.AddressV,
+                AddressW = i.Desc.AddressW,
+                MipLODBias = i.Desc.MipLODBias,
+                MaxAnisotropy = i.Desc.MaxAnisotropy,
+                ComparisonFunc = i.Desc.ComparisonFunc,
+                BorderColor_0 = i.Desc.BorderColor_0,
+                BorderColor_1 = i.Desc.BorderColor_1,
+                BorderColor_2 = i.Desc.BorderColor_2,
+                BorderColor_3 = i.Desc.BorderColor_3,
+                MinLOD = i.Desc.MinLOD,
+                MaxLOD = i.Desc.MaxLOD,
             }).ToArray();
         }
     }
--- DiligentEngineWrapper/ImmutableSamplerDesc.PassStruct.h
+++ DiligentEngineWrapper/ImmutableSamplerDesc.PassStruct.h
@@ -2,12 +2,62 @@
 #include "Primitives/interface/BasicTypes.h"
 #include "Graphics/GraphicsEngine/interface/GraphicsTypes.h"
 
-namespace Diligent 
+namespace Diligent
 {
-struct ImmutableSamplerDescPassStruct
-{
+    struct ImmutableSamplerDescPassStruct
+    {
         SHADER_TYPE ShaderStages;
         Char* SamplerOrTextureName;
-        SamplerDesc Desc;
-};
+        /// Texture minification filter, see Diligent::FILTER_TYPE for details.
+    /// Default value: Diligent::FILTER_TYPE_LINEAR.
+        FILTER_TYPE MinFilter           DEFAULT_INITIALIZER(FILTER_TYPE_LINEAR);
+
+        /// Texture magnification filter, see Diligent::FILTER_TYPE for details.
+        /// Default value: Diligent::FILTER_TYPE_LINEAR.
+        FILTER_TYPE MagFilter           DEFAULT_INITIALIZER(FILTER_TYPE_LINEAR);
+
+        /// Mip filter, see Diligent::FILTER_TYPE for details. 
+        /// Only FILTER_TYPE_POINT, FILTER_TYPE_LINEAR, FILTER_TYPE_ANISOTROPIC, and 
+        /// FILTER_TYPE_COMPARISON_ANISOTROPIC are allowed.
+        /// Default value: Diligent::FILTER_TYPE_LINEAR.
+        FILTER_TYPE MipFilter           DEFAULT_INITIALIZER(FILTER_TYPE_LINEAR);
+
+        /// Texture address mode for U coordinate, see Diligent::TEXTURE_ADDRESS_MODE for details
+        /// Default value: Diligent::TEXTURE_ADDRESS_CLAMP.
+        TEXTURE_ADDRESS_MODE AddressU   DEFAULT_INITIALIZER(TEXTURE_ADDRESS_CLAMP);
+
+        /// Texture address mode for V coordinate, see Diligent::TEXTURE_ADDRESS_MODE for details
+        /// Default value: Diligent::TEXTURE_ADDRESS_CLAMP.
+        TEXTURE_ADDRESS_MODE AddressV   DEFAULT_INITIALIZER(TEXTURE_ADDRESS_CLAMP);
+
+        /// Texture address mode for W coordinate, see Diligent::TEXTURE_ADDRESS_MODE for details
+        /// Default value: Diligent::TEXTURE_ADDRESS_CLAMP.
+        TEXTURE_ADDRESS_MODE AddressW   DEFAULT_INITIALIZER(TEXTURE_ADDRESS_CLAMP);
+
+        /// Offset from the calculated mipmap level. For example, if a sampler calculates that a texture 
+        /// should be sampled at mipmap level 1.2 and MipLODBias is 2.3, then the texture will be sampled at 
+        /// mipmap level 3.5. Default value: 0.
+        Float32 MipLODBias                  DEFAULT_INITIALIZER(0);
+
+        /// Maximum anisotropy level for the anisotropic filter. Default value: 0.
+        Uint32 MaxAnisotropy                DEFAULT_INITIALIZER(0);
+
+        /// A function that compares sampled data against existing sampled data when comparsion
+        /// filter is used. Default value: Diligent::COMPARISON_FUNC_NEVER.
+        COMPARISON_FUNCTION ComparisonFunc  DEFAULT_INITIALIZER(COMPARISON_FUNC_NEVER);
+
+        /// Border color to use if TEXTURE_ADDRESS_BORDER is specified for AddressU, AddressV, or AddressW. 
+        /// Default value: {0,0,0,0}
+        Float32 BorderColor[4]              DEFAULT_INITIALIZER({});
+
+        /// Specifies the minimum value that LOD is clamped to before accessing the texture MIP levels.
+        /// Must be less than or equal to MaxLOD.
+        /// Default value: 0.
+        float MinLOD                        DEFAULT_INITIALIZER(0);
+
+        /// Specifies the maximum value that LOD is clamped to before accessing the texture MIP levels.
+        /// Must be greater than or equal to MinLOD.
+        /// Default value: +FLT_MAX.
+        float MaxLOD                        DEFAULT_INITIALIZER(+3.402823466e+38F);
+    };
 }
