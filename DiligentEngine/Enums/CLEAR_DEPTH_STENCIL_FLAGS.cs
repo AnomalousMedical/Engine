3c94f6bcb8cb71ab13e5c1614ee37f07a9fc5a23
Andrew Piper <andrew@threax.com>
2021-02-06 09:48:51 -0500

    Added float2.

--- DiligentEngine/Enums/CLEAR_DEPTH_STENCIL_FLAGS.cs
+++ DiligentEngine/Enums/CLEAR_DEPTH_STENCIL_FLAGS.cs
@@ -13,6 +13,7 @@ using Uint16 = System.UInt16;
 using PVoid = System.IntPtr;
 using float4 = Engine.Vector4;
 using float3 = Engine.Vector3;
+using float2 = Engine.Vector2;
 using float4x4 = Engine.Matrix4x4;
 using BOOL = System.Boolean;
 
