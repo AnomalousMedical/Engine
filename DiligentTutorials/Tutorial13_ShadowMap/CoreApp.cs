6c59eb2494f93eb1f883445c7fff9c23fcc0add2
Andrew Piper <andrew@threax.com>
2021-02-14 15:37:51 -0500

    Duplicated textured cube as shadow map.

---
+++ DiligentTutorials/Tutorial13_ShadowMap/CoreApp.cs
@@ -0,0 +1,78 @@
+﻿using Anomalous.OSPlatform;
+using Engine;
+using Engine.Platform;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Tutorial13_ShadowMap
+{
+    public class CoreApp : App
+    {
+        private NativeOSWindow mainWindow;
+        private UpdateTimer mainTimer;
+
+        public CoreApp()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+            PerformanceMonitor.destroyEnabledState();
+
+            base.DisposeGlobalScope();
+            EasyNativeWindow.Destroy(mainWindow);
+            base.FinalDispose();
+        }
+
+        public override bool OnInit(IServiceCollection services, PluginManager pluginManager)
+        {
+            mainWindow = EasyNativeWindow.Create(services, this, o =>
+            {
+                o.Title = "Diligent - Tutorial 13 - Shadow Map";
+            });
+
+            services.AddLogging(o =>
+            {
+                o.AddConsole();
+            });
+
+            services.AddDiligentEngine(pluginManager);
+            services.AddOSPlatform(pluginManager);
+
+            //Add this app's services
+            services.TryAddSingleton<TextureUpdateListener>();
+
+            return true;
+        }
+
+        public override bool OnLink(IServiceScope globalScope)
+        {
+            var log = globalScope.ServiceProvider.GetRequiredService<ILogger<CoreApp>>();
+            log.LogInformation("Running from directory {0}", FolderFinder.ExecutableFolder);
+
+            mainTimer = globalScope.ServiceProvider.GetRequiredService<UpdateTimer>();
+
+            var updateListener = globalScope.ServiceProvider.GetRequiredService<TextureUpdateListener>();
+            mainTimer.addUpdateListener(updateListener);
+
+            PerformanceMonitor.setupEnabledState(globalScope.ServiceProvider.GetRequiredService<SystemTimer>());
+
+            return true;
+        }
+
+        public override int OnExit()
+        {
+            return 0;
+        }
+
+        public override void OnIdle()
+        {
+            mainTimer?.OnIdle();
+        }
+    }
+}
