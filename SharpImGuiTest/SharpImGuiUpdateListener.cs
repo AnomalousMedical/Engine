6e925841a7282a44fe71fb1d4cdd01b8e81ed255
Andrew Piper <andrew@threax.com>
2021-02-19 10:36:32 -0500

    Add some perf mon info.

--- SharpImGuiTest/SharpImGuiUpdateListener.cs
+++ SharpImGuiTest/SharpImGuiUpdateListener.cs
@@ -21,9 +21,12 @@ namespace SharpImGuiTest
         private readonly ISwapChain swapChain;
         private readonly IDeviceContext m_pImmediateContext;
         private String displayText = "Click on something!";
+        private StringBuilder lastUpdateTimeBuilder = new StringBuilder();
 
         public SharpImGuiUpdateListener(GraphicsEngine graphicsEngine, NativeOSWindow window, ISharpGui sharpGui, EventManager eventManager)
         {
+            PerformanceMonitor.Enabled = true;
+
             this.graphicsEngine = graphicsEngine;
             this.window = window;
             this.sharpGui = sharpGui;
@@ -61,23 +64,24 @@ namespace SharpImGuiTest
 
         public unsafe void sendUpdate(Clock clock)
         {
+            PerformanceMonitor.start("Sharp Gui");
             //Put things on the gui
             sharpGui.Begin();
 
             var columnLayout = new ColumnLayout(button1, button2, button3);
             var desiredSize = columnLayout.GetDesiredSize(sharpGui);
             columnLayout.SetRect(new IntRect(window.WindowWidth - desiredSize.Width, window.WindowHeight - desiredSize.Height, desiredSize.Width, desiredSize.Height));
-            
+
             //Buttons
-            if(sharpGui.Button(button1))
+            if (sharpGui.Button(button1))
             {
                 displayText = "Clicked button 1";
             }
-            if(sharpGui.Button(button2))
+            if (sharpGui.Button(button2))
             {
                 displayText = "Clicked button 2";
             }
-            if(sharpGui.Button(button3))
+            if (sharpGui.Button(button3))
             {
                 displayText = "Clicked button 3";
             }
@@ -89,6 +93,7 @@ namespace SharpImGuiTest
 
             sharpGui.Text(450, 400, Color.Black, $"Program has been running for {TimeSpan.FromMilliseconds(clock.CurrentTimeMicro * Clock.MicroToMilliseconds)}");
             sharpGui.Text(750, 500, Color.Black, displayText);
+            sharpGui.Text(750, 600, Color.Black, lastUpdateTimeBuilder.ToString());
 
             sharpGui.End();
 
@@ -106,6 +111,21 @@ namespace SharpImGuiTest
 
             //Draw the gui
             sharpGui.Render(m_pImmediateContext);
+
+            PerformanceMonitor.stop("Sharp Gui");
+
+            lastUpdateTimeBuilder.Clear();
+            foreach (var value in PerformanceMonitor.Timelapses)
+            {
+                lastUpdateTimeBuilder.AppendFormat("{0}: {1} {2} {3} {4}", value.Name, value.Duration, value.Min, value.Max, value.Average);
+                lastUpdateTimeBuilder.AppendLine();
+            }
+
+            foreach (var value in PerformanceMonitor.PerformanceValues)
+            {
+                lastUpdateTimeBuilder.AppendFormat("{0}: {1}", value.Name, value.Value);
+                lastUpdateTimeBuilder.AppendLine();
+            }
 
             this.swapChain.Present(1);
         }
