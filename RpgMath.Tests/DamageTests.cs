50748370d56bd9a6ae44ca40e1f926a7cfe8aad2
Andrew Piper <andrew@threax.com>
2021-03-18 18:48:34 -0400

    Started adding damage calculations.

---
+++ RpgMath.Tests/DamageTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using Engine;
+using Xunit.Abstractions;
+
+namespace RpgMath.Tests
+{
+    public class DamageTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public DamageTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void PhysicalDamage()
+        {
+            var calc = new DamageCalculator();
+
+            var result = calc.Physical(52, 22, 16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Physical(93, 48, 16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Physical(150, 75, 16, 100);
+            output.WriteLine(result.ToString());
+
+            result = calc.Physical(200, 99, 16, 100);
+            output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ RpgMath/DamageCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RpgMath
+{
+    public class DamageCalculator
+    {
+        /// <summary>
+        /// Physical damage formula.
+        /// </summary>
+        /// <param name="att">Total attack power</param>
+        /// <param name="level">Level of attacker</param>
+        /// <param name="power">The power of the attack. 16 is the base power. Above that is extra, below less. Usually 1 unless special effects.</param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public ulong Physical(ulong att, ulong level, ulong power, ulong def)
+        {
+            ulong baseDamage = att + ((att + level) / 32) * ((att * level) / 32);
+            return ((power * (512 - def) * baseDamage) / (16 * 512));
+        }
+    }
+}
