90c975b3d6c017ecf44c0c11c6900731c02967dc
Andrew Piper <andrew@threax.com>
2021-03-19 17:37:45 -0400

    Added more damage formulas.

--- RpgMath.Tests/DamageTests.cs
+++ RpgMath.Tests/DamageTests.cs
@@ -19,7 +19,13 @@ namespace RpgMath.Tests
         {
             var calc = new DamageCalculator();
 
-            var result = calc.Physical(52, 22, 16, 25);
+            var result = calc.Physical(20, 9, 16, 18);
+            output.WriteLine(result.ToString());
+
+            result = calc.Physical(52, 22, 16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Physical(75, 39, 16, 66);
             output.WriteLine(result.ToString());
 
             result = calc.Physical(93, 48, 16, 25);
@@ -29,6 +35,102 @@ namespace RpgMath.Tests
             output.WriteLine(result.ToString());
 
             result = calc.Physical(200, 99, 16, 100);
+            output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void PhysicalDamageRandom()
+        {
+            var calc = new DamageCalculator();
+
+            var result = calc.RandomVariation(calc.Physical(20, 9, 16, 18));
+            output.WriteLine(result.ToString());
+
+            result = calc.RandomVariation(calc.Physical(52, 22, 16, 25));
+            output.WriteLine(result.ToString());
+
+            result = calc.RandomVariation(calc.Physical(75, 39, 16, 66));
+            output.WriteLine(result.ToString());
+
+            result = calc.RandomVariation(calc.Physical(93, 48, 16, 25));
+            output.WriteLine(result.ToString());
+
+            result = calc.RandomVariation(calc.Physical(150, 75, 16, 100));
+            output.WriteLine(result.ToString());
+
+            result = calc.RandomVariation(calc.Physical(200, 99, 16, 100));
+            output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void MagicalDamage()
+        {
+            var calc = new DamageCalculator();
+
+            var result = calc.Magical(20, 9, 16, 18);
+            output.WriteLine(result.ToString());
+
+            result = calc.Magical(52, 22, 16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Magical(75, 39, 16, 66);
+            output.WriteLine(result.ToString());
+
+            result = calc.Magical(93, 48, 16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Magical(150, 75, 16, 100);
+            output.WriteLine(result.ToString());
+
+            result = calc.Magical(200, 99, 16, 100);
+            output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Cure()
+        {
+            var calc = new DamageCalculator();
+
+            var result = calc.Cure(20, 9, 16);
+            output.WriteLine(result.ToString());
+
+            result = calc.Cure(52, 22, 16);
+            output.WriteLine(result.ToString());
+
+            result = calc.Cure(75, 39, 16);
+            output.WriteLine(result.ToString());
+
+            result = calc.Cure(93, 48, 16);
+            output.WriteLine(result.ToString());
+
+            result = calc.Cure(150, 75, 16);
+            output.WriteLine(result.ToString());
+
+            result = calc.Cure(200, 99, 16);
+            output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Item()
+        {
+            var calc = new DamageCalculator();
+
+            var result = calc.Item(16, 18);
+            output.WriteLine(result.ToString());
+
+            result = calc.Item(16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Item(16, 66);
+            output.WriteLine(result.ToString());
+
+            result = calc.Item(16, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.Item(16, 100);
+            output.WriteLine(result.ToString());
+
+            result = calc.Item(16, 100);
             output.WriteLine(result.ToString());
         }
     }
--- RpgMath/DamageCalculator.cs
+++ RpgMath/DamageCalculator.cs
@@ -4,18 +4,69 @@ namespace RpgMath
 {
     public class DamageCalculator
     {
+        private Random random = new Random();
+
         /// <summary>
         /// Physical damage formula.
         /// </summary>
         /// <param name="att">Total attack power</param>
         /// <param name="level">Level of attacker</param>
         /// <param name="power">The power of the attack. 16 is the base power. Above that is extra, below less. Usually 1 unless special effects.</param>
-        /// <param name="def"></param>
+        /// <param name="def">Defense of target</param>
         /// <returns></returns>
         public ulong Physical(ulong att, ulong level, ulong power, ulong def)
         {
-            ulong baseDamage = att + ((att + level) / 32) * ((att * level) / 32);
-            return ((power * (512 - def) * baseDamage) / (16 * 512));
+            ulong baseDamage = att + ((att + level) / 32UL) * ((att * level) / 32UL);
+            return ((power * (512UL - def) * baseDamage) / (16UL * 512UL));
+        }
+
+        /// <summary>
+        /// Magical damage formula.
+        /// </summary>
+        /// <param name="mat">Magic attack</param>
+        /// <param name="level">Level of attacker</param>
+        /// <param name="power">The power of the attack. 16 is the base power. Above that is extra, below less. Usually 1 unless special effects.</param>
+        /// <param name="mdef">Magic defense of target</param>
+        /// <returns></returns>
+        public ulong Magical(ulong mat, ulong level, ulong power, ulong mdef)
+        {
+            ulong baseDamage = 6UL * (mat + level);
+            return ((power * (512UL - mdef) * baseDamage) / (16UL * 512UL));
+        }
+
+        /// <summary>
+        /// Cure formula.
+        /// </summary>
+        /// <param name="mat">Magic attack</param>
+        /// <param name="level">Level of attacker</param>
+        /// <param name="power">The power of the cure. This is 22 * Power.</param>
+        /// <returns></returns>
+        public ulong Cure(ulong mat, ulong level, ulong power)
+        {
+            ulong baseDamage = 6UL * (mat + level);
+            return baseDamage + 22UL * power;
+        }
+
+        /// <summary>
+        /// Item formula.
+        /// </summary>
+        /// <param name="power">The power of the item. This is 16 * Power.</param>
+        /// <param name="def">Def or mdef of target.</param>
+        /// <returns></returns>
+        public ulong Item(ulong power, ulong def)
+        {
+            ulong baseDamage = 16UL * power;
+            return baseDamage * (512UL - def) / 512UL;
+        }
+
+        /// <summary>
+        /// Random variation formula.
+        /// </summary>
+        /// <param name="damage">The calculated damage to randomize.</param>
+        /// <returns>A randomized damage value.</returns>
+        public ulong RandomVariation(ulong damage)
+        {
+            return damage * (3841UL + (ulong)random.Next(255)) / 4096UL;
         }
     }
 }
