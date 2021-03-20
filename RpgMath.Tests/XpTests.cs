f96cd4ba0634b11583ef3bfb922d417de6e5c294
Andrew Piper <andrew@threax.com>
2021-03-20 19:08:20 -0400

    Added more xp tests.

--- RpgMath.Tests/XpTests.cs
+++ RpgMath.Tests/XpTests.cs
@@ -25,12 +25,42 @@ namespace RpgMath.Tests
         }
 
         [Fact]
+        public void ComputeHeroAllLevelsXp()
+        {
+            var calc = new XpCalculator();
+            var arch = Archetype.CreateHero();
+            for(var i = 2; i < 100; ++i)
+            {
+                var result = calc.GetXpNeeded(arch, i);
+                output.WriteLine(result.ToString());
+            }
+        }
+
+        [Fact]
+        public void ComputeLevel7HeroXp()
+        {
+            var calc = new XpCalculator();
+            var arch = Archetype.CreateHero();
+            var result = calc.GetXpNeeded(arch, 7);
+            Assert.Equal(616, result);
+        }
+
+        [Fact]
         public void ComputeLevel99BrawlerXp()
         {
             var calc = new XpCalculator();
             var arch = Archetype.CreateBrawler();
             var result = calc.GetXpNeeded(arch, 99);
             Assert.Equal(2420933, result);
+        }
+
+        [Fact]
+        public void ComputeLevel14BrawlerXp()
+        {
+            var calc = new XpCalculator();
+            var arch = Archetype.CreateBrawler();
+            var result = calc.GetXpNeeded(arch, 14);
+            Assert.Equal(5809, result);
         }
 
         [Fact]
