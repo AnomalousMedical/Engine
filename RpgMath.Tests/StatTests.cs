49d2bfcfb153c7d856207cc5c4ad0debd27d203c
Andrew Piper <andrew@threax.com>
2021-03-20 14:50:00 -0400

    Added stat gain calculation.

---
+++ RpgMath.Tests/StatTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+using Engine;
+using Xunit.Abstractions;
+
+namespace RpgMath.Tests
+{
+    public class StatTests
+    {
+
+        private readonly ITestOutputHelper output;
+
+        public StatTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void ComputePrimaryStatGain()
+        {
+            var calc = new LevelCalculator();
+            var result = calc.ComputePrimaryStatGain(10, 0, 20);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 21);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 22);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 23);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 24);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 25);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 26);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 27);
+            output.WriteLine(result.ToString());
+
+            result = calc.ComputePrimaryStatGain(10, 0, 28);
+            output.WriteLine(result.ToString());
+
+            //Way under baseline
+            result = calc.ComputePrimaryStatGain(10, 0, 0);
+            output.WriteLine(result.ToString());
+            Assert.Equal(3, result);
+
+            //Way over baseline
+            result = calc.ComputePrimaryStatGain(10, 0, 50);
+            output.WriteLine(result.ToString());
+            Assert.Equal(0, result);
+        }
+    }
+}
--- RpgMath/LevelCalculator.cs
+++ RpgMath/LevelCalculator.cs
@@ -4,7 +4,49 @@ using System.Text;
 
 namespace RpgMath
 {
-    class LevelCalculator
+    public class LevelCalculator
     {
+        private Random random = new Random();
+
+        private PrimaryStatCurves primaryStatCurves = new PrimaryStatCurves();
+
+        public long ComputePrimaryStatGain(int level, int rank, int current)
+        {
+            var curve = primaryStatCurves.GetStatCurve(level, rank);
+            long baselineStat = curve.Baseline + (curve.Gradient * level / 100);
+
+            long diff = (random.Next(8) + 1) + baselineStat - current;
+
+            if(diff < 0)
+            {
+                diff = 0;
+            }
+            else if(diff > 11)
+            {
+                diff = 11;
+            }
+
+            switch (diff)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 0;
+                case 4:
+                case 5:
+                case 6:
+                    return 1;
+                case 7:
+                case 8:
+                case 9:
+                    return 2;
+                case 10:
+                case 11:
+                    return 3;
+                default:
+                    throw new InvalidOperationException("Should not happen");
+            }
+        }
     }
 }
