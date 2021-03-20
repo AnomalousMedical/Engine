27c31d62c7523bae82501200b319fd11f42e3300
Andrew Piper <andrew@threax.com>
2021-03-20 11:09:57 -0400

    Added more power numbers to magic tests.

--- RpgMath.Tests/MagicTests.cs
+++ RpgMath.Tests/MagicTests.cs
@@ -15,147 +15,167 @@ namespace RpgMath.Tests
             this.output = output;
         }
 
-        [Fact]
-        public void MagicalDamageEqual()
+        [Theory]
+        [InlineData(8)]
+        [InlineData(20)]
+        [InlineData(64)]
+        [InlineData(133)]
+        [InlineData(272)]
+        public void MagicalDamageEqual(int power = 16)
         {
             var calc = new DamageCalculator();
 
-            var result = calc.Magical(Characters.level10, Characters.level10, 16);
+            var result = calc.Magical(Characters.level10, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level20, Characters.level20, 16);
+            result = calc.Magical(Characters.level20, Characters.level20, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level30, Characters.level30, 16);
+            result = calc.Magical(Characters.level30, Characters.level30, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level40, Characters.level40, 16);
+            result = calc.Magical(Characters.level40, Characters.level40, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level50, Characters.level50, 16);
+            result = calc.Magical(Characters.level50, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level60, Characters.level60, 16);
+            result = calc.Magical(Characters.level60, Characters.level60, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level70, Characters.level70, 16);
+            result = calc.Magical(Characters.level70, Characters.level70, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level80, Characters.level80, 16);
+            result = calc.Magical(Characters.level80, Characters.level80, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level90, Characters.level90, 16);
+            result = calc.Magical(Characters.level90, Characters.level90, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level99, Characters.level99, 16);
+            result = calc.Magical(Characters.level99, Characters.level99, power);
             output.WriteLine(result.ToString());
         }
 
-        [Fact]
-        public void MagicalDamageLow()
+        [Theory]
+        [InlineData(8)]
+        [InlineData(20)]
+        [InlineData(64)]
+        [InlineData(133)]
+        [InlineData(272)]
+        public void MagicalDamageVsLow(int power)
         {
             var calc = new DamageCalculator();
 
-            var result = calc.Magical(Characters.level10, Characters.level10, 16);
+            var result = calc.Magical(Characters.level10, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level20, Characters.level10, 16);
+            result = calc.Magical(Characters.level20, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level30, Characters.level10, 16);
+            result = calc.Magical(Characters.level30, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level40, Characters.level10, 16);
+            result = calc.Magical(Characters.level40, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level50, Characters.level10, 16);
+            result = calc.Magical(Characters.level50, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level60, Characters.level10, 16);
+            result = calc.Magical(Characters.level60, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level70, Characters.level10, 16);
+            result = calc.Magical(Characters.level70, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level80, Characters.level10, 16);
+            result = calc.Magical(Characters.level80, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level90, Characters.level10, 16);
+            result = calc.Magical(Characters.level90, Characters.level10, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level99, Characters.level10, 16);
+            result = calc.Magical(Characters.level99, Characters.level10, power);
             output.WriteLine(result.ToString());
         }
 
-        [Fact]
-        public void MagicalDamageMid()
+        [Theory]
+        [InlineData(8)]
+        [InlineData(20)]
+        [InlineData(64)]
+        [InlineData(133)]
+        [InlineData(272)]
+        public void MagicalDamageVsMid(int power)
         {
             var calc = new DamageCalculator();
 
-            var result = calc.Magical(Characters.level10, Characters.level50, 16);
+            var result = calc.Magical(Characters.level10, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level20, Characters.level50, 16);
+            result = calc.Magical(Characters.level20, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level30, Characters.level50, 16);
+            result = calc.Magical(Characters.level30, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level40, Characters.level50, 16);
+            result = calc.Magical(Characters.level40, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level50, Characters.level50, 16);
+            result = calc.Magical(Characters.level50, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level60, Characters.level50, 16);
+            result = calc.Magical(Characters.level60, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level70, Characters.level50, 16);
+            result = calc.Magical(Characters.level70, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level80, Characters.level50, 16);
+            result = calc.Magical(Characters.level80, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level90, Characters.level50, 16);
+            result = calc.Magical(Characters.level90, Characters.level50, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level99, Characters.level50, 16);
+            result = calc.Magical(Characters.level99, Characters.level50, power);
             output.WriteLine(result.ToString());
         }
 
-        [Fact]
-        public void MagicalDamageHigh()
+        [Theory]
+        [InlineData(8)]
+        [InlineData(20)]
+        [InlineData(64)]
+        [InlineData(133)]
+        [InlineData(272)]
+        public void MagicalDamageVsHigh(int power)
         {
             var calc = new DamageCalculator();
 
-            var result = calc.Magical(Characters.level10, Characters.level99, 16);
+            var result = calc.Magical(Characters.level10, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level20, Characters.level99, 16);
+            result = calc.Magical(Characters.level20, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level30, Characters.level99, 16);
+            result = calc.Magical(Characters.level30, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level40, Characters.level99, 16);
+            result = calc.Magical(Characters.level40, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level50, Characters.level99, 16);
+            result = calc.Magical(Characters.level50, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level60, Characters.level99, 16);
+            result = calc.Magical(Characters.level60, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level70, Characters.level99, 16);
+            result = calc.Magical(Characters.level70, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level80, Characters.level99, 16);
+            result = calc.Magical(Characters.level80, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level90, Characters.level99, 16);
+            result = calc.Magical(Characters.level90, Characters.level99, power);
             output.WriteLine(result.ToString());
 
-            result = calc.Magical(Characters.level99, Characters.level99, 16);
+            result = calc.Magical(Characters.level99, Characters.level99, power);
             output.WriteLine(result.ToString());
         }
     }
