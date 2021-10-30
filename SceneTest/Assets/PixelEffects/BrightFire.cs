71bace0e8cf358c056b1d6f4b452e2a0f084dea7
Andrew Piper <andrew@threax.com>
2021-10-30 17:00:14 -0400

    More effects.

---
+++ SceneTest/Assets/PixelEffects/BrightFire.cs
@@ -0,0 +1,28 @@
+﻿using Engine;
+using Engine.Platform;
+using SceneTest.Resources;
+using System.Collections.Generic;
+
+namespace SceneTest.Assets.PixelEffects
+{
+    class BrightFire : ISpriteAsset
+    {
+        public SpriteMaterialDescription CreateMaterial()
+        {
+            return new SpriteMaterialDescription
+            (
+                colorMap: "FreePixelEffectsPack/9_brightfire_spritesheet.png",
+                materials: new HashSet<SpriteMaterialTextureItem>()
+            );
+        }
+
+        public Sprite CreateSprite()
+        {
+            return new Sprite(new Dictionary<string, SpriteAnimation>()
+            {
+                 { "default", new SpriteAnimation((int)(0.1f * Clock.SecondsToMicro), SpriteBuilder.CreateAnimatedSprite(100, 100, 8, 61)) },
+            })
+            { BaseScale = new Vector3(1, 1, 1) };
+        }
+    }
+}
---
+++ SceneTest/Assets/PixelEffects/WeaponHit.cs
@@ -0,0 +1,28 @@
+﻿using Engine;
+using Engine.Platform;
+using SceneTest.Resources;
+using System.Collections.Generic;
+
+namespace SceneTest.Assets.PixelEffects
+{
+    class WeaponHit : ISpriteAsset
+    {
+        public SpriteMaterialDescription CreateMaterial()
+        {
+            return new SpriteMaterialDescription
+            (
+                colorMap: "FreePixelEffectsPack/10_weaponhit_spritesheet.png",
+                materials: new HashSet<SpriteMaterialTextureItem>()
+            );
+        }
+
+        public Sprite CreateSprite()
+        {
+            return new Sprite(new Dictionary<string, SpriteAnimation>()
+            {
+                 { "default", new SpriteAnimation((int)(0.1f * Clock.SecondsToMicro), SpriteBuilder.CreateAnimatedSprite(100, 100, 6, 31)) },
+            })
+            { BaseScale = new Vector3(1, 1, 1) };
+        }
+    }
+}
