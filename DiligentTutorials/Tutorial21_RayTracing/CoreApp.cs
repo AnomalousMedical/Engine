87cfb6406970fa0fa38e85899baba0845d4ebd67
Andrew Piper <andrew@threax.com>
2021-12-11 11:54:54 -0500

    Add ray tracing tutorial as copy of hello triangle.

---
+++ DiligentTutorials/Tutorial21_RayTracing/CoreApp.cs
@@ -0,0 +1,78 @@
+﻿using Anomalous.OSPlatform;
+using Engine;
+using Engine.Platform;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace DiligentEngineCube
+{
+    public class CoreApp : App
+    {
+        private NativeOSWindow mainWindow;
+        private UpdateTimer mainTimer;
+
+        public CoreApp()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+            PerformanceMonitor.destroyEnabledState();
+
+            base.DisposeGlobalScope();
+            EasyNativeWindow.Destroy(mainWindow);
+            base.FinalDispose();
+        }
+
+        public override bool OnInit(IServiceCollection services, PluginManager pluginManager)
+        {
+            mainWindow = EasyNativeWindow.Create(services, this, o =>
+            {
+                o.Title = "Diligent - Tutorial 01 - Hello Triangle";
+            });
+
+            services.AddLogging(o =>
+            {
+                o.AddConsole();
+            });
+
+            services.AddDiligentEngine(pluginManager);
+            services.AddOSPlatform(pluginManager);
+
+            //Add this app's services
+            services.TryAddSingleton<HelloTriangleUpdateListener>();
+
+            return true;
+        }
+
+        public override bool OnLink(IServiceProvider serviceProvider)
+        {
+            var log = serviceProvider.GetRequiredService<ILogger<CoreApp>>();
+            log.LogInformation("Running from directory {0}", FolderFinder.ExecutableFolder);
+
+            mainTimer = serviceProvider.GetRequiredService<UpdateTimer>();
+
+            var updateListener = serviceProvider.GetRequiredService<HelloTriangleUpdateListener>();
+            mainTimer.addUpdateListener(updateListener);
+
+            PerformanceMonitor.setupEnabledState(serviceProvider.GetRequiredService<SystemTimer>());
+
+            return true;
+        }
+
+        public override int OnExit()
+        {
+            return 0;
+        }
+
+        public override void OnIdle()
+        {
+            mainTimer?.OnIdle();
+        }
+    }
+}
