115621209386ad8baf2c55b8e7565c52d7449f71
Andrew Piper <andrew@threax.com>
2021-12-19 15:19:46 -0500

    Add a copy of the bepu physics demo to be converted to ray tracing.

---
+++ RTBepuDemo/CoreApp.cs
@@ -0,0 +1,95 @@
+﻿using Anomalous.OSPlatform;
+using Engine;
+using Engine.Platform;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace RTBepuDemo
+{
+    public class CoreApp : App
+    {
+        private NativeOSWindow mainWindow;
+        private UpdateTimer mainTimer;
+
+        public CoreApp()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+            PerformanceMonitor.destroyEnabledState();
+
+            base.DisposeGlobalScope();
+            EasyNativeWindow.Destroy(mainWindow);
+            base.FinalDispose();
+        }
+
+        public override bool OnInit(IServiceCollection services, PluginManager pluginManager)
+        {
+            mainWindow = EasyNativeWindow.Create(services, this, o =>
+            {
+                o.Title = "BEPU Physics 2";
+            });
+
+            services.AddLogging(o =>
+            {
+                o.AddConsole();
+            });
+
+            services.AddDiligentEngine(pluginManager)
+                    .AddDiligentEnginePbr(o =>
+                    {
+                        o.CustomizePbrOptions = RendererCI =>
+                        {
+                            //RendererCI.AllowDebugView = true;
+                            RendererCI.UseIBL = true;
+                        };
+                    })
+                    .AddDiligentEnginePbrShapes();
+
+            services.AddOSPlatform(pluginManager);
+
+            services.AddFirstPersonFlyCamera();
+
+            //Add this app's services
+            services.TryAddSingleton<BepuUpdateListener>();
+
+            return true;
+        }
+
+        public override bool OnLink(IServiceProvider serviceProvider)
+        {
+            var log = serviceProvider.GetRequiredService<ILogger<CoreApp>>();
+            log.LogInformation("Running from directory {0}", FolderFinder.ExecutableFolder);
+
+            //Setup virtual file system
+            var vfs = serviceProvider.GetRequiredService<VirtualFileSystem>();
+            var assetPath = Path.GetFullPath("../../../../../Engine-Next-Assets"); //This needs to be less hardcoded.
+            vfs.addArchive(assetPath);
+
+            mainTimer = serviceProvider.GetRequiredService<UpdateTimer>();
+
+            var updateListener = serviceProvider.GetRequiredService<BepuUpdateListener>();
+            mainTimer.addUpdateListener(updateListener);
+
+            PerformanceMonitor.setupEnabledState(serviceProvider.GetRequiredService<SystemTimer>());
+
+            return true;
+        }
+
+        public override int OnExit()
+        {
+            return 0;
+        }
+
+        public override void OnIdle()
+        {
+            mainTimer?.OnIdle();
+        }
+    }
+}
