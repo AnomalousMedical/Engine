ee45e73999ff3479c970ed9a0a52a4a5da745a5f
Andrew Piper <andrew@threax.com>
2021-12-26 16:16:13 -0500

    Don't loop in a cone to reflect rays.

--- DiligentEngine.RT/assets/RayUtils.hlsl
+++ DiligentEngine.RT/assets/RayUtils.hlsl
@@ -283,25 +283,13 @@ void LightAndShadeShinyUV
     pertNormal = normalize(mul((float3x3) ObjectToWorld3x4(), pertNormal));
 
 
-    // Reflect normal.
-    float3 rayDir = reflect(WorldRayDirection(), pertNormal);
-
+    // Reflect from the normal.
     RayDesc ray;
     ray.Origin = WorldRayOrigin() + WorldRayDirection() * RayTCurrent() + normal * SMALL_OFFSET;
     ray.TMin = 0.0;
     ray.TMax = 100.0;
-
-    // Cast multiple rays that are distributed within a cone.
-    float3    color = float3(0.0, 0.0, 0.0);
-    const int ReflBlur = payload.Recursion > 1 ? 1 : 1;// g_ConstantsCB.SphereReflectionBlur;, can use roughness texture here
-    for (int j = 0; j < ReflBlur; ++j)
-    {
-        float2 offset = float2(g_ConstantsCB.DiscPoints[j / 2][(j % 2) * 2], g_ConstantsCB.DiscPoints[j / 2][(j % 2) * 2 + 1]);
-        ray.Direction = DirectionWithinCone(rayDir, offset * 0.01);
-        color += CastPrimaryRay(ray, payload.Recursion + 1).Color;
-    }
-
-    color /= float(ReflBlur);
+    ray.Direction = reflect(WorldRayDirection(), pertNormal);
+    float3 color = CastPrimaryRay(ray, payload.Recursion + 1).Color;
 
     // Sample texturing.
     float3 texColor = colorTexture.SampleLevel(colorSampler, uv, mip).rgb;
